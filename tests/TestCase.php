aa9c2e864e7ea40cc05d6bfdfac2cafa873683f4
vagrant <vagrant@homestead>
2019-01-08 15:37:14 +0000

    Added test for adding a completion_question @wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,19 +1,33 @@
 <?php
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase {
+class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
 
-		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+    protected $baseUrl = 'http://test-correct.test';
 
-		return $app;
-	}
+    const USER_TEACHER = 'p.vries@31.com';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+        return $app;
+    }
+
+    public static function getAuthRequestData($overrides = [])
+    {
+        return array_merge([
+            'session_hash' => '5mTzff9qk9TObgv0NfsU7JjLDgnBsRRYSDfASpPEYLb2GjZHSQh1aePy1vhnBq1gFsjWprSU0dsCUgKTJhJVzbIHEEd7Mzw2Y1Y',
+            'user'         => static::USER_TEACHER,
+        ], $overrides);
+    }
 
 }
