12e0f304fa902098b80c0d03e880dd6ab2a44e07
Erik <Erik@sobit.nl>
2019-06-03 17:55:56 +0200

    added bloom to questions table and made it fillable

---
+++ database/migrations/2019_06_03_173845_add_bloom_to_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBloomToQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->string('bloom')->index('bloom')->after('rtti');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('bloom');
+        });
+    }
+}
