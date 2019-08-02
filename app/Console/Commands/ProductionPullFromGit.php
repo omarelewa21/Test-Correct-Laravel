60bfb796aa1984af68dfb482c3de3ae174478bb1
erik <erik@sobit.nl>
2019-08-02 15:47:12 +0000

    added a safeguard that production:pull can only be run from a production env

--- app/Console/Commands/ProductionPullFromGit.php
+++ app/Console/Commands/ProductionPullFromGit.php
@@ -41,10 +41,11 @@ class ProductionPullFromGit extends Command
     public function handle()
     {
 
-//        if (config('app.env') !== 'production') {
-//            $this->error('You cannot perform this action on this environment! only with APP_ENV set to production');
-//            return false;
-//        }
+        if (config('app.env') !== 'production') {
+            $this->error('You cannot perform this action on this environment! only with APP_ENV set to production');
+            return false;
+        }
+        
         $this->info('Going to pull the latest info from git');
             if(!exec('git pull')){
                 $this->error('I\'m sorry, but we couldn\'t pull the latest data from git, please fix this first');
