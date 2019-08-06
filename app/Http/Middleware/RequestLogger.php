a8abf541f2803006cb3e049776d06f7eb3a3a446
vagrant <vagrant@homestead>
2019-08-06 12:34:38 +0000

    disable RequestLogger when in Production

--- app/Http/Middleware/RequestLogger.php
+++ app/Http/Middleware/RequestLogger.php
@@ -19,41 +19,51 @@ class RequestLogger
      */
     public function handle($request, Closure $next)
     {
-        if(!defined('LARAVEL_START')){
-            define('LARAVEL_START',microtime(true));
+        if ($this->notInProduction()) {
+            if (!defined('LARAVEL_START')) {
+                define('LARAVEL_START', microtime(true));
+            }
         }
         return $next($request);
     }
 
-    public function terminate($request, $response){
-        if($request->path() == 'fpm-status' || $request->path() == 'nginx_status'){
-            return true;
-        }
+    public function terminate($request, $response)
+    {
+        if ($this->notInProduction()) {
+            if ($request->path() == 'fpm-status' || $request->path() == 'nginx_status') {
+                return true;
+            }
 
-        $end = microtime(true) * 1000;
-        $ip = $request->ip();
+            $end = microtime(true) * 1000;
+            $ip = $request->ip();
 
-        if (array_key_exists("HTTP_CF_CONNECTING_IP", $_SERVER)) {
-            $ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            if (array_key_exists("HTTP_CF_CONNECTING_IP", $_SERVER)) {
+                $ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            }
+            if (!defined('LARAVEL_START')) {
+                define('LARAVEL_START', microtime(true));
+            }
+            $record = [
+                'uri'        => $request->url(),
+                'uri_full'   => $request->fullUrl(),
+                'method'     => $request->method(),
+                'request'    => json_encode($request->except(['password'])),
+                'response'   => $response->getContent(),
+                'headers'    => json_encode($request->headers->all()),
+                'code'       => $response->getStatusCode(),
+                'ip'         => $ip,
+                'duration'   => $end - (LARAVEL_START * 1000),
+                'created_at' => date("Y-m-d H:i:s"),
+                'user_id'    => Auth::user() ? Auth::user()->id : null,
+                'user_agent' => $request->header('User-Agent'),
+                'success'    => $response->getStatusCode() === 200
+            ];
+            Log::create($record);
         }
-        if(!defined('LARAVEL_START')){
-            define('LARAVEL_START',microtime(true));
-        }
-        $record = [
-            'uri' => $request->url(),
-            'uri_full' => $request->fullUrl(),
-            'method' => $request->method(),
-            'request' => json_encode($request->except(['password'])),
-            'response' => $response->getContent(),
-            'headers'   => json_encode($request->headers->all()),
-            'code' => $response->getStatusCode(),
-            'ip' => $ip,
-            'duration' => $end - (LARAVEL_START * 1000),
-            'created_at' => date("Y-m-d H:i:s"),
-            'user_id' => Auth::user() ? Auth::user()->id : null,
-            'user_agent' => $request->header('User-Agent'),
-            'success' => $response->getStatusCode() === 200
-        ];
-        Log::create($record);
+    }
+
+    private function notInProduction()
+    {
+        return in_array(env('APP_ENV'), ['local', 'testing']);
     }
 }
