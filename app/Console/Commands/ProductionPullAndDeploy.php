b9855829dcc890e9db8cfc824cff58befbdf8a14
erik <erik@sobit.nl>
2019-08-09 10:23:54 +0000

    added composer to the production pull and deploy script

--- app/Console/Commands/ProductionPullAndDeploy.php
+++ app/Console/Commands/ProductionPullAndDeploy.php
@@ -69,9 +69,9 @@ class ProductionPullAndDeploy extends Command
             $this->addMigrations();
         }
 
-//        if($this->confirm('Do you wan\'t to do a composer install if needed?')){
-//            $this->composerInstall();
-//        }
+        if($this->confirm('Do you wan\'t to do a composer install if needed?')){
+            $this->composerInstall();
+        }
 
 
         $this->info('Start caching');
@@ -96,7 +96,7 @@ class ProductionPullAndDeploy extends Command
 
     protected function composerInstall(){
         $this->info('going to do a composer install');
-        if(!exec('composer install --optimize-autoloader --no-dev')){
+        if(!exec('php composer.phar install --optimize-autoloader --no-dev')){
             $this->error('an error occured while doing a composer install, please take care manually');
         }
         $this->info('done');
