ba018456a16c7421429e6b3144fcb20874643505
Jonathan <jonathanjagt@teachandlearncompany.com>
2020-06-05 11:06:39 +0200

    Refactor artisan command to class
    Now we can use the DatabaseImport in the code without any problems with Artisan.

--- app/Console/Commands/RefreshDatabase.php
+++ app/Console/Commands/RefreshDatabase.php
@@ -3,9 +3,7 @@
 namespace tcCore\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
-use Symfony\Component\Process\Process;
 
 class RefreshDatabase extends Command
 {
@@ -43,27 +41,21 @@ class RefreshDatabase extends Command
     public function handle()
     {
         $sqlImports = [
-            'dropAllTablesAndViews.sql',
-            'testdb.sql',
-            'attainments.sql',
+            database_path('seeds/dropAllTablesAndViews.sql'),
+            database_path('seeds/testdb.sql'),
+            database_path('seeds/attainments.sql'),
         ];
-
-        file_put_contents(public_path() . "/waiting.txt", "1");
         
         if ($this->hasOption('file') && $this->option('file') != null) {
             $sqlImports = [
-                sprintf('testing/db_dump_%s.sql', $this->option('file')),
+                database_path(sprintf('seeds/testing/db_dump_%s.sql', $this->option('file'))),
             ];
         }
-
 
         if (!in_array(env('APP_ENV'), ['local', 'testing'])) {
             $this->error('You cannot perform this action on this environment! only with APP_ENV set to local!!');
             return false;
         }
-
-        // this might be slow, so give us some time
-        ini_set('max_execution_time', 180); //3 minutes
 
         $this->info('start refreshing database...(this can take some time as in several minutes)');
         // only needed when using mysql database, not when sqlite setup is needed
@@ -78,8 +70,6 @@ class RefreshDatabase extends Command
         }
 
         $this->addMigrations();
-
-        file_put_contents(public_path() . "/waiting.txt", "0");
 
         $this->info('refresh database complete');
     }
