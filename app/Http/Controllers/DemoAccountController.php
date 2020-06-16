6b7f4673493cc39db3499b29f19ce3e1016f5bd3
Erik | Sobit <Erik@sobit.nl>
2020-06-16 11:41:39 +0200

    typo

--- app/Http/Controllers/DemoAccountController.php
+++ app/Http/Controllers/DemoAccountController.php
@@ -155,7 +155,5 @@ class DemoAccountController extends Controller
             'how_did_you_hear_about_test_correct' => 'sometimes',
         ];
     }
-=======
-    //
->>>>>>> feature/TC-156-proj.-autom.-docentacc.-new-form-
+
 }
