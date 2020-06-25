8303c26e608d7771c98f74d172a74d3c01d8b0de
vagrant <vagrant@homestead>
2020-06-25 14:02:47 +0000

    added truncate before updateForUser in  OnboardingWizardReport::updateForAllTeachers method

--- app/OnboardingWizardReport.php
+++ app/OnboardingWizardReport.php
@@ -159,6 +159,8 @@ ORDER BY t2.displayorder,
 
     public static function updateForAllTeachers()
     {
+        OnboardingWizardReport::truncate();
+
         User::whereIn('id', Teacher::pluck('user_id'))->where('demo', 0)->each(function ($teacher) {
             if ($teacher->isA('teacher')) {
                 \tcCore\OnboardingWizardReport::updateForUser($teacher);
