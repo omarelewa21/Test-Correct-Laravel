67dfc4e3484ef29c7d9f3def32ba46cf0734abf0
Erik | Sobit <Erik@sobit.nl>
2020-06-26 11:37:27 +0200

    merged feature/TC-156-proj.-autom.-docentacc.-new-form-

--- app/DemoTeacherRegistration.php
+++ app/DemoTeacherRegistration.php
@@ -28,47 +28,51 @@ class DemoTeacherRegistration extends Model
     {
         if (request('shouldRegisterUser') == true) {
 
-        if ($user->schoolLocation->is(SchoolHelper::getTempTeachersSchoolLocation())) {
-            // dit is het scenario dat ik vanaf het formulier van buiten kom.
-            if (request('school_location') && request('website_url')) {
-                $parameterBag = [
-                    'school_location'                     => request('school_location'),
-                    'website_url'                         => request('website_url'),
-                    'address'                             => request('address'),
-                    'postcode'                            => request('postcode'),
-                    'city'                                => request('city'),
-                    'gender'                              => request('gender'),
-                    'gender_different'                    => request('gender_different'),
-                    'name_first'                          => request('name_first'),
-                    'name_suffix'                         => request('name_suffix'),
-                    'name'                                => request('name'),
-                    'mobile'                              => request('mobile'),
-                    'username'                            => request('username'),
-                    'subjects'                            => request('subjects'),
-                    'remarks'                             => request('remarks'),
-                    'how_did_you_hear_about_test_correct' => request('how_did_you_hear_about_test_correct'),
-                    'user_id'                             => $user->getKey(),
-                ];
-            } else {
-                // dit is het scenario dat ik via tel a teacher kom
-                $parameterBag = [
-                    'name_first'                          => $user->name_first,
-                    'name_suffix'                         => $user->suffix,
-                    'name'                                => $user->name,
-                    'username'                            => $user->username,
-                    'how_did_you_hear_about_test_correct' => 'Got invited by a teacher.',
-                    'user_id'                             => $user->getKey(),
-                ];
+            if ($user->schoolLocation->is(SchoolHelper::getTempTeachersSchoolLocation())) {
+                // dit is het scenario dat ik vanaf het formulier van buiten kom.
+                if (request('school_location') && request('website_url')) {
+                    $parameterBag = [
+                        'school_location' => request('school_location'),
+                        'website_url' => request('website_url'),
+                        'address' => request('address'),
+                        'postcode' => request('postcode'),
+                        'city' => request('city'),
+                        'gender' => request('gender'),
+                        'gender_different' => request('gender_different'),
+                        'name_first' => request('name_first'),
+                        'name_suffix' => request('name_suffix'),
+                        'name' => request('name'),
+                        'mobile' => request('mobile'),
+                        'username' => request('username'),
+                        'subjects' => request('subjects'),
+                        'remarks' => request('remarks'),
+                        'how_did_you_hear_about_test_correct' => request('how_did_you_hear_about_test_correct'),
+                        'user_id' => $user->getKey(),
+                    ];
+                } else {
+                    // dit is het scenario dat ik via tel a teacher kom
+                    $inviter = User::find($user->invited_by);
+                    $parameterBag = [
+                        'name_first' => $user->name_first,
+                        'name_suffix' => $user->suffix,
+                        'name' => $user->name,
+                        'username' => $user->username,
+                        'how_did_you_hear_about_test_correct' => sprintf('uitgenodigd door:%s', $inviter->username),
+                        'user_id' => $user->getKey(),
+                    ];
 
                     if ($user->emailDomainInviterAndInviteeAreEqual()) {
-                        if ($inviter = User::find($user->invited_by)) {
-                            if ($demoTeacherRegistration = self::whereUsername($inviter->username)->first()) {
-                                // merge de attributes;
-                                $parameterBag = array_merge(
-                                    $demoTeacherRegistration->toArray(), $parameterBag
-                                );
-                                unset($parameterBag['id']);
-                            }
+                        if ($demoTeacherRegistration = self::whereUsername($inviter->username)->first()) {
+                            // merge de attributes;
+                            $attrToBeCopied = $demoTeacherRegistration->toArray();
+
+                            foreach (['id', 'gender', 'gender_different', 'subjects', 'remarks', 'created_at', 'mobile'] as $attrToBeIgnored) {
+                                unset($attrToBeCopied[$attrToBeIgnored]);
+                            };
+
+                            $parameterBag = array_merge(
+                                $demoTeacherRegistration->toArray(), $parameterBag
+                            );
                         }
                     }
                 }
@@ -77,5 +81,4 @@ class DemoTeacherRegistration extends Model
             }
         }
     }
-
 }
