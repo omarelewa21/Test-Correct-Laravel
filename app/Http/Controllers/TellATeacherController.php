463388aa4cbc1ebb3c025aa4f720b7b5239a7934
Erik | Sobit <Erik@sobit.nl>
2020-07-07 17:46:40 +0200

    changed way tell a teacher works to multi user submit and feedback

--- app/Http/Controllers/TellATeacherController.php
+++ app/Http/Controllers/TellATeacherController.php
@@ -3,17 +3,35 @@
 namespace tcCore\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
+use tcCore\Http\Helpers\UserHelper;
+use tcCore\Http\Requests\CreateTellATeacherRequest;
 use tcCore\Http\Requests\CreateUserRequest;
 
 class TellATeacherController extends Controller
 {
-    public function store(CreateUserRequest $request)
+    public function store(CreateTellATeacherRequest $request)
     {
-        // signal the DemoTeacherRegistration model to record the user in a DemoTeacherRegistration object;
-        $request->merge([
-            'shouldRegisterUser' => true,
-        ]);
+        $r = $request->validated();
+        DB::beginTransaction();
+        try {
+            foreach ($r['data'] as $i => $data) {
+                $data['shouldRegisterUser'] = true;
+                $data = array_merge($data,$r);
+                unset($data['data']);
+                if(!(new UserHelper())->createUserFromData($data)){
+                    logger(sprintf('Error while inviting other teachers %s',json_encode($data)));
+                    throw new \Exception(sprintf('Could not create user %s',$data['username']));
+                }
+            }
+        } catch(\Exception $e){
+            DB::rollback();
+            logger($e->getMessage());
+            return Response::make('Failed to create users', 500);
+        }
 
-        return (new UsersController)->store($request);
+        DB::commit();
+        return Response::make(sprintf('%d',count($r['data'])), 200);
     }
 }
---
+++ app/Http/Helpers/UserHelper.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: erik
+ * Date: 09/04/2020
+ * Time: 10:59
+ */
+
+namespace tcCore\Http\Helpers;
+
+
+use Carbon\Carbon;
+use tcCore\Answer;
+use tcCore\BaseSubject;
+use tcCore\EducationLevel;
+use tcCore\Jobs\SendWelcomeMail;
+use tcCore\Lib\Repositories\PeriodRepository;
+use tcCore\Lib\Repositories\SchoolYearRepository;
+use tcCore\Lib\User\Factory;
+use tcCore\SchoolClass;
+use tcCore\SchoolLocation;
+use tcCore\SchoolLocationEducationLevel;
+use tcCore\SchoolLocationSection;
+use tcCore\SchoolYear;
+use tcCore\Section;
+use tcCore\Student;
+use tcCore\Subject;
+use tcCore\Teacher;
+use tcCore\Test;
+use tcCore\TestParticipant;
+use tcCore\TestTake;
+use tcCore\Text2speech;
+use tcCore\Text2speechLog;
+use tcCore\User;
+
+class UserHelper
+{
+
+    public function createUserFromData($data){
+        $userFactory = new Factory(new User());
+
+        $user = $userFactory->generate($data);
+
+        if ($user !== false) {
+            if (isset($data['send_welcome_mail']) && $data['send_welcome_mail'] == true) {
+                dispatch_now(new SendWelcomeMail($user->getKey(), isset($data['url']) ? $data['url'] : false));
+            }
+            return $user;
+        } else {
+            return false;
+        }
+    }
+}
