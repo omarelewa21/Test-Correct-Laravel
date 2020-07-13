060ece48a46469bb177e7f39e91a9458586da128
Jonathan <jonathanjagt@teachandlearncompany.com>
2020-07-13 11:07:06 +0200

    Add abbreviation to DemoTeacherRegistration

---
+++ database/migrations/2020_07_13_105802_demo_teacher_registration_abbreviation.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DemoTeacherRegistrationAbbreviation extends Migration
+{    /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+   public function up()
+   {
+       Schema::table('demo_teacher_registrations', function (Blueprint $table) {
+           $table->string('abbreviation');
+
+       });
+   }
+
+   /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+   public function down()
+   {
+       Schema::table('demo_teacher_registrations', function (Blueprint $table) {
+           $table->removeColumn('abbreviation');
+       });
+   }
+}
