7698ddfbddc3250fdaab6c06a3c10c9927faa885
vagrant <vagrant@homestead>
2020-08-11 11:48:23 +0000

    changed response format to reflect error format required for field based warnings

--- app/Http/Requests/SchoolClassesStudentImportRequest.php
+++ app/Http/Requests/SchoolClassesStudentImportRequest.php
@@ -32,7 +32,7 @@ class SchoolClassesStudentImportRequest extends Request {
 	public function rules()
 	{
 		return [
-		    'data' => 'array',
+		    //'data' => 'array',
 		    'data.*.username' => 'required|email|unique:users,username',
             'data.*.name_first' => 'required',
             'data.*.name' => 'required'
@@ -50,38 +50,39 @@ class SchoolClassesStudentImportRequest extends Request {
 	}
 
 
-    protected function failedValidation(Validator $validator) {
-	    throw new HttpResponseException(response()->json($this->formatErrors($validator), 422));
-	}
+//    protected function failedValidation(Validator $validator) {
+//	    throw new HttpResponseException(response()->json($this->formatErrors($validator), 422));
+//	}
 
-    protected function formatErrors(Validator $validator)
-    {
-        $results = [];
-        $flags = ['username' => false, 'name_first' => false, 'name' => false];
-        $messages = $validator->errors()->messages();
-        foreach ($messages as $key => $value) {
-            if (!str_contains($key, 'username') && !$flags['username']) {
-                $results[] = 'De emailadressen dienen aanwezig, welgevormd en unique te zijn.';
-                $flags['username'] = true;
-            }
-            else if (str_contains($key, 'name_first') && !$flags['name_first']) {
-                $results[] = 'De voornamen zijn verplicht';
-                $flags['name_first'];
-            }
-            else if (str_contains($key, 'name') && !$flags['name']) {
-                $results[] = 'De achternamen zijn verplicht';
-                $flags['name'] = true;
-            }
-            else if (str_contains($key,'class')){
-                $results[] = 'Het is niet duidelijk om welke klas het gaat';
-            }
-            if($flags['username'] == true && $flags['name_first'] == true && $flags['name'] == true){
-                break;
-            }
-        }
-        return $results;
-//        return $validator->errors()->all();
-    }
+//    protected function formatErrors(Validator $validator)
+//    {
+//        $results = [];
+//        $flags = ['username' => false, 'name_first' => false, 'name' => false];
+//        $messages = $validator->errors()->messages();
+//        logger($messages);
+//        foreach ($messages as $key => $value) {
+//            if (str_contains($key, 'username') && !$flags['username']) {
+//                $results[] = 'De emailadressen dienen aanwezig, welgevormd en unique te zijn.';
+//                $flags['username'] = true;
+//            }
+//            else if (str_contains($key, 'name_first') && !$flags['name_first']) {
+//                $results[] = 'De voornamen zijn verplicht';
+//                $flags['name_first'];
+//            }
+//            else if (str_contains($key, 'name') && !$flags['name']) {
+//                $results[] = 'De achternamen zijn verplicht';
+//                $flags['name'] = true;
+//            }
+//            else if (str_contains($key,'class')){
+//                $results[] = 'Het is niet duidelijk om welke klas het gaat';
+//            }
+//            if($flags['username'] == true && $flags['name_first'] == true && $flags['name'] == true){
+//                break;
+//            }
+//        }
+//        return $results;
+////        return $validator->errors()->all();
+//    }
 
     /**
      * Configure the validator instance.
@@ -89,8 +90,9 @@ class SchoolClassesStudentImportRequest extends Request {
      * @param  \Illuminate\Validation\Validator $validator
      * @return void
      */
-    public function getWithValidator($validator){
+    public function withValidator($validator){
         $validator->after(function ($validator) {
+            $data = $this->request->get('data');
             if($this->schoolClass == null){
                 $validator->errors()->add('class','Er dient een klas opgegeven te worden');
             }
@@ -117,6 +119,7 @@ class SchoolClassesStudentImportRequest extends Request {
 //            else{
 //                $validator->errors()->add('data','Geen valide data ontvangen');
 //            }
+            request()->merge(['data' => $data]);
         });
     }
 
