bbb32f26461cc3017689a314ad4e9e1ae992a4e6
Jonathan <jonathanjagt@teachandlearncompany.com>
2020-09-29 20:19:45 +0200

    Fix issue TCP-312 with converting UUIDs to IDs

--- app/Http/Requests/CreateTestTakeRequest.php
+++ app/Http/Requests/CreateTestTakeRequest.php
@@ -1,5 +1,9 @@
 <?php namespace tcCore\Http\Requests;
 
+use Ramsey\Uuid\Uuid;
+use tcCore\Test;
+use tcCore\TestTake;
+
 class CreateTestTakeRequest extends Request {
 
 	/**
@@ -33,6 +37,39 @@ class CreateTestTakeRequest extends Request {
 		];
 	}
 
+    /**
+     * Configure the validator instance.
+     *
+     * @param \Illuminate\Validation\Validator $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $data = ($this->all());
+
+            if(isset($data["retake_test_take_id"]) && Uuid::isValid($data['retake_test_take_id'])){
+                $educationLevel = TestTake::whereUuid($data['retake_test_take_id'])->first();
+                if(!$educationLevel){
+                    $validator->errors()->add('retake_test_take_id','Deze afname toets kon helaas niet terug gevonden worden.');
+                } else {
+                    $data['retake_test_take_id'] = $educationLevel->getKey();
+                }
+			}
+			
+			if(isset($data["test_id"]) && Uuid::isValid($data['test_id'])){
+                $educationLevel = Test::whereUuid($data['test_id'])->first();
+                if(!$educationLevel){
+                    $validator->errors()->add('test_id','Deze toets kon helaas niet terug gevonden worden.');
+                } else {
+                    $data['test_id'] = $educationLevel->getKey();
+                }
+            }
+
+            request()->merge($data);
+        });
+	}
+	
 	/**
 	 * Get the sanitized input for the request.
 	 *
