c7476308be57f660b193f3e9de33244a74c8a503
vagrant <vagrant@homestead>
2020-10-09 11:17:43 +0000

    added some tests

--- app/Http/Requests/SchoolClassesStudentImportRequest.php
+++ app/Http/Requests/SchoolClassesStudentImportRequest.php
@@ -6,17 +6,19 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 use Ramsey\Uuid\Uuid;
 use tcCore\SchoolLocation;
 
-class SchoolClassesStudentImportRequest extends Request {
+class SchoolClassesStudentImportRequest extends Request
+{
 
     protected $schoolLocation;
     protected $schoolClass;
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
         $this->schoolLocation = $this->route('schoolLocation');
         $this->schoolClass = $this->route('schoolClass');
 
@@ -24,90 +26,141 @@ class SchoolClassesStudentImportRequest extends Request {
             Auth::user()->hasRole('School manager')
             && $this->schoolLocation !== null
             && Auth::user()->school_location_id == $this->schoolLocation->getKey();
-	}
+    }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
-	{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
         $this->filterInput();
 
         $extra_rule = [];
         // unique constraint needs to be added on external_id can only exist within a school if it is the same user (that is username is the currect username)
         foreach ($this->data as $key => $value) {
-            $extra_rule[sprintf('data.%d.external_id', $key)] = sprintf('unique:users,external_id,%s,username,school_location_id,%d',$value['username'], $this->schoolLocation->getKey());
+            if (array_key_exists('username', $value)) {
+                $extra_rule[sprintf('data.%d.external_id', $key)] = sprintf('unique:users,external_id,%s,username,school_location_id,%d', $value['username'], $this->schoolLocation->getKey());
+            }
         }
 
-		return collect([
-		    //'data' => 'array',
-		    'data.*.username' => 'required|email',
-            'data.*.name_first' => 'required',
-            'data.*.name' => 'required',
-//dd            'data.0.external_id' => sprintf('unique:users,external_id,erik@sobit.nl,username,school_location_id,%d',$this->schoolLocation->getKey()),
-            'data.*.name_suffix' => '',
-            'data.*.gender' => '',
-        ])->merge($extra_rule)->toArray();
-	}
+        foreach ($this->data as $key => $value) {
+            if (array_key_exists('username', $value)) {
+                $extra_rule[sprintf('data.%d.username', $key)] = sprintf('unique:users,username,%s,external_id,username,%s', $value['external_id'], $value['username']);
+            }
+        }
 
-	/**
-	 * Get the sanitized input for the request.
-	 *
-	 * @return array
-	 */
-	public function sanitize()
-	{
-		return $this->all();
-	}
+        $rules = collect([
+            //'data' => 'array',
+            'data.*.username'    => 'required|email',
+            'data.*.name_first'  => 'required',
+            'data.*.name'        => 'required',
+            'data.*.name_suffix' => '',
+            'data.*.gender'      => '',
+        ]);
+
+        if ($extra_rule === []) {
+            $mergedRules = $rules->merge([
+                'data.*.external_id' => 'required',
+            ]);
+        } else {
+            $mergedRules = $rules->merge($extra_rule);
+        }
+        logger($mergedRules->toArray());
+
+        return $mergedRules->toArray();
+    }
+
+    /**
+     * Get the sanitized input for the request.
+     *
+     * @return array
+     */
+    public function sanitize()
+    {
+        return $this->all();
+    }
 
     /**
      * Configure the validator instance.
      *
-     * @param  \Illuminate\Validation\Validator $validator
+     * @param \Illuminate\Validation\Validator $validator
      * @return void
      */
-    public function withValidator($validator){
+    public function withValidator($validator)
+    {
         $validator->after(function ($validator) {
-            if($this->schoolClass == null){
-                $validator->errors()->add('class','Er dient een klas opgegeven te worden');
+            if ($this->schoolClass == null) {
+                $validator->errors()->add('class', 'Er dient een klas opgegeven te worden');
             }
 
-            $data = collect(request()->input('data'));
-            $uniqueFields = ['external_id'];
-            $groupedByDuplicates = $data->groupBy(function($row, $key) {
-                return $row['external_id'];
-            })->map(function($item) {
-                return collect($item)->count();
-            })->filter(function($item, $key){
-                return $item > 1;
-            });
+            $data = $this->addDuplicateExternalIdErrors($validator);
+            $data = $this->addDuplicateUsernameErrors($validator);
 
-
-            if ($groupedByDuplicates->count() < $data->count()) {
-                collect($this->data)->each(function($item, $key)  use ($groupedByDuplicates, $validator){
-                    if (array_key_exists( $item['external_id'], $groupedByDuplicates->toArray())) {
-                        $validator->errors()->add(
-                            sprintf('data.%d.external_id', $key),
-                            'Deze import bevat dubbele studentennummers'
-                        );
-                    }
-                });
-            }
-
-            $data = $data->toArray();
-
-            if(isset($data['filter']) && isset($data['filter']['school_location_id']) && Uuid::isValid($data['filter']['school_location_id'])){
+            if (isset($data['filter']) && isset($data['filter']['school_location_id']) && Uuid::isValid($data['filter']['school_location_id'])) {
                 $item = SchoolLocation::whereUuid($data['filter']['school_location_id'])->first();
-                if(!$item){
-                        $validator->errors()->add('school_location_id','De school locatie kon niet gevonden worden.');
-                    } else {
-                        $data['filter']['school_location_id'] = $item->getKey();
-                    }
+                if (!$item) {
+                    $validator->errors()->add('school_location_id', 'De school locatie kon niet gevonden worden.');
+                } else {
+                    $data['filter']['school_location_id'] = $item->getKey();
+                }
             }
             $this->merge(['data' => $data]);
         });
+    }
+
+    private function addDuplicateExternalIdErrors($validator){
+        $data = collect(request()->input('data'));
+        $uniqueFields = ['external_id'];
+        $groupedByDuplicates = $data->groupBy(function ($row, $key) {
+            if (array_key_exists('external_id', $row)) {
+                return $row['external_id'];
+            }
+        })->map(function ($item) {
+            return collect($item)->count();
+        })->filter(function ($item, $key) {
+            return $item > 1;
+        });
+
+        if ($groupedByDuplicates->count() < $data->count()) {
+            collect($this->data)->each(function ($item, $key) use ($groupedByDuplicates, $validator) {
+                if (array_key_exists('external_id', $item) && array_key_exists($item['external_id'], $groupedByDuplicates->toArray())) {
+                    $validator->errors()->add(
+                        sprintf('data.%d.external_id', $key),
+                        'Deze import bevat dubbele studentennummers'
+                    );
+                }
+            });
+        }
+
+        return $data->toArray();
+    }
+
+    private function addDuplicateUsernameErrors($validator){
+        $data = collect(request()->input('data'));
+        $groupedByDuplicates = $data->groupBy(function ($row, $key) {
+            if (array_key_exists('username', $row)) {
+                return $row['username'];
+            }
+        })->map(function ($item) {
+            return collect($item)->count();
+        })->filter(function ($item, $key) {
+            return $item > 1;
+        });
+
+        if ($groupedByDuplicates->count() < $data->count()) {
+            collect($this->data)->each(function ($item, $key) use ($groupedByDuplicates, $validator) {
+                if (array_key_exists('username', $item) && array_key_exists($item['username'], $groupedByDuplicates->toArray())) {
+                    $validator->errors()->add(
+                        sprintf('data.%d.username', $key),
+                        'Deze import bevat dubbele emailadressen'
+                    );
+                }
+            });
+        }
+
+        return $data->toArray();
     }
 
 }
---
+++ tests/Feature/SchoolClassStudentImportControllerTest.php
@@ -0,0 +1,382 @@
+<?php
+
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Hash;
+use tcCore\Exceptions\Handler;
+use tcCore\SchoolClass;
+use tcCore\SchoolLocation;
+use tcCore\Student;
+use tcCore\User;
+use Tests\TestCase;
+
+class SchoolClassStudentImportControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+
+    /** @test */
+    public function it_can_import_a_user()
+    {
+        $this->assertCount(0, User::whereUsername("carloschoep+K999jjanssen@hotmail.com")->get());
+
+        $countStudentsBefore = \tcCore\Student::count();
+
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                'external_id' => "12345",
+                'name_first'  => "Jan",
+                'name_suffix' => "",
+                'name'        => "Janssen",
+                'username'    => "carloschoep+K999jjanssen@hotmail.com",
+            ]],
+        ]))->assertSuccessful();
+        $this->assertEquals('1 studenten zijn toegevoegd', $response->decodeResponseJson());
+
+        $this->assertCount(1, User::whereUsername("carloschoep+K999jjanssen@hotmail.com")->get());
+        $this->assertCount(++$countStudentsBefore, Student::get());
+        $this->assertEquals(
+            3,
+            User::whereUsername("carloschoep+K999jjanssen@hotmail.com")->first()->students()->first()->class_id
+        );
+    }
+
+    /** @test */
+    public function it_can_import_multiple_users()
+    {
+        $this->assertCount(0, User::whereUsername("carloschoep+K999jjanssen@hotmail.com")->get());
+
+        $countStudentsBefore = \tcCore\Student::count();
+
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                'external_id' => "12345",
+                'name_first'  => "Jan",
+                'name_suffix' => "",
+                'name'        => "Janssen",
+                'username'    => "carloschoep+K999jjanssen@hotmail.com",
+            ], [
+                'external_id' => "12346",
+                'name_first'  => "Janus",
+                'name_suffix' => "",
+                'name'        => "Janssens",
+                'username'    => "carloschoep+K1000jjanssen@hotmail.com",
+            ]],
+        ]))->assertSuccessful();
+        $this->assertEquals('2 studenten zijn toegevoegd', $response->decodeResponseJson());
+
+        $this->assertCount(1, User::whereUsername("carloschoep+K999jjanssen@hotmail.com")->get());
+        $this->assertCount(1, User::whereUsername("carloschoep+K1000jjanssen@hotmail.com")->get());
+        $this->assertCount(2 + $countStudentsBefore, Student::get());
+        $this->assertEquals(
+            3,
+            User::whereUsername("carloschoep+K999jjanssen@hotmail.com")->first()->students()->first()->class_id
+        );
+        $this->assertEquals(
+            3,
+            User::whereUsername("carloschoep+K1000jjanssen@hotmail.com")->first()->students()->first()->class_id
+        );
+    }
+
+    /** @test */
+    public function the_email_address_should_be_unique_for_every_user_in_the_request()
+    {
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                'external_id' => "12345",
+                'name_first'  => "Jan",
+                'name_suffix' => "",
+                'name'        => "Janssen",
+                'username'    => "carloschoep+K999jjanssen@hotmail.com",
+            ], [
+                'external_id' => "12346",
+                'name_first'  => "Janus",
+                'name_suffix' => "",
+                'name'        => "Janssens",
+                'username'    => "carloschoep+K999jjanssen@hotmail.com",
+            ]],
+        ]))->assertStatus(422);
+
+        $decodedResponse = $response->decodeResponseJson();
+        $requiredErrors = [
+            'data.0.username'   => 'Deze import bevat dubbele emailadressen',
+            'data.1.username'   => 'Deze import bevat dubbele emailadressen',
+        ];
+
+        foreach ($requiredErrors as $errorField => $errorMessage) {
+            $this->assertArrayHasKey($errorField, $decodedResponse['errors']);
+            $this->assertEquals($errorMessage, $decodedResponse['errors'][$errorField][0]);
+        }
+    }
+
+    /** @test */
+    public function the_email_address_should_be_unique_and_cannot_already_be_in_the_database()
+    {
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                'external_id' => "12345",
+                'name_first'  => "Jan",
+                'name_suffix' => "",
+                'name'        => "Janssen",
+                'username'    => "carloschoep+K999jjanssen@hotmail.com",
+            ],],
+        ]))->assertSuccessful();
+
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [ [
+                'external_id' => "12346",
+                'name_first'  => "Janus",
+                'name_suffix' => "",
+                'name'        => "Janssens",
+                'username'    => "carloschoep+K999jjanssen@hotmail.com",
+            ]],
+        ]))->assertStatus(422);
+
+        $decodedResponse = $response->decodeResponseJson();
+        $requiredErrors = [
+            'data.0.username'   => 'The data.0.username has already been taken.',
+        ];
+
+        foreach ($requiredErrors as $errorField => $errorMessage) {
+            $this->assertArrayHasKey($errorField, $decodedResponse['errors']);
+            $this->assertEquals($errorMessage, $decodedResponse['errors'][$errorField][0]);
+        }
+    }
+
+    /** @test */
+    public function the_external_id_should_be_unique_in_the_request_between_users()
+    {
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                'external_id' => "12345",
+                'name_first'  => "Jan",
+                'name_suffix' => "",
+                'name'        => "Janssen",
+                'username'    => "thisOne@hotmail.com",
+            ], [
+                'external_id' => "12345",
+                'name_first'  => "Janus",
+                'name_suffix' => "",
+                'name'        => "oepsi",
+                'username'    => "other@hotmail.com",
+            ]],
+        ]))->assertStatus(422);
+
+        $decodedResponse = $response->decodeResponseJson();
+        $requiredErrors = [
+            'data.0.external_id'   => 'Deze import bevat dubbele studentennummers',
+            'data.1.external_id'   => 'Deze import bevat dubbele studentennummers',
+        ];
+
+        foreach ($requiredErrors as $errorField => $errorMessage) {
+            $this->assertArrayHasKey($errorField, $decodedResponse['errors']);
+            $this->assertEquals($errorMessage, $decodedResponse['errors'][$errorField][0]);
+        }
+    }
+
+    /** @test */
+    public function when_importing_the_same_users_in_the_same_class_twice_it_does_not_fail_nor_does_it_enter_a_extra_student_and_or_user_record()
+    {
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                'external_id' => "12345",
+                'name_first'  => "Jan",
+                'name_suffix' => "",
+                'name'        => "Janssen",
+                'username'    => "thisOne@hotmail.com",
+            ], ],
+        ]))->assertSuccessful();
+
+        $beforeCountStudents = Student::count();
+        $beforeCountUsers = User::count();
+
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                'external_id' => "12345",
+                'name_first'  => "Jan",
+                'name_suffix' => "",
+                'name'        => "Janssen",
+                'username'    => "thisOne@hotmail.com",
+            ], ],
+        ]))->assertSuccessful();
+
+        $this->assertEquals($beforeCountStudents, Student::count());
+        $this->assertEquals($beforeCountUsers, User::count());
+    }
+
+    /** @test */
+    public function when_importing_the_same_users_in_a_different_class_it_does_not_fail_and_enter_a_extra_student_but_no_user_record()
+    {
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                'external_id' => "12345",
+                'name_first'  => "Jan",
+                'name_suffix' => "",
+                'name'        => "Janssen",
+                'username'    => "thisOne@hotmail.com",
+            ], ],
+        ]))->assertSuccessful();
+
+        $beforeCountStudents = Student::count();
+        $beforeCountUsers = User::count();
+
+        // move school_class with id=2 to school_location 3;
+        $schoolClassTwo = SchoolClass::find(2);
+        $schoolClassTwo->school_location_id = 3;
+        $schoolClassTwo->save();
+
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(2)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                'external_id' => "12345",
+                'name_first'  => "Jan",
+                'name_suffix' => "",
+                'name'        => "Janssen",
+                'username'    => "thisOne@hotmail.com",
+            ], ],
+        ]))->assertSuccessful();
+
+        $this->assertEquals(++$beforeCountStudents, Student::count());
+        $this->assertEquals($beforeCountUsers, User::count());
+    }
+
+
+    /** @test */
+    public function the_external_id_should_be_unique_for_this_email_address_in_this_school_location()
+    {
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                'external_id' => "12345",
+                'name_first'  => "Jan",
+                'name_suffix' => "",
+                'name'        => "Janssen",
+                'username'    => "thisOne@hotmail.com",
+            ], ],
+        ]))->assertSuccessful();
+
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [  [
+                'external_id' => "12345",
+                'name_first'  => "Janus",
+                'name_suffix' => "",
+                'name'        => "oepsi",
+                'username'    => "other@hotmail.com",
+            ], ],
+        ]))->assertStatus(422);
+
+        $decodedResponse = $response->decodeResponseJson();
+        $requiredErrors = [
+            'data.0.external_id'   => 'The data.0.external id has already been taken.',
+        ];
+
+        foreach ($requiredErrors as $errorField => $errorMessage) {
+            $this->assertArrayHasKey($errorField, $decodedResponse['errors']);
+            $this->assertEquals($errorMessage, $decodedResponse['errors'][$errorField][0]);
+        }
+    }
+
+    /** @test */
+    public function it_should_return_required_column_validation_errors_for_required_fields()
+    {
+        $response = $this->post(
+            route(
+                'school_classes.import', [
+                'schoolLocation' => SchoolLocation::find(3)->uuid,
+                'schoolClass'    => SchoolClass::find(3)->uuid,
+            ])
+            , static::getSchoolBeheerderAuthRequestData([
+            'data' => [[
+                "12345",
+                "Jan",
+                "",
+                "Janssen",
+                "carloschoep+K999jjanssen@hotmail.com",
+            ]],
+        ]))->assertStatus(422);
+        $decodedResponse = $response->decodeResponseJson();
+
+        $requiredErrors = [
+            'data.0.username'   => 'The data.0.username field is required.',
+            'data.0.name_first' => 'The data.0.name_first field is required.',
+            'data.0.name'       => 'The data.0.name field is required.',
+        ];
+
+        foreach ($requiredErrors as $errorField => $errorMessage) {
+            $this->assertArrayHasKey($errorField, $decodedResponse['errors']);
+            $this->assertEquals($errorMessage, $decodedResponse['errors'][$errorField][0]);
+        }
+    }
+}
