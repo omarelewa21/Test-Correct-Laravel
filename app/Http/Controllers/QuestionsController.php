6eb9896941b8d97adf54b07284741de9c448e2e5
Gert-Mark <info@gmwconsult.nl>
2020-11-03 16:53:02 +0000

    extra scope toegevoegd aan Question model QuestionController@index

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -24,7 +24,7 @@ class QuestionsController extends Controller {
     }
 
     public function index(Request $request) {
-        $questions = Question::filtered($request->get('filter', []), $request->get('order', []))
+        $questions = Question::opensourceAndDemo()->filtered($request->get('filter', []), $request->get('order', []))
             // don't show questions from the cito import
             ->where(function($query) {
                 $query->where('scope', '!=', 'cito') // should be in filtered, but can't be due to the way it is build starting with an or
