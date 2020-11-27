5faa4830a849b35a7480775c494c2c39c43152e6
Erik | Sobit <Erik@sobit.nl>
2020-11-27 16:33:53 +0100

    updated the details for restriction after gathering info from the testportal

--- app/Http/Middleware/CakeLaravelFilter.php
+++ app/Http/Middleware/CakeLaravelFilter.php
@@ -19,8 +19,8 @@ class CakeLaravelFilter
             return $next($request);
         } else {
             if($_SERVER['REMOTE_ADDR'] == '127.0.0.1'
-                && $_SERVER['SERVER_PORT'] === 81
-                && $_SERVER['SERVER_NAME'] == '127.0.0.1') {
+                && $_SERVER['SERVER_PORT'] === 80
+                && $_SERVER['SERVER_NAME'] == 'tc-dev.webbix.nl') {
                 return $next($request);
             } else {
                 abort(403,'Access denied');
