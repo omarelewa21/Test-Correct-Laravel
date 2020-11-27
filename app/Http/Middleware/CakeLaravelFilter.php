c55f606ddaeb5eb07d6a190b78a37010afdab532
Erik | Sobit <Erik@sobit.nl>
2020-11-27 11:16:58 +0100

    started with middleware

---
+++ app/Http/Middleware/CakeLaravelFilter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace tcCore\Http\Middleware;
+
+use Closure;
+
+class CakeLaravelFilter
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(config('cake_laravel_filter.skip') === true){
+            return $next($request);
+        } else {
+            if($_SERVER['ip'] == '127.0.0.1'
+                && $_SERVER['SERVER_PORT'] === 81
+                && $_SERVER['SERVER_NAME'] == '127.0.0.1') {
+                return $next($request);
+            } else {
+                abort(403,'Access denied');
+            }
+        }
+        return $next($request);
+    }
+}
---
+++ config/cake_laravel_filter.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Key
+    |--------------------------------------------------------------------------
+    |
+    | You can find your API key on your Bugsnag dashboard.
+    |
+    | This api key points the Bugsnag notifier to the project in your account
+    | which should receive your application's uncaught exceptions.
+    |
+    */
+
+    'skip' => env('CAKE_LARAVEL_FILTER_SKIP',false),
+
+    ];
