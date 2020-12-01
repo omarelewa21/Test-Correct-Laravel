901589ef101a88049ad21c05a55b88f71178433a
Martin Folkerts <m.folkerts@sobit.nl>
2020-12-01 11:47:11 +0100

    added school_location_id to scoped filtered of TestTake Model

---
+++ database/migrations/2020_12_01_090935_add_school_location_id_to_test_takes.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSchoolLocationIdToTestTakes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('test_takes', function (Blueprint $table) {
+            $table->unsignedInteger('school_location_id')->nullable();
+        });
+
+        DB::statement('
+                UPDATE test_takes set school_location_id = (select school_location_id from users where id = test_takes.user_id)
+            ');
+
+        Schema::table('test_takes', function (Blueprint $table) {
+            $table->foreign('school_location_id')->references('id')->on('school_locations');
+            $table->unsignedInteger('school_location_id')->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('test_takes', function (Blueprint $table) {
+            $table->dropForeign(['school_location_id']);
+            $table->dropColumn('school_location_id');
+        });
+    }
+}
