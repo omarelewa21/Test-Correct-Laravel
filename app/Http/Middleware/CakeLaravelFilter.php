01bf4e35753f9ef59bbbc66906ac61cffad34668
Erik | Sobit <Erik@sobit.nl>
2020-12-01 13:14:35 +0100

    make the filters flexible

--- app/Http/Middleware/CakeLaravelFilter.php
+++ app/Http/Middleware/CakeLaravelFilter.php
@@ -16,9 +16,9 @@ class CakeLaravelFilter
     public function handle($request, Closure $next)
     {
         if(config('cake_laravel_filter.skip') !== true){
-            if($_SERVER['REMOTE_ADDR'] == '127.0.0.1'
-                && $_SERVER['SERVER_PORT'] == '80'
-                && $_SERVER['SERVER_NAME'] == 'tc-dev.webbix.nl') {
+            if($_SERVER['REMOTE_ADDR'] == config('cake_laravel_filter.remote_addr')
+                && $_SERVER['SERVER_PORT'] == config('cake_laravel_filter.server_port')
+                && $_SERVER['SERVER_NAME'] == config('cake_laravel_filter.server_name')) {
                 return $next($request);
             } else {
                 abort(403,'Access denied');
