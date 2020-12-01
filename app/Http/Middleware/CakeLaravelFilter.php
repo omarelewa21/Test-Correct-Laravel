879df3570410e47410b2a1807453c970844174f7
Erik | Sobit <Erik@sobit.nl>
2020-12-01 11:43:22 +0100

    less strict port checking

--- app/Http/Middleware/CakeLaravelFilter.php
+++ app/Http/Middleware/CakeLaravelFilter.php
@@ -17,8 +17,8 @@ class CakeLaravelFilter
     {
         if(config('cake_laravel_filter.skip') !== true){
             if($_SERVER['REMOTE_ADDR'] == '127.0.0.1'
-                && $_SERVER['SERVER_PORT'] === 80
-                && $_SERVER['SERVER_NAME'] == 'tc-dev.webbix.nl') {
+                && $_SERVER['SERVER_PORT'] == '80'
+                && $_SERVER['SERVER_NAME'] == 'tc-dev.webbix.nltc-dev.webbix.nl') {
                 return $next($request);
             } else {
                 abort(403,'Access denied');
