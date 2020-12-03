ce0ee398990d6bc9d4eebcdd9a155bf94083c2ef
Erik | Sobit <Erik@sobit.nl>
2020-12-03 10:08:45 +0100

    added extra security by adding a security key to every call from cake to the backend

--- app/Http/Middleware/CakeLaravelFilter.php
+++ app/Http/Middleware/CakeLaravelFilter.php
@@ -15,10 +15,14 @@ class CakeLaravelFilter
      */
     public function handle($request, Closure $next)
     {
+
         if(config('cake_laravel_filter.skip') !== true){
+            $key = $request->header('CakeLaravelFilterKey',null);
+
             if($_SERVER['REMOTE_ADDR'] == config('cake_laravel_filter.remote_addr')
                 && $_SERVER['SERVER_PORT'] == config('cake_laravel_filter.server_port')
-                && $_SERVER['SERVER_NAME'] == config('cake_laravel_filter.server_name')) {
+                && $_SERVER['SERVER_NAME'] == config('cake_laravel_filter.server_name')
+                && $key == config('cake_laravel_filter.key')) {
                 return $next($request);
             } else {
                 abort(403,'Access denied');
