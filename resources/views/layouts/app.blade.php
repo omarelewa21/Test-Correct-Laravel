2a43ea3e3762fff4ed58b775679488de2db06b21
roansobit <roan@sobit.nl>
2021-01-11 16:05:43 +0100

    Start header layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,26 @@
 <x-layouts.base>
+    <header class="header flex flex-wrap content-center fixed w-full z-10">
+        <a class="mr-4" href="#">
+            <img class="" src="/svg/logos/Logo-Test-Correct-2.svg"
+                 alt="Test-Correct">
+        </a>
+        <div class="user flex flex-wrap items-center ml-auto space-x-6">
+            <x-fraud-detected/>
 
+            <x-dropdown label="{{ Auth::user()->getNameFullAttribute() }}">
+                <x-dropdown.item>Inleveren</x-dropdown.item>
+            </x-dropdown>
+        </div>
+    </header>
+    <main class="flex-1 mx-28 m-foot-head">
+        {{ $slot }}
+    </main>
+    <footer class="footer flex content-center fixed w-full bottom-0 z-10">
+        <div class="flex items-center ml-auto space-x-6">
+            {{ $footerbuttons }}
 
-            <main class="flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6" tabindex="0" x-data="" x-init="$el.focus()">
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                    {{ $slot }}
-                </div>
-            </main>
+        </div>
+    </footer>
 
+    {{--    <x-notification></x-notification>--}}
 </x-layouts.base>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,12 +3,14 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
+    <title>Test-Correct</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
     <!-- Alpine -->
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
     @livewireStyles
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body class="antialiased font-sans bg-gray-200">
 {{ $slot }}
