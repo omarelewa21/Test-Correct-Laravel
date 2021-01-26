87f621afe4565a5038e5ee9dfc77a773b1c5a608
Erik | Sobit <Erik@sobit.nl>
2021-01-26 10:14:19 +0100

    refactored migration with unguard and reguard

--- database/migrations/2021_01_24_130953_add_electron_test_take_event_types.php
+++ database/migrations/2021_01_24_130953_add_electron_test_take_event_types.php
@@ -13,24 +13,31 @@ class AddElectronTestTakeEventTypes extends Migration
      *
      * @return void
      */
-    private static function createAndAdd($name, $confirm, $reason) {
-        TestTakeEventType::create(['name' => $name]);
-        DB::table('test_take_event_types')->where('name', '=', $name)->update(['requires_confirming' => $confirm, 'reason' => $reason]);
+    private static function createAndAdd($id, $name, $confirm, $reason) {
+        TestTakeEventType::create([
+            'id' => $id,
+            'name' => $name,
+            'requires_confirming' => $confirm,
+            'reason' => $reason
+        ]);
+//        DB::table('test_take_event_types')->where('name', '=', $name)->update(['requires_confirming' => $confirm, 'reason' => $reason]);
     }
 
     public function up()
     {
-        AddElectronTestTakeEventTypes::createAndAdd('Pressed meta key', 1, 'before-input-meta');
-        AddElectronTestTakeEventTypes::createAndAdd('Pressed alt key', 1, 'before-input-alt');
-        AddElectronTestTakeEventTypes::createAndAdd('Application closed alt+f4', 1, 'alt+f4');
-        AddElectronTestTakeEventTypes::createAndAdd('Lost focus blur', 1, 'blur');
-        AddElectronTestTakeEventTypes::createAndAdd('Window hidden', 1, 'hide');
-        AddElectronTestTakeEventTypes::createAndAdd('Window minimized', 1, 'minimize');
-        AddElectronTestTakeEventTypes::createAndAdd('Window moved', 1, 'move');
-        AddElectronTestTakeEventTypes::createAndAdd('Window not fullscreen', 1, 'leave-full-screen');
-        AddElectronTestTakeEventTypes::createAndAdd('Always on top changed', 1, 'always-on-top-changed');
-        AddElectronTestTakeEventTypes::createAndAdd('Window resized', 1, 'resize');
-        AddElectronTestTakeEventTypes::createAndAdd('Force shutdown', 1, 'session-end');
+        TestTakeEventType::unguard();
+        AddElectronTestTakeEventTypes::createAndAdd(11, 'Pressed meta key', 1, 'before-input-meta');
+        AddElectronTestTakeEventTypes::createAndAdd(12, 'Pressed alt key', 1, 'before-input-alt');
+        AddElectronTestTakeEventTypes::createAndAdd(13,'Application closed alt+f4', 1, 'alt+f4');
+        AddElectronTestTakeEventTypes::createAndAdd(14,'Lost focus blur', 1, 'blur');
+        AddElectronTestTakeEventTypes::createAndAdd(15, 'Window hidden', 1, 'hide');
+        AddElectronTestTakeEventTypes::createAndAdd(16, 'Window minimized', 1, 'minimize');
+        AddElectronTestTakeEventTypes::createAndAdd(17,'Window moved', 1, 'move');
+        AddElectronTestTakeEventTypes::createAndAdd(18, 'Window not fullscreen', 1, 'leave-full-screen');
+        AddElectronTestTakeEventTypes::createAndAdd(19, 'Always on top changed', 1, 'always-on-top-changed');
+        AddElectronTestTakeEventTypes::createAndAdd(20, 'Window resized', 1, 'resize');
+        AddElectronTestTakeEventTypes::createAndAdd(21, 'Force shutdown', 1, 'session-end');
+        TestTakeEventType::reguard();
     }
 
     /**
