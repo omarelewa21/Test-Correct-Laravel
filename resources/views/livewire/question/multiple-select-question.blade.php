9a0aa3a1898898b5deb6a144c2be2d78edbac622
roansobit <roan@sobit.nl>
2021-02-02 11:24:10 +0100

    wip

--- resources/views/livewire/question/multiple-select-question.blade.php
+++ resources/views/livewire/question/multiple-select-question.blade.php
@@ -6,35 +6,23 @@
             @foreach( $question->multipleChoiceQuestionAnswers as $link)
                 <div class="flex items-center mc-radio">
                     <label
-                        for="link{{ $link->id }}"
-                        class="
-                         relative
-                         w-full
-                        flex
-                        hover:font-bold
-                        p-5 border
-                        border-blue-grey
-                        rounded-10
-                        base
-                        multiple-choice-question
-                        transition
-                        ease-in-out duration-150
-                        focus:outline-none
-                        justify-between
-                        {!! ($this->answer == $link->id) ? 'active' :'' !!}
-                            "
+                            for="link{{ $link->id }}"
+                            class="relative w-full flex hover:font-bold p-5 border-2 border-blue-grey rounded-10 base
+                            multiple-choice-question transition ease-in-out duration-150 focus:outline-none
+                            justify-between {!! ($this->answer == $link->id) ? 'active' :'' !!}"
                     >
                         <input
-                            wire:model="answer"
-                            id="link{{ $link->id }}"
-                            name="Question_{{ $question->id }}"
-                            type="radio"
-                            class="hidden"
-                            value="{{ $link->id }}"
+                                wire:model="answer"
+                                id="link{{ $link->id }}"
+                                name="Question_{{ $question->id }}"
+                                type="radio"
+                                class="hidden"
+                                value="{{ $link->id }}"
                         >
                         <div>{!! $link->answer !!}</div>
                         <div class="{!! ($this->answer == $link->id) ? '' :'hidden' !!}">
-                            <x-icon.checkmark></x-icon.checkmark>
+                            <x-icon.checkmark class="checkmark transition"></x-icon.checkmark>
+                            <x-icon.close class="w-4 h-4 close transition"/>
                         </div>
                     </label>
                 </div>
