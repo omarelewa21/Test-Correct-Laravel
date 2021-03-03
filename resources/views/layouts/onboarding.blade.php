5bdd0e49417e1909eac6de8121d5d1726198db90
Martin Folkerts <m.folkerts@sobit.nl>
2021-03-03 14:06:50 +0100

    build version for tagmanager for testing (other branch is for master!!!)

--- resources/views/layouts/onboarding.blade.php
+++ resources/views/layouts/onboarding.blade.php
@@ -15,11 +15,35 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-156194835-1"></script>
     @livewireStyles
     <link rel="stylesheet" href="/css/onboarding.css">
+    <script>
+        function runTagManager(){
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-592V9J3');
+        }
+
+
+        // runTagManager();
+    </script>
 
 </head>
 <body class="antialiased font-sans bg-light-grey">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-592V9J3"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 {{ $slot }}
 
 @livewireScripts
+
+<script>
+    window.addEventListener('onboarding-step-updated', function(event) {
+        runTagManager();
+    });
+
+    runTagManager();
+</script>
 </body>
 </html>
