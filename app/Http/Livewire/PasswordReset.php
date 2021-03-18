c8dadb7a6afcdca6da753ddd05b4456e8c81774d
Martin Folkerts <m.folkerts@sobit.nl>
2021-03-18 19:29:57 +0100

    first version without send teacher

---
+++ app/Http/Livewire/PasswordReset.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace tcCore\Http\Livewire;
+
+use Illuminate\Auth\Passwords\PasswordBroker;
+use Illuminate\Support\Facades\Password;
+use Livewire\Component;
+use tcCore\User;
+
+class PasswordReset extends Component
+{
+    public $password;
+    public $password_confirmation;
+    public $username;
+    public $token;
+
+    public $showSuccessModal = true;
+
+    public $btnDisabled = false;
+
+    protected $queryString = ['token'];
+
+    protected $messages = [
+        'password.required' => 'Wachtwoord is verplicht',
+        'password.min'      => 'Wachtwoord moet langer zijn dan 8 karakters',
+        'password.regex'    => 'Wachtwoord voldoet niet aan de eisen',
+        'password.same'     => 'Wachtwoord komt niet overeen',
+    ];
+
+
+    public function getMinCharRuleProperty()
+    {
+        if (empty($this->password)) {
+            return 0;
+        } else {
+            return mb_strlen($this->password) < 8 ? false : true;
+        }
+    }
+
+    public function getMinDigitRuleProperty()
+    {
+        if (empty($this->password)) {
+            return 0;
+        } else {
+            return preg_match('/\d/', $this->password) ? true : false;
+        }
+    }
+
+    public function getSpecialCharRuleProperty()
+    {
+        if (empty($this->password)) {
+            return 0;
+        } else {
+            return preg_match('/[^a-zA-Z\d]/', $this->password) ? true : false;
+        }
+    }
+
+    public function rules()
+    {
+        return [
+            'username' => 'required|email',
+            'password' => 'required|min:8|regex:/\d/|regex:/[^a-zA-Z\d]/|same:password_confirmation',
+            'token'    => 'required',
+        ];
+    }
+
+
+    public function resetPassword()
+    {
+        $this->validate();
+
+        $credentials = [
+            'password_confirmation' => $this->password_confirmation,
+            'password'              => $this->password,
+            'token'                 => $this->token,
+            'username'              => $this->username,
+        ];
+
+
+        $response = Password::reset($credentials, function ($user, $password) {
+            $user->password = bcrypt($password);
+            $user->save();
+        });
+
+        if ($response === PasswordBroker::PASSWORD_RESET){
+            $this->showSuccessModal = true;
+        }
+
+        if ($response === PasswordBroker::INVALID_USER) {
+            $this->addError('password', 'Het opgegeven emailadres is niet correct');
+        };
+
+        if ($response === PasswordBroker::INVALID_TOKEN) {
+                $this->addError('password', 'De gebruikte link niet correct, of verlopen');
+        }
+    }
+
+    public function redirectToLogin()
+    {
+        $this->redirect(config('app.url_login'));
+    }
+
+
+    public function render()
+    {
+        return view('livewire.password-reset')->layout('layouts.onboarding');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::get('/user/confirm_email/{EmailConfirmation}', 'tcCore\Http\Controllers\U
 Route::get('/inv/{shortcode}','tcCore\Http\Controllers\Api\ShortCodeController@registerClickAndRedirect');
 Route::get('/', tcCore\Http\Livewire\Onboarding::class);
 
+Route::get('/password-reset', tcCore\Http\Livewire\PasswordReset::class)->name('password.reset');
+
 Route::middleware('auth')->prefix('student')->name('student.')->group(function () {
     Route::get('/test-take/{test_take}', tcCore\Http\Livewire\Student\TestTake::class)->name('test-take');
     Route::get('/test-take-stub/{test_take}', tcCore\Http\Livewire\Student\TesttakeStub::class)->name('test-take-stub');
