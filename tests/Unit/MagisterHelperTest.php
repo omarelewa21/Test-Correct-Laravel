f78181ecbdf33060c29800303648c092bf0b0ad3
Martin Folkerts <m.folkerts@sobit.nl>
2021-03-29 13:25:45 +0200

    updates

---
+++ tests/Unit/MagisterHelperTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit;
+
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Str;
+use tcCore\EducationLevel;
+use tcCore\Http\Helpers\DemoHelper;
+use tcCore\Http\Helpers\MagisterHelper;
+use tcCore\Lib\Repositories\SchoolYearRepository;
+use tcCore\OnboardingWizard;
+use tcCore\OnboardingWizardStep;
+use tcCore\OnboardingWizardUserStep;
+use tcCore\Period;
+use tcCore\SchoolClass;
+use tcCore\SchoolLocation;
+use tcCore\Section;
+use tcCore\Shortcode;
+use tcCore\Subject;
+use tcCore\Teacher;
+use tcCore\Test;
+use tcCore\TestTake;
+use tcCore\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Tests\Unit\Http\Helpers\DemoHelperTestHelper;
+use Tests\Unit\Http\Helpers\OnboardingTestHelper;
+
+class MagisterHelperTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function test_guzzle()
+    {
+        $string = MagisterHelper::guzzle();
+
+
+
+        dd($string);
+
+    }
+
+}
