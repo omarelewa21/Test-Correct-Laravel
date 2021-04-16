49b4ae94708ee2855d3446f8276a7f660552dd9b
Erik | Sobit <Erik@sobit.nl>
2021-04-16 11:35:54 +0200

    set hostname for bugsnag

--- config/bugsnag.php
+++ config/bugsnag.php
@@ -6,12 +6,30 @@ class AppVersionGetter {
 
     private static $releasesPath  = "../../.dep/releases";
 
+    protected static $serverList = [
+        '10.233.253.122' => 'web02',
+        '10.233.253.124' => 'web03',
+        '10.233.169.175' => 'web04',
+        '10.233.169.176' => 'web05',
+        '10.233.169.177' => 'web06',
+        '10.233.169.178' => 'web07',
+    ];
+
     public static $defaultFilters = [
         'api_key', 'session_hash', 'main_address', 'main_city', 'main_country', 'main_postal', 
         'invoice_address', 'visit_address', 'visit_postal', 'visit_city', 'visit_country', 
         'username', 'name_first', 'name_suffix', 'abbreviation', 'password', 'wachtwoord',
         'cakelaravelfilterkey', 'cookie', 'name', 'user'
     ];
+
+    public static function getServer()
+    {
+        $ip = $_SERVER["SERVER_ADDR"];
+        if(in_array($ip,self::serverList)){
+            return self::$serverList[$ip];
+        }
+        return $ip;
+    }
 
     public static function configureAppversion() {
         if (!file_exists(self::$releasesPath)) {
@@ -83,12 +101,13 @@ class AppVersionGetter {
 		return trim($output);
 
 	}
-
 }
 
 $appVersion = AppVersionGetter::configureAppversion();
 
 $filters = empty(env('BUGSNAG_FILTERS')) ? AppVersionGetter::$defaultFilters : array_merge(explode(',', str_replace(' ', '', env('BUGSNAG_FILTERS'))), AppVersionGetter::$defaultFilters);
+
+$server = AppVersionGetter::getServer();
 
 return [
 
@@ -179,7 +198,7 @@ return [
     |
     */
 
-    'hostname' => env('BUGSNAG_HOSTNAME'),
+    'hostname' => $server,
 
     /*
     |--------------------------------------------------------------------------
