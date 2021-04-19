6f854c3ec450921fb16123513d4a62dad90e28ba
Erik | Sobit <Erik@sobit.nl>
2021-04-19 14:00:59 +0200

    removed logger lines and added timestamp to call to frontend in order to make sure it is going to do it without cache

--- app/Deployment.php
+++ app/Deployment.php
@@ -32,41 +32,41 @@ class Deployment extends Model
 
     public function handleIfNeeded($oldStatus) : void
     {
-        logger('Deployment: handle if needed');
+//        logger('Deployment: handle if needed');
         if($oldStatus !== $this->status){
-            logger('Deployment: status changed to '.$this->status);
+//            logger('Deployment: status changed to '.$this->status);
             if($this->status === static::ACTIVE){
-                logger('Deployment: show maintenance should be set');
+//                logger('Deployment: show maintenance should be set');
                 $this->showMaintenance();
             }
             else if($oldStatus === static::ACTIVE){
-                logger('Deployment: remove maintenance should be set');
+//                logger('Deployment: remove maintenance should be set');
                 $this->removeMaintenance();
             }
-            logger('Deployment: do a portal call in order to set the notification');
+//            logger('Deployment: do a portal call in order to set the notification');
             $this->callCakeForMaintenanceCheck();
         }
     }
 
     public function showMaintenance()
     {
-        logger('Deployment: ready to set mainteanance');
+//        logger('Deployment: ready to set mainteanance');
         // nothing to do on the laravel side as this is based on an ACTIVE status
     }
 
     protected function removeMaintenance()
     {
-        logger('Deployment: ready to remove maintenance');
+//        logger('Deployment: ready to remove maintenance');
         // nothing to do on the laravel side as this is based on an ACTIVE status or not
     }
 
     protected function callCakeForMaintenanceCheck()
     {
-        logger('Deployment: ready to call Cake');
-        $url = sprintf('%sdeployment_maintenance/check_for_maintenance',config('app.url_login'));
-        logger('Deployment: url to call '.$url);
+//        logger('Deployment: ready to call Cake');
+        $url = sprintf('%sdeployment_maintenance/check_for_maintenance?%d',config('app.url_login'),date("YmdHis"));
+//        logger('Deployment: url to call '.$url);
         $response = file_get_contents($url);
-        logger('Deployment: response from url ');
-        logger($response);
+//        logger('Deployment: response from url ');
+//        logger($response);
     }
 }
