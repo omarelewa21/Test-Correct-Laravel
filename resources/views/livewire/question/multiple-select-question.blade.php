81b2d9ad2b89ed1e9d64df629629c046a9f751fe
roansobit <roan@sobit.nl>
2021-04-23 13:19:52 +0200

    Added media query to fix hover issue in ipad safari

--- resources/views/livewire/question/multiple-select-question.blade.php
+++ resources/views/livewire/question/multiple-select-question.blade.php
@@ -6,7 +6,7 @@
                 <div id="mc-s-{{$value}}" wire:key="mc-s-{{$value}}" class="flex items-center mc-radio">
                     <label id="mc_s_label_{{$value}}" wire:key="mc_s_label_{{$value}}"
                             for="link{{ $value }}"
-                            class="relative w-full flex hover:font-bold p-5 border-2 border-blue-grey rounded-10 base
+                            class="relative w-full flex p-5 border-2 border-blue-grey rounded-10 base
                             multiple-choice-question transition ease-in-out duration-150 focus:outline-none
                             justify-between cursor-pointer {!! ($this->answerStruct[$value] == 1) ? 'active' :'' !!}"
                     >
