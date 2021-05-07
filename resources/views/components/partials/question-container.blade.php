68ceaf3c0f7dac4de93bd9e31c33a5c0808f7b0d
Jonathan <jonathanjagt@teachandlearncompany.com>
2021-05-07 12:08:20 +0200

    Add initial selids for Selection question support
    This is related to SEL-69 and commit
    d424aad32830c5535b2a87d8e3098eba17e06bc9

--- resources/views/components/partials/question-container.blade.php
+++ resources/views/components/partials/question-container.blade.php
@@ -68,7 +68,7 @@
                 <x-icon.locked class="ml-2"/>
             @endif
 
-            <h1 class="inline-block ml-2 mr-6">{!! __($question->caption) !!}</h1>
+            <h1 class="inline-block ml-2 mr-6" selid="questiontitle">{!! __($question->caption) !!}</h1>
 
             @if ($question->score > 0)
                 <h4 class="inline-block">{{ $question->score }} pt</h4>
