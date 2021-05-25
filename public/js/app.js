f73da22d1d9b4165f73cb5d0354f0f47ce09150e
roansobit <roan@sobit.nl>
2021-05-25 17:13:55 +0200

    Move component JS to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3828,6 +3828,91 @@ makeHeaderMenuActive = function makeHeaderMenuActive(elementId) {
   document.getElementById(elementId).classList.add('active');
 };
 
+parent.skip = false;
+var notifsent = false;
+var lastLostFocus = {
+  notification: false,
+  delay: 3 * 60,
+  reported: {}
+};
+var alert = false;
+var checkFocusTimer = false;
+var Notify = {
+  notify: function notify(message, initialType) {
+    var type = initialType ? initialType : 'info';
+    window.dispatchEvent(new CustomEvent('notify', {
+      detail: {
+        message: message,
+        type: type
+      }
+    }));
+  }
+};
+
+function runCheckFocus() {
+  if (!checkFocusTimer) {
+    checkFocusTimer = setInterval(checkPageFocus, 300);
+  }
+}
+
+runCheckFocus();
+
+function checkPageFocus() {
+  if (!parent.skip) {
+    if (!document.hasFocus()) {
+      if (!notifsent) {
+        // checks for the notifcation if it is already sent to the teacher
+        console.log('lost focus from checkPageFocus');
+        Core.lostFocus('lost-focus');
+        notifsent = true;
+      }
+    } else {
+      notifsent = false; //mark it as not sent, to active it again
+    }
+  } else {
+    window.focus(); //we need to set focus back to the window before changing skip value
+
+    parent.skip = false;
+  }
+}
+
+function shouldLostFocusBeReported(reason) {
+  if (reason == null) {
+    reason == "undefined";
+  }
+
+  if (!(reason in lastLostFocus.reported) || !alert) {
+    lastLostFocus.reported[reason] = new Date().getTime() / 1000;
+    return true;
+  }
+
+  var now = new Date().getTime() / 1000;
+  var lastTime = lastLostFocus.reported[reason];
+
+  if (lastTime <= now - lastLostFocus.delay) {
+    lastLostFocus.reported[reason] = new Date().getTime() / 1000;
+    return true;
+  }
+
+  return false;
+}
+
+var Core = {
+  lostFocus: function lostFocus(reason) {
+    if (reason == "printscreen") {
+      Notify.notify('Het is niet toegestaan om een screenshot te maken, we hebben je docent hierover geïnformeerd', 'error');
+    } else {
+      Notify.notify('Het is niet toegestaan om uit de app te gaan', 'error');
+    }
+
+    if (shouldLostFocusBeReported(reason)) {
+      livewire.find(document.querySelector('[testtakemanager]').getAttribute('wire:id')).call('createTestTakeEvent', reason);
+    }
+
+    alert = true;
+  }
+};
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
