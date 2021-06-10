6c252f1920e65074eaa1d2eb3e327570afec8f2f
Erik | Sobit <Erik@sobit.nl>
2021-06-10 10:04:14 +0200

    make sure deleted users, teachers, subjects are allowed for the list of authors to collect

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -14,9 +14,9 @@ class AuthorsController extends Controller
 {
     public function index(Request $request)
     {
-        $builder = User::whereIn('id', // find all users part of this selection
-            Teacher::with('user')->whereIn('subject_id', // find the teachers with these subjects
-                Subject::whereIn('section_id', // get all subjects belonging to the section memberships
+        $builder = User::withTrashed()->whereIn('id', // find all users part of this selection
+            Teacher::with('user')->withTrashed()->whereIn('subject_id', // find the teachers with these subjects
+                Subject::withTrashed()->whereIn('section_id', // get all subjects belonging to the section memberships
                     Auth::user()->sections()->select('sections.id')->where('demo',0) // get section memberships of this teacher where section is not part of the demo environment
                 )->select('subjects.id')
             )->select('teachers.user_id')
