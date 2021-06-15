74d697dd355165d63def1ecdf675d38486cbd3b4
Mohamed Elsayed <MohamedElsayed96@bitbucket.org>
2021-06-15 13:21:34 +0200

    fixing new player

--- resources/views/components/attachment/attachments-button.blade.php
+++ resources/views/components/attachment/attachments-button.blade.php
@@ -1,5 +1,4 @@
-{{ __("@if(!$question->attachments->isEmpty())
-") }}@if(!$question->attachments->isEmpty())
+@if(!$question->attachments->isEmpty())
     @foreach($question->attachments as $attachment)
         <x-button.text-button class="mb-4" wire:click="showAttachment({{ $attachment->getKey() }})" wire:key="attachment-{{$attachment->getKey()}}">
             <x-icon.attachment/>
