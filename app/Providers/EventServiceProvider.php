13483255db0d6281b731b641d89abcc5f11eefa6
Martin Folkerts <m.folkerts@sobit.nl>
2021-06-15 14:10:11 +0200

    added dd in provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -44,6 +44,11 @@ class EventServiceProvider extends ServiceProvider {
                     exit;
                 } else {
                     Session::flash('saml_attributes', $user->getAttributes());
+                    dd([
+                        'saml' =>  $user->getAttributes(),
+                        'route' => route('auth.login', ['tab' => 'entree']),
+                    ]);
+
                     return redirect(route('auth.login', ['tab' => 'entree']));
                 }
             }
