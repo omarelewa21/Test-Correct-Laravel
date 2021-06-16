20235ed9732e7d3ba20666cc7c87f6df3723aedf
roansobit <roan@sobit.nl>
2021-06-16 13:06:23 +0200

    Added extra check for confirmed testTakeEvents

--- resources/js/app.js
+++ resources/js/app.js
@@ -79,6 +79,8 @@ function shouldLostFocusBeReported(reason) {
         return true;
     }
 
+    window.Livewire.emit('checkConfirmedEvents', reason);
+
     return false;
 }
 
@@ -89,6 +91,8 @@ Core = {
         } else {
             Notify.notify('Het is niet toegestaan om uit de app te gaan', 'error');
         }
+
+        window.Livewire.emit('setFraudDetected');
 
         if (shouldLostFocusBeReported(reason)) {
             livewire.find(document.querySelector('[testtakemanager]').getAttribute('wire:id')).call('createTestTakeEvent', reason);
