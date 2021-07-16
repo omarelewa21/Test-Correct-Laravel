9b28ed144e5d2efc0bd8fc9b3a19815fb0ea9f4b
Erik | Sobit <Erik@sobit.nl>
2021-07-16 16:38:57 +0200

    add notice if lvs section is missing & only check for redirect to entree if teacher or student

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -102,7 +102,7 @@ class Login extends Component
             return $this->addError('invalid_user', __('auth.failed'));
         }
 
-        if(EntreeHelper::shouldPromptForEntree(auth()->user())) {
+        if((Auth()->user()->isA('teacher') || Auth()->user()->isA('student')) && EntreeHelper::shouldPromptForEntree(auth()->user())) {
             auth()->logout();
             return $this->addError('should_first_go_to_entree', __('auth.should_first_login_using_entree'));
         }
