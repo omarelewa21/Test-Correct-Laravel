6b647912d7bcda1353201465c7603868f5a25757
Erik | Sobit <Erik@sobit.nl>
2021-07-21 10:25:55 +0200

    show attributes

--- app/Http/Helpers/EntreeHelper.php
+++ app/Http/Helpers/EntreeHelper.php
@@ -132,10 +132,14 @@ class EntreeHelper
     private function validateAttributes()
     {
         if (!array_key_exists('eckId', $this->attr) || !array_key_exists(0, $this->attr['eckId'])) {
+            logger('attributes');
+            logger($this->attr);
             throw new \Exception('no eckId found in saml request');
         }
 
         if (!array_key_exists('mail', $this->attr) || !array_key_exists(0, $this->attr['mail'])) {
+            logger('attributes');
+            logger($this->attr);
             throw new \Exception('no mail found in saml request');
         }
     }
