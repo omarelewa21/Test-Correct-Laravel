f2195979f87e6a50f38be68c5dab3c186325cb59
roansobit <roan@sobit.nl>
2021-08-16 08:59:02 +0200

    Added handleInactiveStudent actions

--- app/Http/Livewire/Student/TestTake.php
+++ app/Http/Livewire/Student/TestTake.php
@@ -110,6 +110,14 @@ class TestTake extends Component
 
     public function handleInactiveStudent()
     {
+        $testParticipant = TestParticipant::whereId($this->testParticipantId)->first();
+        $testParticipant->handInTestTake();
+        $this->createTestTakeEvent('stop-test');
 
+        Auth::logout();
+        session()->invalidate();
+        session()->regenerateToken();
+
+        $this->redirect(config('app.url_login'));
     }
 }
