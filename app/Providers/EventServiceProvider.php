bd86c883b688045c5c2d48162b5cb5631bf1715f
Erik | Sobit <Erik@sobit.nl>
2021-09-30 12:06:57 +0200

    fixed typo

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -39,7 +39,7 @@ class EventServiceProvider extends ServiceProvider {
 
             $entreeHelper->blockIfReplayAttackDetected();
 
-            $this->blockIfEckIdAttributeIsNotPresent();
+            $entreeHelper->blockIfEckIdAttributeIsNotPresent();
 
             $entreeHelper->redirectIfBrinUnknown();
 
