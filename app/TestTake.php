686ec1920115855ffed11dae4f414823372854ce
Martin Folkerts <m.folkerts@sobit.nl>
2021-10-22 18:55:15 +0200

    refactored introduced withoutDemotTeacherForUser

--- app/TestTake.php
+++ app/TestTake.php
@@ -480,18 +480,9 @@ class TestTake extends BaseModel
             });
         } elseif (in_array('Teacher', $roles)) {
             $user = Auth::user();
-            $query->accessForTeacher(Auth::user());
-
-
+            $query->accessForTeacher($user);
             // don't show demo tests from other teachers
-
-            $query->where(function($query) use ($user) {
-                $query->where(function($query) use ($user) {
-                    $query->where($this->getTable().'.demo',1)
-                        ->where($this->getTable().'.user_id',$user->getKey());
-                })
-                    ->orWhere($this->getTable().'.demo',0);
-            });
+            $query->withoutDemoTeacherForUser($user);
 
             // TC-158 only show testtakes from tests from other subjects or if demo subject dan ook zelf de eigenaar
             $query->where(function($q) use ($user){
@@ -532,13 +523,13 @@ class TestTake extends BaseModel
             });
         }
 
-        $query->where($this->getTable().'.school_location_id', Auth::user()->school_location_id);
+        $query->belongsToSchoolLocation(Auth::user());
+        //$query->where($this->getTable().'.school_location_id', Auth::user()->school_location_id);
 
         $testTable = with(new Test())->getTable();
         $query->select($this->getTable() . '.*')
             ->join($testTable, $testTable . '.id', '=', $this->getTable() . '.test_id');
-        // 20200207 MF t zou kunnen dat er een kopie van een test wordt gemaakt voordat een test_take wordt gescheduled maar dat weet ik niet zeker, maar any how het zou niet nodig hoeven zijn dat test niet deleted is.
-        // ->where($testTable . '.' . with(new Test())->getDeletedAtColumn(), null);
+
         foreach ($filters as $key => $value) {
             switch ($key) {
                 case 'user_id':
@@ -874,6 +865,20 @@ class TestTake extends BaseModel
                 ->orUserIsCreatorScope($query, $user)
                 ->orUserIsInvigilatorScope($query, $user)
                 ->orUserHasAccessToSchoolClassParticipantsAndSubjectScope($query, $user);
+        });
+    }
+
+    public function scopeBelongsToSchoolLocation($query, User $user) {
+        $query->where($this->getTable().'.school_location_id', $user->school_location_id);
+    }
+
+    public function scopeWithoutDemoTeacherForUser($query, User $user)
+    {
+        $query->where(function ($query) use ($user) {
+            $query->where(function ($query) use ($user) {
+                $query->where($this->getTable().'.demo', 1)
+                    ->where($this->getTable().'.user_id', $user->getKey());
+            })->orWhere($this->getTable().'.demo', 0);
         });
     }
 }
