9f305a29a1a5e5c73034c0d9497de461e5b9f102
Martin Folkerts <m.folkerts@sobit.nl>
2021-11-10 16:24:54 +0100

    new version of surveilanceController

--- app/Http/Controllers/SurveillanceController.php
+++ app/Http/Controllers/SurveillanceController.php
@@ -27,7 +27,6 @@ class SurveillanceController extends Controller
     private $ipCheck = false;
     private $eventIdsThatRequireConfirming = null;
 
-
     public function index()
     {
         if (!Auth::user()->isA(['teacher', 'invigilator'])) {
@@ -70,103 +69,48 @@ class SurveillanceController extends Controller
             ->where('requires_confirming', '1')
             ->groupBy('test_participant_id');
 
-        return TestTake::query()
-            ->select(
-                'test_takes.id as id',
-                'test_takes.uuid as uuid',
-                'tests.name as test_name'
-            )
-            ->withoutGlobalScope(ArchivedScope::class)
-            ->join('invigilators', function ($query) use ($owner) {
-                return $query
-                    ->on('test_takes.id', 'invigilators.test_take_id')
-                    ->where(function ($query) use ($owner) {
-                        $query->where('invigilators.user_id', '=', $owner->id)
-                            ->orWhere('test_takes.user_id', '=', $owner->id);
-
-                    })->whereIn('test_takes.id', function ($query) use ($owner) {
-                        $currentSchoolYearId = SchoolYearRepository::getCurrentSchoolYear()->getKey();
-                        $teacherTable = with((new Teacher)->getTable());
-                        $schoolClassTable = with((new SchoolClass())->getTable());
-                        $query->select('test_take_id')
-                            ->from(with(new TestParticipant())->getTable())
-                            ->whereNull('deleted_at')
-                            ->whereIn('school_class_id',
-                                function ($query) use ($teacherTable, $schoolClassTable, $currentSchoolYearId) {
-                                    $query->select('class_id')
-                                        ->from($teacherTable)
-                                        ->join($schoolClassTable, "$teacherTable.class_id", '=',
-                                            "$schoolClassTable.id")
-                                        ->where('user_id', Auth::id())
-                                        ->where('school_year_id', $currentSchoolYearId)
-                                        ->whereNull("$teacherTable.deleted_at")
-                                        ->whereNull("$schoolClassTable.deleted_at");
-                                })
-                            ->whereIn('test_takes.id',
-                                function ($query) use ($teacherTable, $schoolClassTable, $currentSchoolYearId) {
-                                    $testTable = with(new Test())->getTable();
-                                    $query
-                                        ->select('test_takes.id')
-                                        ->from('test_takes')
-                                        ->join($testTable, $testTable.'.id', '=', 'test_takes.test_id')
-                                        ->whereNull($testTable.'.deleted_at')
-                                        ->whereIn($testTable.'.subject_id', function ($query) use (
-                                            $teacherTable,
-                                            $schoolClassTable,
-                                            $currentSchoolYearId
-                                        ) {
-                                            $query->select('subject_id')
-                                                ->from($teacherTable)
-                                                ->join($schoolClassTable, "$teacherTable.class_id", '=',
-                                                    "$schoolClassTable.id")
-                                                ->where('user_id', Auth::id())
-                                                ->where('school_year_id', $currentSchoolYearId)
-                                                ->whereNull("$teacherTable.deleted_at")
-                                                ->whereNull("$schoolClassTable.deleted_at");
-                                        });
-                                });
-                    });
-            })
-            ->join('tests', 'test_takes.test_id', 'tests.id')
-            ->whereIn('test_takes.id', $this->getCachedTestTakeIds($owner))
-            ->where('test_take_status_id', '=', '3')
-            ->with([
-                'testParticipants' => function ($query) use ($participantHasEvents) {
-                    $query->select(
-                        'id',
-                        'test_take_id',
-                        'user_id',
-                        'test_take_status_id',
-                        'allow_inbrowser_testing',
-                        'test_take_status_id as status',
-                        'ip_address',
-                        'uuid',
-                        'school_class_id',
-                        DB::raw("case coalesce(has_events.event, 0) when 0 then 'false' else 'true' end as has_events")
-                    )->addSelect(
-                        [
-                            'answered_count' => function ($query) {
-                                $query->selectRaw('sum(score)')
-                                    ->from('answers')
-                                    ->join('questions', 'answers.question_id', '=', 'questions.id')
-                                    ->whereRaw('test_participant_id = test_participants.id and done=1');
-                            },
-                            'answers_total'  => function ($query) {
-                                $query->selectRaw('sum(score)')
-                                    ->from('answers')
-                                    ->join('questions', 'answers.question_id', '=', 'questions.id')
-                                    ->whereRaw('test_participant_id = test_participants.id');
-                            },
-                            'ip_check'       => function ($query) {
-                                $query = $this->getIpCheckQuery($query);
-                            }
-                        ]
-                    )->leftJoinSub($participantHasEvents, 'has_events', function ($join) {
-                        $join->on('test_participants.id', '=', 'has_events.test_participant_id');
-                    });
-                },
-            ])
-            ->get();
+        return TestTake::select('test_takes.id as id', 'test_takes.uuid as uuid', 'tests.name as test_name')
+        ->withoutGlobalScope(ArchivedScope::class)
+        ->join('tests', 'test_takes.test_id', 'tests.id')
+        ->whereIn('test_takes.id', $this->getCachedTestTakeIds($owner))
+        ->where('test_take_status_id', '=', '3')
+        ->with([
+            'testParticipants' => function ($query) use ($participantHasEvents) {
+                $query->select(
+                    'id',
+                    'test_take_id',
+                    'user_id',
+                    'test_take_status_id',
+                    'allow_inbrowser_testing',
+                    'test_take_status_id as status',
+                    'ip_address',
+                    'uuid',
+                    'school_class_id',
+                    DB::raw("case coalesce(has_events.event, 0) when 0 then 'false' else 'true' end as has_events")
+                )->addSelect(
+                    [
+                        'answered_count' => function ($query) {
+                            $query->selectRaw('sum(score)')
+                                ->from('answers')
+                                ->join('questions', 'answers.question_id', '=', 'questions.id')
+                                ->whereRaw('test_participant_id = test_participants.id and done=1');
+                        },
+                        'answers_total'  => function ($query) {
+                            $query->selectRaw('sum(score)')
+                                ->from('answers')
+                                ->join('questions', 'answers.question_id', '=', 'questions.id')
+                                ->whereRaw('test_participant_id = test_participants.id');
+                        },
+                        'ip_check'       => function ($query) {
+                            $query = $this->getIpCheckQuery($query);
+                        }
+                    ]
+                )->leftJoinSub($participantHasEvents, 'has_events', function ($join) {
+                    $join->on('test_participants.id', '=', 'has_events.test_participant_id');
+                });
+            },
+        ])
+        ->get();
     }
 
     private function transformForService(TestTake $testTake)
@@ -252,11 +196,7 @@ class SurveillanceController extends Controller
     private function getCachedTestTakeIds(User $owner)
     {
         $ids = cache()->remember('surveilence_data_'.$owner->uuid, now()->addSeconds(60), function () use ($owner) {
-            return TestTake::query()->where('test_takes.test_take_status_id', 3)
-                ->withoutDemoTeacherForUser($owner)
-                ->onlyTestsFromSubjectsOrIfDemoThenOnlyWhenOwner($owner)
-                ->pluck('id')
-                ->toArray();
+            return TestTake::filtered()->pluck('id');
         });
 
         return $ids;
