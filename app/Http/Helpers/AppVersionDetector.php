a487188d97fe32a6be58858deda3f3d540acf58e
Erik | Sobit <Erik@sobit.nl>
2021-11-19 08:30:54 +0100

    add macosElectron app 3.1.0 as needs update

--- app/Http/Helpers/AppVersionDetector.php
+++ app/Http/Helpers/AppVersionDetector.php
@@ -223,9 +223,10 @@ class AppVersionDetector
                 "3.2.0-beta.5",
             ],
             "needsUpdate" => [
-
+                "3.1.0"
             ],
             "needsUpdateDeadline" => [
+                "3.1.0" => "12 december 2021",
             ],
         ]
     ];
