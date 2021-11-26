67b58ca65032346f7293ddccc4c71c07122a81ff
roansobit <roan@sobit.nl>
2021-11-26 14:52:06 +0100

    only show version tag if version != x

--- resources/views/components/version-badge.blade.php
+++ resources/views/components/version-badge.blade.php
@@ -1,4 +1,4 @@
-@if(!session()->get('isInBrowser'))
+@if(!session()->get('isInBrowser') && $version != 'x')
     <span class="text-xs absolute min-w-max bottom-0 left-[60px] {{ $status }}">
         {{ __('student.version') }}: {{ $version  }}
     </span>
