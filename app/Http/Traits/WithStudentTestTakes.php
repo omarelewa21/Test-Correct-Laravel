269c198701d52fc5f349f4bb455dc485e94e20bb
roansobit <roan@sobit.nl>
2021-12-13 08:35:46 +0100

    merged trait

--- app/Http/Traits/WithStudentTestTakes.php
+++ app/Http/Traits/WithStudentTestTakes.php
@@ -5,7 +5,9 @@ namespace tcCore\Http\Traits;
 
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use tcCore\TestKind;
 use tcCore\TestParticipant;
 use tcCore\TestTakeStatus;
 use tcCore\TestTake;
@@ -19,21 +21,60 @@ trait WithStudentTestTakes
             return TestTake::leftJoin('test_participants', 'test_participants.test_take_id', '=', 'test_takes.id')
                 ->leftJoin('tests', 'tests.id', '=', 'test_takes.test_id')
                 ->leftJoin('subjects', 'subjects.id', '=', 'tests.subject_id')
-                ->select('test_takes.*', 'tests.name as test_name', 'tests.question_count', 'subjects.name as subject_name')
+                ->select(
+                    'test_takes.*',
+                    'tests.name as test_name',
+                    'tests.question_count',
+                    'subjects.name as subject_name',
+                    DB::raw(
+                        sprintf(
+                            "case when tests.test_kind_id = %d then 'true' else 'false' end as is_assignment",
+                            TestKind::ASSESSMENT_TYPE
+                        )
+                    )
+                )
                 ->where('test_participants.user_id', Auth::id())
                 ->where('test_takes.test_take_status_id', '<=', TestTakeStatus::STATUS_TAKING_TEST)
-                ->where('test_takes.time_start', '>=', date('y-m-d'))
+                ->where(function ($query) {
+                    $query->where(function ($query) {
+                        // dit is voor de toetsen.
+                        $query->where('test_takes.time_start', '>=', date('y-m-d'));
+                        $query->whereNull('test_takes.time_end');
+                    })->orWhere(function ($query) {
+                        // dit is voor opdrachten;
+                        $query->where('test_takes.time_end', '>=', now());
+                    });
+                })
                 ->orderBy($orderColumn, $orderDirection)
                 ->paginate($paginateBy);
         }
         return TestTake::leftJoin('test_participants', 'test_participants.test_take_id', '=', 'test_takes.id')
             ->leftJoin('tests', 'tests.id', '=', 'test_takes.test_id')
             ->leftJoin('subjects', 'subjects.id', '=', 'tests.subject_id')
-            ->select('test_takes.*', 'tests.name as test_name', 'subjects.name as subject_name')
+            ->select(
+                'test_takes.*',
+                'tests.name as test_name',
+                'subjects.name as subject_name',
+                DB::raw(
+                    sprintf(
+                        "case when tests.test_kind_id = %d then 'true' else 'false' end as is_assignment",
+                        TestKind::ASSESSMENT_TYPE
+                    )
+                )
+            )
             ->where('test_participants.user_id', Auth::id())
             ->where('test_takes.test_take_status_id', '<=', TestTakeStatus::STATUS_TAKING_TEST)
-            ->where('test_takes.time_start', '>=', date('y-m-d'))
-            ->orderBy($orderColumn, $orderDirection )
+            ->where(function ($query) {
+                $query->where(function ($query) {
+                    // dit is voor de toetsen.
+                    $query->where('test_takes.time_start', '>=', date('y-m-d'));
+                    $query->whereNull('test_takes.time_end');
+                })->orWhere(function ($query) {
+                    // dit is voor opdrachten;
+                    $query->where('test_takes.time_end', '>=', now());
+                });
+            })
+            ->orderBy($orderColumn, $orderDirection)
             ->take($amount)
             ->get();
     }
@@ -44,10 +85,13 @@ trait WithStudentTestTakes
             return TestParticipant::leftJoin('test_takes', 'test_participants.test_take_id', '=', 'test_takes.id')
                 ->leftJoin('tests', 'test_takes.test_id', '=', 'tests.id')
                 ->leftJoin('subjects', 'subjects.id', '=', 'tests.subject_id')
-                ->select('test_participants.rating', 'test_participants.retake_rating','test_takes.time_start', 'test_takes.retake', 'test_takes.user_id', 'test_takes.uuid as test_take_uuid', 'tests.name', 'tests.subject_id', 'subjects.name as subject_name')
+                ->select('test_participants.rating', 'test_participants.retake_rating', 'test_takes.time_start', 'test_takes.retake', 'test_takes.user_id', 'test_takes.uuid as test_take_uuid', 'tests.name', 'tests.subject_id', 'subjects.name as subject_name')
                 ->where('test_participants.user_id', Auth::id())
-                ->where('test_participants.rating', '!=', null)->orWhere('test_participants.retake_rating', '!=', null)
                 ->where('test_takes.test_take_status_id', '!=', TestTakeStatus::STATUS_RATED)
+                ->where(function ($query) {
+                    $query->where('test_participants.rating', '!=', null)
+                        ->orWhere('test_participants.retake_rating', '!=', null);
+                })
                 ->orderBy($orderColumn, $orderDirection)
                 ->paginate($paginateBy);
         }
@@ -56,8 +100,11 @@ trait WithStudentTestTakes
             ->leftJoin('subjects', 'subjects.id', '=', 'tests.subject_id')
             ->select('test_participants.rating', 'test_participants.retake_rating', 'test_takes.time_start', 'test_takes.retake', 'test_takes.user_id', 'tests.name', 'tests.subject_id', 'subjects.name as subject_name')
             ->where('test_participants.user_id', Auth::id())
-            ->where('test_participants.rating', '!=', null)->orWhere('test_participants.retake_rating', '!=', null)
             ->where('test_takes.test_take_status_id', '!=', TestTakeStatus::STATUS_RATED)
+            ->where(function ($query) {
+                $query->where('test_participants.rating', '!=', null)
+                    ->orWhere('test_participants.retake_rating', '!=', null);
+            })
             ->orderBy($orderColumn, $orderDirection)
             ->take($amount)
             ->get();
@@ -97,7 +144,7 @@ trait WithStudentTestTakes
             $rating = $participant->retake_rating;
         }
 
-        str_replace('.',',',round($rating,1));
+        str_replace('.', ',', round($rating, 1));
 
         return $rating;
     }
@@ -106,7 +153,7 @@ trait WithStudentTestTakes
     {
         $names = $testTake->schoolClasses()->pluck('name');
 
-        collect($names)->each(function($name, $key) use ($names) {
+        collect($names)->each(function ($name, $key) use ($names) {
             if (Str::contains($name, 'guest_class')) {
                 $names[$key] = 'Gast accounts';
             }
