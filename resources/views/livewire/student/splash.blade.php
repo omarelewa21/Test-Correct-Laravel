5c57b348d363d89dc59b5e480afe57bee64d99c2
Erik | Sobit <Erik@sobit.nl>
2021-12-16 09:58:50 +0100

    make it 5 seconds

--- resources/views/livewire/student/splash.blade.php
+++ resources/views/livewire/student/splash.blade.php
@@ -1,5 +1,5 @@
 <div id="login-body" class="flex justify-center items-center min-h-screen"
-     x-init="setTimeout(function() {$wire.handleDataAndRedirect();},2500);
+     x-init="setTimeout(function() {$wire.handleDataAndRedirect();},5000);
             addRelativePaddingToBody('login-body', 10);
 
             "
