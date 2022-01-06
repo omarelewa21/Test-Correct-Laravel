8dae79b0295c4e20ce2649d5b61d4ae623e4f49b
Erik | Sobit <Erik@sobit.nl>
2022-01-06 10:19:25 +0100

    renamed the url

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@
 return [
 
     'url_login' => env('URL_LOGIN'),
-    'url_logout' => sprintf('%susers/logoutFromLaravel', env('URL_LOGIN')),
+    'url_logout' => sprintf('%susers/logout_from_laravel', env('URL_LOGIN')),
 
     'base_url' => env('BASE_URL',''),
 
