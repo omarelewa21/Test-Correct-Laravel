cae08a10eea0d70dd2ec9d886fabf6d4ae46f598
G.M. ter Wee <gm@gms-mac-mini.home>
2022-01-14 09:58:56 +0100

    made attachment button disabled instead of invisible

--- resources/views/components/attachment/attachments-button.blade.php
+++ resources/views/components/attachment/attachments-button.blade.php
@@ -1,14 +1,17 @@
-
+@props([
+'blockAttachments' => false,
+'question'
+])
 @if(!$question->attachments->isEmpty())
     @foreach($question->attachments as $attachment)
-        <x-button.text-button class="mb-4" wire:click="showAttachment('{{ $attachment->uuid }}')" wire:key="key-attachment-{{$attachment->uuid}}">
+        <x-button.text-button class="mb-4" wire:click="showAttachment('{{ $attachment->uuid }}')" wire:key="key-attachment-{{$attachment->uuid}}" :disabled="$blockAttachments">
             <x-icon.attachment/>
             <span>{{ __('test_take.attachment') }} {{$loop->iteration}}</span>
         </x-button.text-button>
     @endforeach
 @elseif($this->group && !$this->group->attachments->isEmpty())
     @foreach($this->group->attachments as $attachment)
-        <x-button.text-button class="mb-4" wire:click="showAttachment('{{ $attachment->uuid }}')" wire:key="key-attachment-{{$attachment->uuid}}">
+        <x-button.text-button class="mb-4" wire:click="showAttachment('{{ $attachment->uuid }}')" wire:key="key-attachment-{{$attachment->uuid}}" :disabled="$blockAttachments">
             <x-icon.attachment/>
             <span>{{ __('test_take.attachment') }} {{$loop->iteration}}</span>
         </x-button.text-button>
