31984c03a338e40f373aa5bed7b8b1bac1175d4a
Erik | Sobit <Erik@sobit.nl>
2022-02-01 11:06:13 +0100

    finish matching question

--- app/Http/Livewire/Teacher/Questions/CmsMatching.php
+++ app/Http/Livewire/Teacher/Questions/CmsMatching.php
@@ -136,15 +136,33 @@ class CmsMatching extends CmsBase
                 $tq = TestQuestion::whereUuid($this->instance->testQuestionId)->first();
                 $q = $tq->question;
             }
+            $corresponding = (object) [
+                'id' => '',
+                'answer' => '',
+                'type' => ''
+            ];
+            $this->instance->cmsPropertyBag['answerStruct'] = $q->matchingQuestionAnswers->map(function ($answer, $key) use (&$corresponding) {
+                if($answer->type === 'LEFT'){
+                    $corresponding = (object) [
+                      'id' => $answer->id,
+                      'answer' => $answer->answer,
+                    ];
+                    return null;
+                }
 
-            $this->instance->cmsPropertyBag['answerStruct'] = $q->rankingQuestionAnswers->map(function ($answer, $key) {
-                return [
-                    'id'     => Uuid::uuid4(),
-                    'order'  => $key + 1,
-                    'left' => $this->transformHtmlCharsReverse($answer->left),
-                    'right' => $this->transformHtmlCharsReverse($answer->right),
-                ];
-            })->toArray();
+                else if($answer->correct_answer_id === $corresponding->id){
+                    return [
+                        'id'     => Uuid::uuid4(),
+                        'order'  => $key + 1,
+                        'left' => $this->transformHtmlCharsReverse($corresponding->answer),
+                        'right' => $this->transformHtmlCharsReverse($answer->answer),
+                    ];
+                }
+
+                else {
+                    throw new \Exception('Mismatch in the answer details, get in contact with the Test-Correct Helpdesk and notify them about this error with question ID '.$this->instance->question['id']);
+                }
+            })->filter(function($answer, $key) { return $answer != null; })->toArray();
         }
         $this->instance->cmsPropertyBag['answerCount'] = count($this->instance->cmsPropertyBag['answerStruct']);
     }
