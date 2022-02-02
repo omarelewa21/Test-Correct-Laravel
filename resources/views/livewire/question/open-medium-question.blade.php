c3dda24a87683daeb451f6beb5d567aa3a204904
G.M. ter Wee <gm@gms-mac-mini.home>
2022-02-02 16:02:48 +0100

    reordered plugins in ckeditor

--- resources/views/livewire/question/open-medium-question.blade.php
+++ resources/views/livewire/question/open-medium-question.blade.php
@@ -25,10 +25,11 @@
                         { name: 'paragraph', items: [ 'NumberedList', 'BulletedList' ] },
                         { name: 'insert', items: [ 'Table' ] },
                         { name: 'styles', items: ['Font', 'FontSize' ] },
-                        { name: 'wirisplugins', items: ['ckeditor_wiris_formulaEditor', 'ckeditor_wiris_formulaEditorChemistry']},
                         @if(Auth::user()->text2speech)
-                        { name: 'readspeaker_toolbar', items: ['readContent'] }
+                            { name: 'readspeaker_toolbar', items: ['readContent'] },
                         @endif
+                        { name: 'wirisplugins', items: ['ckeditor_wiris_formulaEditor', 'ckeditor_wiris_formulaEditorChemistry']}
+
                     ],
                     fontSize_sizes : '1/1.000em;2/1.1250em;3/1.250em;4/1.375em;5/1.4375em;6/1.5em;7/1.625em;8/1.750em;9/2.250em;10/3em;11/4.5em',
                     contentsCss : '/ckeditor/student.css'
