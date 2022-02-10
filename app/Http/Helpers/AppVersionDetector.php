968b4ad8dadc9801653779ca5a2410e7950827b4
Erik | Sobit <Erik@sobit.nl>
2022-02-10 11:21:45 +0100

    moved from ok to needs update and needs update out if expired

--- app/Http/Helpers/AppVersionDetector.php
+++ app/Http/Helpers/AppVersionDetector.php
@@ -148,12 +148,6 @@ class AppVersionDetector
         ],
         "windowsElectron" => [
             "ok" => [
-                "3.2.0",
-                "3.2.0-beta.1",
-                "3.2.0-beta.2",
-                "3.2.0-beta.3",
-                "3.2.0-beta.4",
-                "3.2.0-beta.5",
                 "3.2.1",
                 "3.2.1-beta.1",
                 "3.2.1-beta.2",
@@ -192,25 +186,27 @@ class AppVersionDetector
                 "3.3.0-beta.5",
             ],
             "needsUpdate" => [
+                "3.1.1",
                 "3.1.3",
                 "3.1.3-beta.1",
                 "3.1.3-beta.2",
                 "3.1.3-beta.3",
                 "3.1.3-beta.4",
                 "3.1.3-beta.5",
-            ],
-            "needsUpdateDeadline" => [
-                "3.1.3" => "6 maart 2022",
-            ],
-        ],
-        "macosElectron" => [
-            "ok" => [
                 "3.2.0",
                 "3.2.0-beta.1",
                 "3.2.0-beta.2",
                 "3.2.0-beta.3",
                 "3.2.0-beta.4",
                 "3.2.0-beta.5",
+            ],
+            "needsUpdateDeadline" => [
+                "3.1.3" => "6 maart 2022",
+                "3.2.0" => "3 april 2022",
+            ],
+        ],
+        "macosElectron" => [
+            "ok" => [
                 "3.2.1",
                 "3.2.1-beta.1",
                 "3.2.1-beta.2",
@@ -255,9 +251,16 @@ class AppVersionDetector
                 "3.1.1-beta.3",
                 "3.1.1-beta.4",
                 "3.1.1-beta.5",
+                "3.2.0",
+                "3.2.0-beta.1",
+                "3.2.0-beta.2",
+                "3.2.0-beta.3",
+                "3.2.0-beta.4",
+                "3.2.0-beta.5",
             ],
             "needsUpdateDeadline" => [
-                "3.1.1" => "6 maart 2022"
+                "3.1.1" => "6 maart 2022",
+                "3.2.0" => "3 april 2022",
             ],
         ]
     ];
