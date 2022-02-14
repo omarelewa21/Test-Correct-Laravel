cf2f5c5b044af8c27c961baace4777d47648ee3a
G.M. ter Wee <gm@gms-mac-mini.home>
2022-02-14 16:48:50 +0100

    added command output to migration for debugging

--- database/migrations/2022_02_04_110608_update_exam_tests_and_questions.php
+++ database/migrations/2022_02_04_110608_update_exam_tests_and_questions.php
@@ -16,12 +16,16 @@ class UpdateExamTestsAndQuestions extends Migration
     public function up()
     {
         try {
+            $output = new ConsoleOutput();
             $examSchoolLocation = \tcCore\SchoolLocation::where('customer_code', config('custom.examschool_customercode'))->first();
             if(is_null($examSchoolLocation)){
                 throw new Exception('examschool not found');
             }
             $tests = Test::where('owner_id',$examSchoolLocation->getKey())->where('scope','!=','exam')->get();
+            $output->writeln('<info>testscount:'.$tests->count().'</info>') ;
             foreach ($tests as $test){
+                $output->writeln('<info>test id:'.$test->getKey().'</info>') ;
+                $output->writeln('<info>hasNonPublishableExamSubject:'.$test->hasNonPublishableExamSubject().'</info>') ;
                 if($test->hasNonPublishableExamSubject()){
                     continue;
                 }
