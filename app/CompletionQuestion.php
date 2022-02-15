5fdca6dbbc2421023f44bf48ea22ac2ea3d8e09b
erik <erik@sobit.nl>
2022-02-15 17:29:59 +0100

    html_entity_encode should be htmlentities

--- app/CompletionQuestion.php
+++ app/CompletionQuestion.php
@@ -209,7 +209,7 @@ class CompletionQuestion extends Question implements QuestionInterface
             $tagAnswers = array_map('trim', $tagAnswers);
             if (in_array(trim($answers[$refTag]), $tagAnswers)
                 || in_array(trim(BaseHelper::transformHtmlCharsReverse($answers[$refTag])), $tagAnswers)
-                || in_array(trim(html_entity_encode($answers[$refTag])), $tagAnswers)
+                || in_array(trim(htmlentities($answers[$refTag])), $tagAnswers)
             ) {
                 $correct++;
             }
