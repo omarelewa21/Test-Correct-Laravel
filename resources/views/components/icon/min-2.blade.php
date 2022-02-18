65aecaaeb9a203ee4edc09fb2b56e314128cc3d7
roansobit <roan@sobit.nl>
2022-02-18 15:41:39 +0100

    Put SVG icons in components

---
+++ resources/views/components/icon/min-2.blade.php
@@ -0,0 +1,4 @@
+<svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" width="14" height="16">
+    <path d="M2 8h10" stroke="currentColor" stroke-width="3" fill="none" fill-rule="evenodd"
+          stroke-linecap="round"/>
+</svg>
