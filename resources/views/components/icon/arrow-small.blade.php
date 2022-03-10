ed47b0336f96b5a4b2046cdf84fe5441838984a1
Erik | Sobit <Erik@sobit.nl>
2022-03-10 13:59:26 +0100

    added arrows for ios 12 reordering the sidebar not working

--- resources/views/components/icon/arrow-small.blade.php
+++ resources/views/components/icon/arrow-small.blade.php
@@ -1,4 +1,4 @@
-<svg class="inline-block" width="11" height="10" xmlns="http://www.w3.org/2000/svg">
+<svg  {{ $attributes->merge(['class' => 'inline-block']) }} width="11" height="10" xmlns="http://www.w3.org/2000/svg">
     <g class="stroke-current" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-width="2">
         <path d="M1 5h8M5 1l4 4-4 4"/>
     </g>
--- resources/views/components/question/drawing/drawing-tool.blade.php
+++ resources/views/components/question/drawing/drawing-tool.blade.php
@@ -276,9 +276,20 @@
                     <x-icon.preview/>
                     <x-icon.preview-off/>
                 </button>
+                @if(!session()->get('TLCIsIos12',false))
+                <div class="grid grid-rows-2 gap-0">
+                    <button class="shape-btn up-btn" title="{{ __('drawing-modal.Versleep volgorde') }}">
+                        <x-icon.arrow-small class="-rotate-90"/>
+                    </button>
+                    <button class="shape-btn drag-btn" title="{{ __('drawing-modal.Versleep volgorde') }}">
+                        <x-icon.arrow-small class="rotate-90"/>
+                    </button>
+                </div>
+                @else
                 <button class="shape-btn drag-btn" title="{{ __('drawing-modal.Versleep volgorde') }}">
                     <x-icon.reorder/>
                 </button>
+                @endif
             </div>
         </div>
     </div>
