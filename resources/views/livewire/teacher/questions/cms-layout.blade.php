f4cbc2b0faa17e9e24ef158cd80aad7428cbf8b6
roansobit <roan@sobit.nl>
2022-03-14 17:15:37 +0100

    Removed abundant text

--- resources/views/livewire/teacher/questions/cms-layout.blade.php
+++ resources/views/livewire/teacher/questions/cms-layout.blade.php
@@ -427,7 +427,7 @@
                                 @endforeach
 
                             @endif
-                        </div>Openbaar maken
+                        </div>
                     </x-content-section>
                 </div>
             @endif
