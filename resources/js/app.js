6cf9120ec1a77265429fab1ca484a2fecf6a70f9
Hashtechs <hashtechs.stack@gmail.com>
2022-03-19 23:07:08 +0200

    clearClipboard functionality added when starting and ending the test

--- resources/js/app.js
+++ resources/js/app.js
@@ -239,3 +239,41 @@ String.prototype.capitalize = function ()
 {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
+
+clearClipboard = function () {
+    //source: https://stackoverflow.com/a/30810322
+    function fallbackCopyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+            var successful = document.execCommand("copy");
+            var msg = successful ? "successful" : "unsuccessful";
+        } catch (err) {
+        }
+
+        document.body.removeChild(textArea);
+    }
+    function copyTextToClipboard(text) {
+        return new Promise((resolve, reject) => {
+            if (!navigator.clipboard) {
+                fallbackCopyTextToClipboard(text);
+                resolve();
+            }
+            navigator.clipboard.writeText(text).then(() => {
+                resolve();
+            }).catch(() => { fallbackCopyTextToClipboard(text); resolve(); });
+        });
+    }
+
+    return copyTextToClipboard(' ');
+}
