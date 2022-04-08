4438e212095efe0d9450a5ca26846bfe94f5b7ab
roansobit <roan@sobit.nl>
2022-04-08 14:33:52 +0200

    Added conditional for cms drawer

--- resources/views/layouts/cms.blade.php
+++ resources/views/layouts/cms.blade.php
@@ -1,7 +1,8 @@
 <x-layouts.base>
     <div id="cms-container" class="min-h-screen">
-        <livewire:drawer.cms/>
-
+        @if(Auth::user()->schoolLocation->allow_cms_drawer && Auth::user()->schoolLocation->allow_new_drawing_question)
+            <livewire:drawer.cms/>
+        @endif
         {{ $slot }}
     </div>
 </x-layouts.base>
