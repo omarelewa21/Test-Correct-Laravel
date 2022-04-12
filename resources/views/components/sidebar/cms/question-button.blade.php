d4d972553253ad8ad4a2eb4c1a4210bc1e4527e0
roansobit <roan@sobit.nl>
2022-04-12 08:54:11 +0200

    Fixed popover menu for question buttons

--- resources/views/components/sidebar/cms/question-button.blade.php
+++ resources/views/components/sidebar/cms/question-button.blade.php
@@ -4,7 +4,7 @@
     'subQuestion' => 'false',
     'testQuestion' => null,
 ])
-<div class="question-button flex items-center cursor-pointer bold py-2 @if($subQuestion === 'false') px-6 @endif @if($question->uuid === $this->testQuestionId) primary active @endif"
+<div class="question-button flex items-center cursor-pointer bold py-2 @if($subQuestion === 'false') pl-6 pr-4 @endif @if($question->uuid === $this->testQuestionId) primary active @endif"
      wire:click="showQuestion('{{ $testQuestion ? $testQuestion->uuid : null }}', '{{ $question->uuid }}', {{ $subQuestion }})"
      @click="$dispatch('question-change', {old: '{{ $this->testQuestionId }}', new: '{{ $question->uuid }}' })"
      style="max-width: 300px"
@@ -33,7 +33,7 @@
                     </div>
                 </div>
             </div>
-            <div class="flex space-x-2.5 mt-1 text-sysbase"
+            <div class="flex items-start space-x-2.5 mt-1 text-sysbase"
                  x-data="{
                          options:false,
                           toggleOptions(e){
@@ -49,47 +49,43 @@
                          }
                      }"
             >
-                <div class="py-3 flex items-center h-full rounded-md">
+                <div class="flex h-full rounded-md">
                     <x-icon.locked/>
                 </div>
-
-                <button class="py-3 px-1 flex items-center h-full rounded-md hover:bg-primary hover:text-white transition relative"
-                        @click.stop="options = true"
-                >
-                    <div x-show="options" @click.stop="options=false" class="fixed inset-0 " style="width: var(--sidebar-width)"></div>
-                    <x-icon.options/>
-                    <div x-cloak
-                         x-show="options"
-                         x-ref="optionscontainer"
-                         class="absolute flex flex-col -right-5 top-10 bg-white text-sysbase py-2 main-shadow rounded-10 w-72 z-10"
-                         @click.outside="options = false"
-
-                         x-transition:enter="transition ease-out origin-top-right duration-200"
-                         x-transition:enter-start="opacity-0 transform scale-90"
-                         x-transition:enter-end="opacity-100 transform scale-100"
-                         x-transition:leave="transition origin-top-right ease-in duration-100"
-                         x-transition:leave-start="opacity-100 transform scale-100"
-                         x-transition:leave-end="opacity-0 transform scale-90"
+                <div class="flex">
+                    <button class="px-2 flex rounded-md hover:text-primary transition relative"
+                            @click.stop="options = true"
                     >
-{{--                        <button class="flex items-center space-x-2 py-1 px-4 base hover:text-primary hover:bg-offwhite transition w-full"--}}
-{{--                                @click="$dispatch('delete-modal', ['question'])"--}}
-{{--                        >--}}
-{{--                            <x-icon.remove/>--}}
-{{--                            <span class="text-base bold inherit">{{ __('cms.Verwijderen') }}</span>--}}
-{{--                        </button>--}}
-                        <div class="px-4 py-2 flex text-sysbase" title="{{ __('cms.Verwijderen') }}">
-                            <x-icon.trash/> {{ __('cms.Verwijderen') }}
-                        </div>
-                        <div
-                                class="px-4 py-2 flex text-sysbase"
-                                title="{{ __('cms.Wijzigen') }}"
-                                wire:click="showQuestion('{{ $testQuestion ? $testQuestion->uuid : null }}', '{{ $question->uuid }}', false)"
-                                @click="$dispatch('question-change', {old: '{{ $this->testQuestionId }}', new: '{{ $question->uuid }}' })"
+                        <div x-show="options" @click.stop="options=false" class="fixed inset-0 " style="width: var(--sidebar-width)"></div>
+                        <x-icon.options/>
+                        <div x-cloak
+                             x-show="options"
+                             x-ref="optionscontainer"
+                             class="absolute flex flex-col -right-5 top-5 bg-white text-sysbase py-2 main-shadow rounded-10 w-72 z-10"
+                             @click.outside="options = false"
+
+                             x-transition:enter="transition ease-out origin-top-right duration-200"
+                             x-transition:enter-start="opacity-0 transform scale-90"
+                             x-transition:enter-end="opacity-100 transform scale-100"
+                             x-transition:leave="transition origin-top-right ease-in duration-100"
+                             x-transition:leave-start="opacity-100 transform scale-100"
+                             x-transition:leave-end="opacity-0 transform scale-90"
                         >
-                            <x-icon.edit/> {{ __('cms.Wijzigen') }}
+                            <div class="flex items-center space-x-2 py-1 px-4 base hover:text-primary hover:bg-offwhite transition w-full" title="{{ __('cms.Verwijderen') }}">
+                                <x-icon.trash/>
+                                <span class="text-base bold inherit">{{ __('cms.Verwijderen') }}</span>
+                            </div>
+                            <div class="flex items-center space-x-2 py-1 px-4 base hover:text-primary hover:bg-offwhite transition w-full"
+                                 title="{{ __('cms.Wijzigen') }}"
+                                 wire:click="showQuestion('{{ $testQuestion ? $testQuestion->uuid : null }}', '{{ $question->uuid }}', false)"
+                                 @click="$dispatch('question-change', {old: '{{ $this->testQuestionId }}', new: '{{ $question->uuid }}' })"
+                            >
+                                <x-icon.edit/>
+                                <span class="text-base bold inherit">{{ __('cms.Wijzigen') }}</span>
+                            </div>
                         </div>
-                    </div>
-                </button>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
