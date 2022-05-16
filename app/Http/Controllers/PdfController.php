746b6657f71dd51a7c9401f374176ceb48f34235
G.M. ter Wee <gm@GMs-Mac-mini.local>
2022-05-16 17:27:02 +0200

    wiris in pdf wip

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use tcCore\Http\Helpers\PdfHelper;
 use tcCore\Http\Requests\HtmlToPdfRequest;
+use GuzzleHttp\Client;
 
 class PdfController extends Controller
 {
@@ -23,6 +24,7 @@ class PdfController extends Controller
     public function HtmlToPdf(HtmlToPdfRequest $request)
     {
         $html = $this->base64ImgPaths($request->get('html'));
+        $html = $this->svgWirisFormulas($html);
         $output = PdfHelper::HtmlToPdf($html);
         return response($output);
     }
@@ -131,6 +133,103 @@ class PdfController extends Controller
         }
         return $server->getImageAsBase64($file, $widthHeight+['fit'=>'contain',  'fm' => 'jpg', 'q' => $quality,]);
     }
-    
 
+    private function svgWirisFormulas($html)
+    {
+        $internalErrors = libxml_use_internal_errors(true);
+        $doc = new DOMDocument('1.0', 'UTF-8');
+        $doc->loadHTML($html);
+        libxml_use_internal_errors($internalErrors);
+        $mathList = $doc->getElementsByTagName('math');
+         foreach ($mathList as $mathNode){
+            $this->replaceMathNodeWithSvg($mathNode,$doc);
+        }
+        $html = $doc->saveHTML($doc->documentElement);
+        return $html;
+    }
+
+    private function replaceMathNodeWithSvg($mathNode,$doc)
+    {
+        try {
+            $mathNodeString = $doc->saveHtml($mathNode);
+            $svg = $this->getWirisSvg($mathNodeString);
+            $svgFragment = $doc->createDocumentFragment();
+            $svgFragment->appendXML($svg);
+            $mathNode->parentNode->replaceChild($svgFragment,$mathNode);
+            dump($doc->saveHTML());
+        }catch (\Throwable $th) {
+            Bugsnag::notifyException($th);
+            return;
+        }
+    }
+
+
+    private function getWirisSvg($mml)
+    {
+        $data = [
+            'mml'=> $mml,
+            'lang'=> 'en-gb',
+            'metrics'=> true,
+            'centerbaseline'=> false,
+
+        ];
+        $createPath = config('app.base_url').'/ckeditor/plugins/ckeditor_wiris/integration/createimage.php';
+        $path = config('app.base_url').'ckeditor/plugins/ckeditor_wiris/integration/showimage.php';
+        $client = new Client();
+        $res = $client->request('POST',$createPath,[
+            'form_params' => $data]);
+        $formulaUrl = $res->getBody()->getContents();
+        $components = parse_url($formulaUrl);
+        parse_str($components['query'], $results);
+        $formula = $results['formula'];
+        $data1 = [
+            'lang'=> 'en-gb',
+            'metrics'=> true,
+            'centerbaseline'=> false,
+            'formula' => $formula,
+            'version' => '7.26.0.1439',
+        ];
+        $res = $client->request('GET',$path,['query' => $data1]);
+        $res = $client->request('POST',$path,[
+            'form_params' => $data]);
+        $json = json_decode($res->getBody()->getContents(),true);
+        return $json['result']['content'];
+    }
+
+    private function getTestWiris()
+    {
+        $data = [
+            'mml'=> '<math xmlns="http://www.w3.org/1998/Math/MathML"><mroot><mfrac><mi>t</mi><mi>v</mi></mfrac><mi>e</mi></mroot></math>',
+            'lang'=> 'en-gb',
+            'metrics'=> true,
+            'centerbaseline'=> false,
+
+        ];
+        $createPath = config('app.base_url').'/ckeditor/plugins/ckeditor_wiris/integration/createimage.php';
+        $path = config('app.base_url').'ckeditor/plugins/ckeditor_wiris/integration/showimage.php';
+        $client = new Client();
+        $res = $client->request('POST',$createPath,[
+            'form_params' => $data]);
+        $formulaUrl = $res->getBody()->getContents();
+        $components = parse_url($formulaUrl);
+        parse_str($components['query'], $results);
+        $formula = $results['formula'];
+        $data1 = [
+            'lang'=> 'en-gb',
+            'metrics'=> true,
+            'centerbaseline'=> false,
+            'formula' => $formula,
+            'version' => '7.26.0.1439',
+        ];
+        $res = $client->request('GET',$path,['query' => $data1]);
+        $res = $client->request('POST',$path,[
+            'form_params' => $data]);
+        $json = json_decode($res->getBody()->getContents(),true);
+        dump($json);
+        dump($json['result']['content']);
+//        $clean = stripslashes($res->getBody()->getContents());
+//        $clean = substr($clean, 1, -1);
+//        dump(json_decode($clean));
+
+    }
 }
