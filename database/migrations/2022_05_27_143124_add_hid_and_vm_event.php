bd17d8970740301dd51dd4965408babb1e79947d
Jonathan <jonathanjagt@teachandlearncompany.com>
2022-05-27 14:44:17 +0200

    Add HID and VM event type
    See TCP-1722

---
+++ database/migrations/2022_05_27_143124_add_hid_and_vm_event.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddHidAndVmEvent extends Migration
+{
+    private static function createAndAdd($id, $name, $confirm, $reason) {
+        TestTakeEventType::create([
+            'id' => $id,
+            'name' => $name,
+            'requires_confirming' => $confirm,
+            'reason' => $reason
+        ]);
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        TestTakeEventType::unguard();
+        AddHidAndVmEvent::createAndAdd(27, 'Forbidden device', 1, 'hid');
+        AddHidAndVmEvent::createAndAdd(28, 'VM detected', 1, 'vm');
+        TestTakeEventType::reguard();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('test_take_event_types')->where('name', '=', 'VM detected')->delete();
+    }
+}
