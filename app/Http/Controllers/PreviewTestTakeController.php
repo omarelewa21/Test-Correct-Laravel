ef00c415eae7864c1e73bd147ae46a530532bdc3
G.M. ter Wee <gm@gms-mac-mini.home>
2022-06-21 10:45:52 +0200

    student answers wip

--- app/Http/Controllers/PreviewTestTakeController.php
+++ app/Http/Controllers/PreviewTestTakeController.php
@@ -2,6 +2,7 @@
 
 namespace tcCore\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use tcCore\GroupQuestionQuestion;
@@ -19,8 +20,14 @@ class PreviewTestTakeController extends Controller
 
     public function show(TestTake $testTake, Request $request)
     {
+        $titleForPdfPage = $testTake->test->name.' '.Carbon::now()->format('d-m-Y H:i');
+        view()->share('titleForPdfPage',$titleForPdfPage);
         $testParticipants = $testTake->testParticipants()->whereNotNull('answer_id')->get();
-        return view('test-take-overview-preview',compact(['testTake','testParticipants']));
+        $html = view('test-take-overview-preview',compact(['testTake','testParticipants']))->render();
+        return response()->make(PdfController::HtmlToPdfFromString($html), 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="toets.pdf"'
+        ]);
     }
 
 
