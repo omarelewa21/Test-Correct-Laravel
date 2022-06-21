f452d2159936a60a00473c18cf0e559d37ed8c76
Erik | Sobit <Erik@sobit.nl>
2022-06-21 13:30:49 +0200

    macbook and windows app version update

--- app/Http/Helpers/AppVersionDetector.php
+++ app/Http/Helpers/AppVersionDetector.php
@@ -147,12 +147,6 @@ class AppVersionDetector
         ],
         "windowsElectron" => [
             "ok" => [
-                "3.2.2",
-                "3.2.2-beta.1",
-                "3.2.2-beta.2",
-                "3.2.2-beta.3",
-                "3.2.2-beta.4",
-                "3.2.2-beta.5",
                 "3.2.3",
                 "3.2.3-beta.1",
                 "3.2.3-beta.2",
@@ -179,20 +173,14 @@ class AppVersionDetector
                 "3.3.0-beta.5",
             ],
             "needsUpdate" => [
-                "3.2.1"
+                "3.2.2",
             ],
             "needsUpdateDeadline" => [
-                "3.2.1" => "13 juni 2022",
+                "3.2.2" => "4 augustus 2022",
             ],
         ],
         "macosElectron" => [
             "ok" => [
-                "3.2.2",
-                "3.2.2-beta.1",
-                "3.2.2-beta.2",
-                "3.2.2-beta.3",
-                "3.2.2-beta.4",
-                "3.2.2-beta.5",
                 "3.2.3",
                 "3.2.3-beta.1",
                 "3.2.3-beta.2",
@@ -219,10 +207,10 @@ class AppVersionDetector
                 "3.3.0-beta.5",
             ],
             "needsUpdate" => [
-                "3.2.1"
+                "3.2.2",
             ],
             "needsUpdateDeadline" => [
-                "3.2.1" => "13 juni 2022",
+                "3.2.2" => "4 augustus 2022",
             ],
         ]
     ];
