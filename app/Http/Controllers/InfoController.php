1ddef83632dbedf08d38096a6e26f44a1515ca8f
Hashtechs <hashtechs.stack@gmail.com>
2022-06-25 13:53:21 +0200

    Another methodology followed

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -119,16 +119,17 @@ class InfoController extends Controller
         foreach($imgs as $img) {
             $src = $img->getAttribute('src');
             $url_arr = parse_url($src);
-            if($url_arr['host'] !== request()->getHost()){          // continue if saved image domain !=  test-correct.{env_type}
+            if($url_arr['host'] !== request()->getHost()){                          // continue if saved image domain !=  test-correct.{env_type}
                 parse_str($url_arr['query'], $query);
                 $filename = $query['filename'];
-                $response = Http::post(                                                                         // Send a post request to get image content
-                    $url_arr['scheme'] . '://' . $url_arr['host'] . '/infos/inlineInfoImage/' . $filename,      // link
-                    ['key' => $this->key]                                                                       // secret key between cake and laravel
-                );
-                if($response->successful()){
-                    Storage::disk('inline_images')->put($filename, base64_decode($response->body()));
-                    $img->setAttribute('src', request()->getSchemeAndHttpHost() . '/infos/inline-image/'. $filename);
+                
+                if(Storage::disk('cake')->exists('questionanswers/' . $filename)){                                          // check image file exists in cake
+                    $contents = Storage::disk('cake')->get('questionanswers/' . $filename);                                 // get image content
+                    
+                    if(Storage::disk('inline_images')->put($filename, $contents)){                                          // if storing to laravel succeeds
+                        Storage::disk('cake')->delete('questionanswers/' . $filename);                                      // delete file in cake
+                        $img->setAttribute('src', request()->getSchemeAndHttpHost() . '/infos/inline-image/'. $filename);   // reference laravel path in img src
+                    }
                 }
             }
         }
