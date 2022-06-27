5b4afe7a34e18fe1baa87364e253f66007ac5c20
G.M. ter Wee <gm@GMs-Mac-mini.local>
2022-06-27 17:04:19 +0200

    student answers pdf wip

--- resources/views/components/partials/answer-model-question-container.blade.php
+++ resources/views/components/partials/answer-model-question-container.blade.php
@@ -1,11 +1,12 @@
 @props([
 'question',
 'number',
-'answer'
+'answer',
+'pdf_type' => 'answer_model'
 ])
 
 <div class="flex flex-col p-8 sm:p-10 content-section rs_readable page-no-break" >
-    <div class="question-title flex-pdf flex-wrap-pdf items-center question-indicator border-bottom mb-6" >
+    <div class="question-title flex-pdf flex-wrap-pdf items-center question-indicator border-bottom mb-6"  style="position:relative;">
         <div class="inline-flex-pdf question-number rounded-full text-center justify-center items-center complete" >
             <span class="align-middle cursor-default">{{ $number }}</span>
         </div>
@@ -16,6 +17,24 @@
 
         <h1 class="inline-block-pdf ml-2 mr-6" selid="questiontitle" > {!! __($question->caption) !!} </h1>
         <h4 class="inline-block-pdf">{{ $question->score }} pt</h4>
+        @if ($this->answered && $pdf_type=='student_test_take')
+            @if($this->isQuestionFullyAnswered())
+                <p class="ml-auto cta-primary flex space-x-2 items-center inline-block-pdf" style="position:relative;left:800px;">
+                    <x-icon.checkmark-pdf class="student_test_take_checkmark_pdf"/>
+                    <span class="ml-auto font-size-14 bold align-middle uppercase">{{ __('test_take.answered') }}</span>
+                </p>
+            @else
+                <p class="ml-auto mid-grey flex space-x-2 items-center inline-block-pdf"  style="position:relative;left:800px;">
+                    <x-icon.checkmark-pdf class="student_test_take_checkmark_pdf"/>
+                    <span class="ml-auto font-size-14 bold align-middle uppercase">{{ __('test_take.partly_answered') }}</span>
+                </p>
+            @endif
+        @elseif(!$this->answered && $pdf_type=='student_test_take')
+            <p class="ml-auto mid-grey flex space-x-2 items-center inline-block-pdf" style="position:relative;left:800px;">
+                <x-icon.close class="student_test_take_checkmark_pdf"/>
+                <span class="ml-auto font-size-14 bold align-middle uppercase">{{ __('test_take.not_answered') }}</span>
+            </p>
+        @endif
     </div>
     <div class="flex flex-1 overview">
         <div class="questionContainer w-full">
