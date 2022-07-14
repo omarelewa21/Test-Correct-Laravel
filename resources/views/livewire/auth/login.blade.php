ac68a8f88651617db01a84b9c9ef987870df83c7
roan <roan@sobit.nl>
2022-07-14 17:06:17 +0200

    changed login styling

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -7,11 +7,12 @@
      x-on:resize.window.debounce.200ms="addRelativePaddingToBody('login-body')"
      wire:ignore.self
 >
-    <div class="w-full max-w-[800px] mx-4 py-4">
+    <div class="w-full max-w-[540px] mx-4 py-4">
 
 
         @if($tab == 'login')
             @if($showGuestSuccess)
+                {{-- remove and replace with toaster? --}}
                 <div class="flex cta-gradient w-full p-10 -mb-4 rounded-t-10 relative top-2.5 space-x-2.5">
                     <div class="flex" x-data="">
                         <x-stickers.congratulations/>
@@ -25,28 +26,26 @@
                     </div>
                 </div>
             @endif
-            <div class="content-section p-10 mb-4 space-y-5 shadow-xl flex flex-col " style="min-height: 550px">
-                <div class="flex items-center space-x-2.5">
-                    <div class="flex">
-                        <x-stickers.login/>
-                    </div>
-                    <div>
-                        <h1>{{ __('auth.log_in_verb') }}</h1>
-                    </div>
-
+            {{-- top content block height:120px --}}
+            <div class="bg-white rounded-t-[10px] px-10 pt-[31px] pb-0 shadow-lg flex flex-col relative border-b border-secondary">
+                <div class="absolute -top-10 left-1/2 -translate-x-1/2 flex">
+                    <x-stickers.login/>
+                </div>
+                <div class="flex items-center justify-center pb-[5px]">
+                    <h1 class="text-[28px]">{{ __('auth.log_in_verb') }}</h1>
                 </div>
 
                 <div class="flex flex-col flex-1">
-                    <div class="flex w-full space-x-6 mb-5 border-b border-light-grey">
-                        <div :class="{'border-b-2 border-primary -mb-px' : openTab === 1}">
-                            <x-button.text-button class="primary"
+                    <div class="flex w-full space-x-6 ">
+                        <div :class="{'border-b-2 border-primary -mb-px primary' : openTab === 1}">
+                            <x-button.text-button class="text-inherit hover:primary"
                                                   @click="openTab = 1"
                             >
                                 {{ __('auth.log_in_verb') }}
                             </x-button.text-button>
                         </div>
-                        <div class="" :class="{'border-b-2 border-primary -mb-px' : openTab === 2}">
-                            <x-button.text-button class="primary"
+                        <div class="" :class="{'border-b-2 border-primary -mb-px primary' : openTab === 2}">
+                            <x-button.text-button class="text-inherit hover:primary"
                                                   @click="openTab = 2;"
                             >
                                 {{ __('auth.log_in_with_temporary_student_login') }}
@@ -54,20 +53,28 @@
                         </div>
                     </div>
 
+                </div>
+            </div>
+            {{-- middle content block height:377px --}}
+            <div class="bg-white px-10 py-[30px] space-y-5 shadow-lg flex flex-col relative border-b border-secondary"
+                 style="min-height: 377px">
+
+                <div class="flex flex-col flex-1">
+
                     <div class="flex flex-col flex-1" x-show="openTab === 1">
                         <form wire:submit.prevent="login" action="#" method="POST" class="flex-col flex flex-1">
-                            <div class="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0">
+                            <div class="flex flex-col space-y-4">
                                 <x-input.group label="{{ __('auth.emailaddress')}}" class="flex-1">
                                     <x-input.text wire:model.lazy="username" autofocus></x-input.text>
                                 </x-input.group>
                                 <x-input.group label="{{ __('auth.password')}}" class="flex-1 relative">
-                                    <div class="group"
-                                         @mouseenter="hoverPassword = true"
+                                    <div @mouseenter="hoverPassword = true"
                                          @mouseleave="hoverPassword = false"
                                          @click="showPassword = !showPassword; hoverPassword = false; initialPreviewIconState = false">
-                                        <x-icon.preview-off class="absolute bottom-3 right-3.5 primary-hover cursor-pointer"
-                                                            x-bind:class="{'opacity-50' : initialPreviewIconState, 'hover:text-sysbase': (!showPassword && !hoverPassword)}"
-                                                            x-show="(!showPassword && !hoverPassword) || (showPassword && hoverPassword)"/>
+                                        <x-icon.preview-off
+                                                class="absolute bottom-3 right-3.5 primary-hover cursor-pointer"
+                                                x-bind:class="{'opacity-50' : initialPreviewIconState, 'hover:text-sysbase': (!showPassword && !hoverPassword)}"
+                                                x-show="(!showPassword && !hoverPassword) || (showPassword && hoverPassword)"/>
                                         <div class="absolute bottom-3 right-3.5 flex items-center h-[16px]">
                                             <x-icon.preview class="primary-hover cursor-pointer"
                                                             x-bind:class="{'hover:text-sysbase': (showPassword && !hoverPassword)}"
@@ -190,29 +197,18 @@
                                 @enderror
                             </div>
                             {{-- With forgot_password button, ml_auto can be switched justify-between on the parent --}}
-                            <div class="flex mt-auto pt-4 justify-between">
-                                <div class="flex order-2 space-x-4">
-                                    <x-button.primary type="link" class="bg-[#2e3192]" size="md"
-                                                      href="{{ route('saml2_login', 'entree') }}">
-                                        <x-icon.entreefederatie/>
-                                        <span>{{ __('auth.login_with_entree') }}</span>
-                                    </x-button.primary>
-                                    <x-button.cta class="" size="md">
-                                        <span>{{ __('auth.log_in_verb') }}</span>
-                                    </x-button.cta>
-                                </div>
-                                <x-button.text-button class="order-1"
-                                                      wire:click.prevent="$set('tab', 'forgotPassword')">
-                                    <span class="text-base">{{__('auth.forgot_password_long')}}</span>
-                                    <x-icon.arrow/>
-                                </x-button.text-button>
+                            <div class="flex mt-auto pt-4">
+                                <x-button.cta class="flex flex-grow justify-center" size="md">
+                                    <span>{{ __('auth.log_in_verb') }}</span>
+                                </x-button.cta>
                             </div>
                         </form>
                     </div>
 
                     <div class="flex flex-col flex-1" x-show="openTab === 2" x-cloak>
 
-                        <form wire:submit.prevent="guestLogin" action="#" method="POST" class="flex-col flex flex-1">
+                        <form wire:submit.prevent="guestLogin" action="#" method="POST"
+                              class="flex-col flex flex-1 justify-start">
                             <div class="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0">
                                 <x-input.group label="{{ __('auth.first_name')}}" class="w-56">
                                     <x-input.text wire:model.lazy="firstName" autofocus></x-input.text>
@@ -220,13 +216,12 @@
                                 <x-input.group label="{{ __('auth.suffix')}}" class="w-28">
                                     <x-input.text wire:model.lazy="suffix" autofocus></x-input.text>
                                 </x-input.group>
-                                <x-input.group label="{{ __('auth.last_name')}}" class="flex-1">
-                                    <x-input.text wire:model.lazy="lastName" autofocus></x-input.text>
-                                </x-input.group>
                             </div>
-
-                            <div class="">
-                                <div class="mx-auto mt-4 flex flex-col">
+                            <x-input.group label="{{ __('auth.last_name')}}" class="mt-4">
+                                <x-input.text wire:model.lazy="lastName" autofocus></x-input.text>
+                            </x-input.group>
+                            <div class="flex-1 ">
+                                <div class="mx-auto flex flex-col">
                                     <x-partials.test-take-code/>
                                 </div>
                             </div>
@@ -293,32 +288,77 @@
                                 @enderror
                                 @if($showGuestError)
                                     @if($guest_message == 'removed_by_teacher')
-                                    <div class="notification warning stretched mt-4">
-                                        <span class="title">{{ __('auth.log_in_again') }}</span>
-                                        <span class="body">{{ __('auth.removed_by_teacher') }}</span>
-                                    </div>
+                                        <div class="notification warning stretched mt-4">
+                                            <span class="title">{{ __('auth.log_in_again') }}</span>
+                                            <span class="body">{{ __('auth.removed_by_teacher') }}</span>
+                                        </div>
                                     @endif
                                     @if($guest_message == 'no_browser_testing')
                                         <div class="notification warning stretched mt-4">
                                             <span class="title">{{ __('auth.cannot_log_in_to_test_with_browser') }}</span>
                                             <span class="body">{{ __('auth.usage_of_app_is_required_for_this_test') }}</span>
                                             <a href="{{ $studentDownloadUrl }}" class="bold text-sm"
-                                               target="_blank">{{ __("auth.download_and_install_the_app") }} <x-icon.arrow-small></x-icon.arrow-small></a>
+                                               target="_blank">{{ __("auth.download_and_install_the_app") }}
+                                                <x-icon.arrow-small></x-icon.arrow-small>
+                                            </a>
                                         </div>
                                     @endif
                                 @endif
                             </div>
 
                             <div class="flex mt-auto pt-4">
-                                <x-button.cta class="ml-auto" size="md">
+                                <x-button.cta class="flex flex-grow justify-center" size="md">
                                     <span>{{ __('auth.log_in_verb') }}</span>
                                 </x-button.cta>
                             </div>
                         </form>
                     </div>
+
                 </div>
             </div>
-        @elseif($tab == 'forgotPassword')
+            <div class="bg-white rounded-b-[10px] px-10 pb-5 pt-2 mb-1.5 space-y-5 shadow-lg flex flex-col relative border-b border-secondary">
+
+                <div class="flex flex-col flex-1 space-y-2 items-center">
+                    <div>
+                        <span>
+                            {{__('auth.do_you_have_an_entree_account')}}
+                        </span>
+                    </div>
+                    <x-button.primary type="link" class="" size="md"
+                                      href="{{ route('saml2_login', 'entree') }}">
+                        <x-icon.entreefederatie/>
+                        <span>{{ __('auth.login_with_entree') }}</span>
+                    </x-button.primary>
+
+                </div>
+            </div>
+
+
+            {{-- todo: create text buttons under login container: --}}
+
+            <div class="w-full flex flex-col items-center">
+                <div class="flex items-center space-x-2">
+                    <span class="hidden md:flex">
+                        {{__('auth.forgot_password_long')}}
+                    </span>
+                    <x-button.text-button class="order-1" size="sm"
+                                          wire:click.prevent="$set('tab', 'forgotPassword')">
+                        <span class="text-base">{{__('auth.forgot_password')}}</span>
+                        <x-icon.arrow/>
+                    </x-button.text-button>
+                </div>
+                <div class="flex items-center space-x-2">
+                    <span class="hidden md:flex">
+                        {{__('auth.Maak account long')}}
+                    </span>
+                    <x-button.text-button class="order-1" size="sm"
+                                          @click="Livewire.emit('open-auth-modal')">
+                        <span class="text-base">{{__('auth.Maak account')}}</span>
+                        <x-icon.arrow/>
+                    </x-button.text-button>
+                </div>
+            </div>
+        @elseif($tab == 'forgotPassword') {{-- todo: bring forgot password in line with login tab --}}
             <div class="content-section p-10 space-y-5 shadow-xl flex flex-col " style="min-height: 550px">
                 <form wire:submit.prevent="sendForgotPasswordEmail" action="#" method="POST"
                       class="flex-col flex flex-1">
@@ -719,7 +759,7 @@
                 </form>
             </div>
         @endif
-        <div class="flex flex-col md:flex-row justify-center items-center md:space-x-4" browser wire:ignore>
+        {{--<div class="flex flex-col md:flex-row justify-center items-center md:space-x-4" browser wire:ignore>
             <x-button.primary type="link" href="{{ $this->studentDownloadUrl }}">
                 <x-icon.download/>
                 <span>{{__('auth.download_student_app')}}</span>
@@ -729,7 +769,7 @@
                 <span>{{__('auth.request_account_from_teacher')}}</span>
                 <x-icon.arrow/>
             </x-button.text-button>
-        </div>
+        </div>--}}
     </div>
 
     <x-modal.auth-create-account maxWidth="lg" wire:model="showAuthModal"/>
