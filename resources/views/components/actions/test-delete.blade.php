bc43822a634b37aaa81b348361909e8c1af0da10
Martin Folkerts <martinfolkerts@iMac-van-Martin.local>
2022-07-15 16:57:50 +0200

    fixed json bug in test-delete action

--- resources/views/components/actions/test-delete.blade.php
+++ resources/views/components/actions/test-delete.blade.php
@@ -1,9 +1,9 @@
-<div>
-    @if($variant == 'icon_button')
+<div x-data="{}">
+    @if($variant == 'icon-button')
         @if ($test->canDelete(auth()->user()))
             <x-button.primary
                     class="pl-[12px] pr-[12px]"
-                    @click="$emitTo('teacher.test-delete-modal', 'displayModal', '{{  $test->uuid }}')">
+                    @click="$wire.emitTo('teacher.test-delete-modal', 'displayModal', '{{  $test->uuid }}')">
                 <x-icon.trash/>
             </x-button.primary>
         @else
@@ -16,7 +16,7 @@
         @if( $test->canDelete(auth()->user()))
             <button
                     class="flex items-center space-x-2 py-1 px-4 base hover:text-primary hover:bg-offwhite transition w-full"
-                    @click="$event.target.dispatchEvent(new CustomEvent('context-menu-close', { bubbles: true }));$wire.emitTo('teacher.test-delete-modal', 'displayModal', '{{ json_encode(['testUuid' => $test->uuid]) }} ')"
+                    @click="$event.target.dispatchEvent(new CustomEvent('context-menu-close', { bubbles: true }));$wire.emitTo('teacher.test-delete-modal', 'displayModal', '{{  $test->uuid }}')"
             >
                 <x-icon.remove/>
                 <span class="text-base bold inherit">{{ __('cms.Verwijderen') }}</span>
