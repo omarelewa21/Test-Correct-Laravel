36317505ea124b064bb0edcc6fc91fa66fdab824
Hashtechs <hashtechs.stack@gmail.com>
2022-07-18 00:29:29 +0200

    show group attachment and question attachment

--- resources/views/components/attachment/attachments-button.blade.php
+++ resources/views/components/attachment/attachments-button.blade.php
@@ -2,6 +2,23 @@
 'blockAttachments' => false,
 'question'
 ])
+
+@if($this->group && !$this->group->attachments->isEmpty())
+    @foreach($this->group->attachments as $attachment)
+        <x-button.text-button class="mb-4"
+            x-on:click="$wire.closeAttachmentModal();
+                setTimeout(function(){
+                    $wire.showAttachment('{{ $attachment->uuid }}');
+                }, 500);"
+            wire:key="key-attachment-{{$attachment->uuid}}" :disabled="$blockAttachments"
+                              selid="open-attachment-btn"
+        >
+            <x-icon.attachment/>
+            <span wire:ignore>{{ __('test_take.attachment') }} {{$loop->iteration}}</span>
+        </x-button.text-button>
+    @endforeach
+@endif
+
 @if(!$question->attachments->isEmpty())
     @foreach($question->attachments as $attachment)
         <x-button.text-button class="mb-4"
@@ -11,20 +28,6 @@
                 }, 500);"
             wire:key="key-attachment-{{$attachment->uuid}}" :disabled="$blockAttachments"
                                 selid="open-attachment-btn"
-        >
-            <x-icon.attachment/>
-            <span wire:ignore>{{ __('test_take.attachment') }} {{$loop->iteration}}</span>
-        </x-button.text-button>
-    @endforeach
-@elseif($this->group && !$this->group->attachments->isEmpty())
-    @foreach($this->group->attachments as $attachment)
-        <x-button.text-button class="mb-4"
-            x-on:click="$wire.closeAttachmentModal();
-                setTimeout(function(){
-                    $wire.showAttachment('{{ $attachment->uuid }}');
-                }, 500);"
-            wire:key="key-attachment-{{$attachment->uuid}}" :disabled="$blockAttachments"
-                              selid="open-attachment-btn"
         >
             <x-icon.attachment/>
             <span wire:ignore>{{ __('test_take.attachment') }} {{$loop->iteration}}</span>
