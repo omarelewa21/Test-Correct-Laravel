17e0a11adcfc555e88f942937407bdc4e9c7f285
roan <roan@sobit.nl>
2022-08-02 15:56:00 +0200

    removed copy button on national items

--- resources/views/livewire/actions/test-duplicate-test.blade.php
+++ resources/views/livewire/actions/test-duplicate-test.blade.php
@@ -1,24 +1,26 @@
 <div>
-    @if ($variant == 'icon-button')
-        <x-button.primary
-                class="pl-[12px] pr-[12px]"
-                wire:click="duplicateTest"
-                title="{{ __('teacher.Kopie maken') }}"
-        >
-            <x-icon.copy/>
-        </x-button.primary>
-        <x-slot name="text">
+    @if($showButton)
+        @if ($variant == 'icon-button')
+            <x-button.primary
+                    class="pl-[12px] pr-[12px]"
+                    wire:click="duplicateTest"
+                    title="{{ __('teacher.Kopie maken') }}"
+            >
+                <x-icon.copy/>
+            </x-button.primary>
+            <x-slot name="text">
             <span class="text-base text-left">
                 {{ __('cms.Kopie maken') }}
             </span>
-        </x-slot>
-    @elseif($variant == 'context-menu')
-        <button
-                class="flex items-center space-x-2 py-1 px-4 base hover:text-primary hover:bg-offwhite transition w-full"
-                wire:click="duplicateTest"
-        >
-            <x-icon.copy/>
-            <span class="text-base bold inherit">{{ __('cms.Kopie maken') }}</span>
-        </button>
+            </x-slot>
+        @elseif($variant == 'context-menu')
+            <button
+                    class="flex items-center space-x-2 py-1 px-4 base hover:text-primary hover:bg-offwhite transition w-full"
+                    wire:click="duplicateTest"
+            >
+                <x-icon.copy/>
+                <span class="text-base bold inherit">{{ __('cms.Kopie maken') }}</span>
+            </button>
+        @endif
     @endif
 </div>
