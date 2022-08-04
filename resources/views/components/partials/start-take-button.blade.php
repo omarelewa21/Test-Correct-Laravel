f3e0231eba29b184d291a89c9a39e6702a823d23
Sander Smits <sander.smits@test-correct.nl>
2022-08-04 11:55:50 +0200

    Add selid to take button for students
    Selenium has now his own id to find
    the test take btn.

--- resources/views/components/partials/start-take-button.blade.php
+++ resources/views/components/partials/start-take-button.blade.php
@@ -2,14 +2,14 @@
 
 @if($isAssignment)
     @if($timeStart <= now() && $timeEnd >= now())
-        <x-button.cta size="sm" type="link"
+        <x-button.cta selid="dashboard-start-take-button" size="sm" type="link"
                       href="{{ route('student.waiting-room', ['take' => $uuid]) }}">{{ __('student.make') }}</x-button.cta>
     @else
         <span class="italic text-sm lowercase">{{ __('student.planned') }}</span>
     @endif
 @else
     @if($timeStart == \Carbon\Carbon::today())
-        <x-button.cta size="sm" type="link"
+        <x-button.cta selid="dashboard-start-take-button" size="sm" type="link"
                       href="{{ route('student.waiting-room', ['take' => $uuid]) }}">{{ __('student.make') }}</x-button.cta>
     @else
         <span class="italic text-sm lowercase">{{ __('student.planned') }}</span>
