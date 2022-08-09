17502d6de42a4199bfd0dc139b3d677992bd9907
roan <roan@sobit.nl>
2022-08-09 15:52:55 +0200

    Changed the way author filter is being filled

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -2,15 +2,9 @@
 
 namespace tcCore\Http\Controllers;
 
-use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Response;
-use tcCore\Http\Helpers\BaseHelper;
-use tcCore\Subject;
-use tcCore\Teacher;
-use tcCore\Test;
+use tcCore\TestAuthor;
 use tcCore\User;
 
 class AuthorsController extends Controller
@@ -43,22 +37,12 @@ class AuthorsController extends Controller
 
     private function getBuilderForOwnSubjects($user)
     {
-        return User::withTrashed()->whereIn('id', // find all users part of this selection
-            Teacher::withTrashed()->whereIn('subject_id', // find the teachers with these subjects
-                Subject::withTrashed()->whereIn('section_id', // get all subjects belonging to the section memberships
-                    $user->sections()->select('sections.id')->where('demo',0) // get section memberships of this teacher where section is not part of the demo environment
-                )->select('subjects.id')
-            )->select('teachers.user_id')
-        )->select('id','name_first','name_suffix','name')->groupBy('users.id');
+        return TestAuthor::schoolLocationAuthorUsers($user);
     }
 
     private function getBuilderForOwnSubjectsAndSharedSections($user)
     {
-        return User::withTrashed()->whereIn('id', // find all users part of this selection
-            Teacher::withTrashed()->whereIn('subject_id', // find the teachers with these subjects
-                $user->subjectsIncludingShared()->where('demo',0)->pluck('id')
-            )->select('teachers.user_id')
-        )->select('id','name_first','name_suffix','name')->groupBy('users.id');
+        return TestAuthor::schoolLocationAndSharedSectionsAuthorUsers($user);
     }
 
 }
