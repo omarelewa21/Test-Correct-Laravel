648becee7d8daf4ac5300fc6f4a8acd7a33dfef3
jonnevantleven <jonne@sobit.nl>
2022-08-26 11:05:45 +0200

    added new stickers

---
+++ resources/views/components/stickers/test-export-questions.blade.php
@@ -0,0 +1,36 @@
+<svg {{ $attributes }} width="64" height="64" xmlns="http://www.w3.org/2000/svg">
+    <title>sticker/exporteer-toets</title>
+    <g fill="none" fill-rule="evenodd">
+        <path d="m39.983 2.054 8.08 53.9-39.046 5.992-8.08-53.9 39.046-5.992Z" stroke="#041F74" stroke-width="3"
+              fill="#FFF"/>
+        <path d="m62.637 7.774-7.09 54.04-39.184-5.003 7.09-54.039 39.184 5.002Z" stroke="#041F74" stroke-width="3"
+              fill="#FFF"/>
+        <path d="m52 56.5.5-41.672L39.525 1.646 12 1.5 11.5 56l40.5.5Z" stroke="#041F74" stroke-width="3" fill="#FFF"/>
+        <g transform="translate(20 9)">
+            <path stroke="#CEDAF3" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" d="M8.5 2.5h14"/>
+            <circle fill="#CEDAF3" cx="2.5" cy="2.5" r="2.5"/>
+        </g>
+        <g stroke="#CEDAF3" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+            <path d="M21 18h22M21 23h18"/>
+        </g>
+        <g fill="#CEDAF3" stroke="#CEDAF3" stroke-linejoin="round" stroke-width="2">
+            <path d="M21 28h4v4h-4zM37 28h4v4h-4zM29 28h4v4h-4z"/>
+        </g>
+        <g>
+            <g transform="translate(20 37)">
+                <path stroke="#CEDAF3" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" d="M8.5 2.5h14"/>
+                <circle fill="#CEDAF3" cx="2.5" cy="2.5" r="2.5"/>
+            </g>
+            <g stroke="#CEDAF3" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                <path d="M21 46h22M21 51h18"/>
+            </g>
+        </g>
+        <path fill="#041F74" d="M53.776 14.776H39V0z"/>
+        <g transform="translate(16 33)" stroke-width="3">
+            <circle stroke="#041F74" fill="#004DF5" cx="15.5" cy="15.5" r="14"/>
+            <g stroke="#FFF" stroke-linecap="round">
+                <path d="M9.5 21.5h12M15.5 17.5v-9M19.5 12.5l-4-4-4 4"/>
+            </g>
+        </g>
+    </g>
+</svg>
