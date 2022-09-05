90f24ba5a51d15bf9c5b7e22b3ae9bf509ae721f
roan <roan@sobit.nl>
2022-09-05 16:05:14 +0200

    Merged creathlon branch 2126 and fixed files

--- resources/views/livewire/teacher/tests-overview.blade.php
+++ resources/views/livewire/teacher/tests-overview.blade.php
@@ -13,9 +13,9 @@
      }"
      wire:init="handleReferrerActions()"
      class="flex flex-col  w-full min-h-full bg-lightGrey border-t border-secondary top-0"
-@checked="$event.detail ? checkedCount += 1 : checkedCount -= 1"
-@question-added.window="Notify.notify('Vraag toegevoegd!')"
-@question-removed.window="Notify.notify('Vraag verwijderd!')"
+     @checked="$event.detail ? checkedCount += 1 : checkedCount -= 1"
+     @question-added.window="Notify.notify('Vraag toegevoegd!')"
+     @question-removed.window="Notify.notify('Vraag verwijderd!')"
 >
     <x-menu.tab.container style="transition: top 0.3s linear;top: 150px"
                           x-on:tiles-hidden.window="$el.style.top = '100px'"
@@ -54,125 +54,124 @@
                 </div>
                 <div class="flex flex-wrap w-full gap-2 mt-2">
 
-                @if ($this->isExternalContentTab())
+                    @if ($this->isExternalContentTab())
+                        <x-input.choices-select
+                                wire:key="base_subject_{{ $this->openTab }}"
+                                :multiple="true"
+                                :options="$this->basesubjects"
+                                :withSearch="true"
+                                placeholderText="{{ __('general.Categorie') }}"
+                                wire:model="filters.{{ $this->openTab }}.base_subject_id"
+                                filterContainer="questionbank-{{ $this->openTab }}-active-filters"
+                        />
+                    @else
+                        <x-input.choices-select
+                                wire:key="subject_{{ $this->openTab }}"
+                                :multiple="true"
+                                :options="$this->subjects"
+                                :withSearch="true"
+                                placeholderText="{{ __('student.subject') }}"
+                                wire:model="filters.{{ $this->openTab }}.subject_id"
+                                filterContainer="questionbank-{{ $this->openTab }}-active-filters"
+                        />
+                    @endif
                     <x-input.choices-select
-                            wire:key="base_subject_{{ $this->openTab }}"
+                            wire:key="education_level_year_{{ $this->openTab }}"
                             :multiple="true"
-                            :options="$this->basesubjects"
+                            :options="$this->educationLevelYear"
                             :withSearch="true"
-                            placeholderText="{{ __('general.Categorie') }}"
-                            wire:model="filters.{{ $this->openTab }}.base_subject_id"
+                            placeholderText="{{ __('general.Leerjaar') }}"
+                            wire:model="filters.{{ $this->openTab }}.education_level_year"
                             filterContainer="questionbank-{{ $this->openTab }}-active-filters"
                     />
-                @else
                     <x-input.choices-select
-                            wire:key="subject_{{ $this->openTab }}"
+                            wire:key="educationLevel_{{ $this->openTab }}"
                             :multiple="true"
-                            :options="$this->subjects"
+                            :options="$this->educationLevel"
                             :withSearch="true"
-                            placeholderText="{{ __('student.subject') }}"
-                            wire:model="filters.{{ $this->openTab }}.subject_id"
+                            placeholderText="{{ __('general.Niveau') }}"
+                            wire:model="filters.{{ $this->openTab }}.education_level_id"
                             filterContainer="questionbank-{{ $this->openTab }}-active-filters"
                     />
-                @endif
-                <x-input.choices-select
-                        wire:key="education_level_year_{{ $this->openTab }}"
-                        :multiple="true"
-                        :options="$this->educationLevelYear"
-                        :withSearch="true"
-                        placeholderText="{{ __('general.Leerjaar') }}"
-                        wire:model="filters.{{ $this->openTab }}.education_level_year"
-                        filterContainer="questionbank-{{ $this->openTab }}-active-filters"
-                />
-                <x-input.choices-select
-                        wire:key="educationLevel_{{ $this->openTab }}"
-                        :multiple="true"
-                        :options="$this->educationLevel"
-                        :withSearch="true"
-                        placeholderText="{{ __('general.Niveau') }}"
-                        wire:model="filters.{{ $this->openTab }}.education_level_id"
-                        filterContainer="questionbank-{{ $this->openTab }}-active-filters"
-                />
-                @if ($this->canFilterOnAuthors())
-                    <x-input.choices-select
-                            wire:key="authors_{{ $this->openTab }}"
-                            :multiple="true"
-                            :options="$this->authors"
-                            :withSearch="true"
-                            placeholderText="{{ __('general.Auteurs') }}"
-                            wire:model="filters.{{ $this->openTab }}.author_id"
-                            filterContainer="questionbank-{{ $this->openTab }}-active-filters"
-                    />
-                @endif
+                    @if ($this->canFilterOnAuthors())
+                        <x-input.choices-select
+                                wire:key="authors_{{ $this->openTab }}"
+                                :multiple="true"
+                                :options="$this->authors"
+                                :withSearch="true"
+                                placeholderText="{{ __('general.Auteurs') }}"
+                                wire:model="filters.{{ $this->openTab }}.author_id"
+                                filterContainer="questionbank-{{ $this->openTab }}-active-filters"
+                        />
+                    @endif
 
-                @if($this->hasActiveFilters())
-                    <x-button.text-button class="ml-auto text-base"
-                                          size="sm"
-                                          @click="$dispatch('enable-loading-grid');document.getElementById('questionbank-{{ $this->openTab }}-active-filters').innerHTML = '';"
-                                          wire:click="clearFilters('{{ $this->openTab }}')"
-                                          wire:key="clearfilters-{{ $this->openTab }}"
-                    >
-                        <span class="min-w-max">{{ __('teacher.Filters wissen') }}</span>
-                        <x-icon.close-small/>
-                    </x-button.text-button>
-                @else
-                    <x-button.text-button class="ml-auto text-base disabled"
-                                          size="sm"
-                                          disabled
-                                          wire:key="clearfilters-disabled-{{ $this->openTab }}"
-                    >
-                        <span class="min-w-max">{{ __('teacher.Filters wissen') }}</span>
-                        <x-icon.close-small/>
-                    </x-button.text-button>
-                @endif
+                    @if($this->hasActiveFilters())
+                        <x-button.text-button class="ml-auto text-base"
+                                              size="sm"
+                                              @click="$dispatch('enable-loading-grid');document.getElementById('questionbank-{{ $this->openTab }}-active-filters').innerHTML = '';"
+                                              wire:click="clearFilters('{{ $this->openTab }}')"
+                                              wire:key="clearfilters-{{ $this->openTab }}"
+                        >
+                            <span class="min-w-max">{{ __('teacher.Filters wissen') }}</span>
+                            <x-icon.close-small/>
+                        </x-button.text-button>
+                    @else
+                        <x-button.text-button class="ml-auto text-base disabled"
+                                              size="sm"
+                                              disabled
+                                              wire:key="clearfilters-disabled-{{ $this->openTab }}"
+                        >
+                            <span class="min-w-max">{{ __('teacher.Filters wissen') }}</span>
+                            <x-icon.close-small/>
+                        </x-button.text-button>
+                    @endif
+                </div>
+                <div id="questionbank-{{ $this->openTab }}-active-filters"
+                     wire:ignore
+
+                     x-data=""
+                     :class="($el.childElementCount !== 1) ? 'mt-2' : ''"
+                     class="flex flex-wrap gap-2"
+                >
+                </div>
             </div>
-            <div id="questionbank-{{ $this->openTab }}-active-filters"
-                 wire:ignore
 
-                 x-data=""
-                 :class="($el.childElementCount !== 1) ? 'mt-2' : ''"
-                 class="flex flex-wrap gap-2"
-            >
-            </div>
-        </div>
-
-        {{-- Content --}}
-        <div class="flex flex-col pt-4 pb-16" style="min-height: 500px">
-            <div class="flex justify-between">
+            {{-- Content --}}
+            <div class="flex flex-col pt-4 pb-16" style="min-height: 500px">
+                <div class="flex justify-between">
                     <span class="note text-sm" wire:loading
                           wire:target="filters,clearFilters,$set">{{  __('general.searching') }}</span>
 
-                <span class="note text-sm"
-                      wire:loading.remove
-                      wire:target="filters,clearFilters,$set">
+                    <span class="note text-sm"
+                          wire:loading.remove
+                          wire:target="filters,clearFilters,$set">
                         {{ trans_choice($this->getMessageKey($results->total()), $results->total(), ['count' => $results->total()]) }}
                     </span>
-                <div class="flex space-x-2.5">
-                    <x-button.cta class="px-4" wire:click="$emit('openModal', 'teacher.test-start-create-modal')">
-                        <x-icon.plus-2/>
-                        <span>{{ __('general.create test') }}</span>
-                    </x-button.cta>
+                    <div class="flex space-x-2.5">
+                        <x-button.cta class="px-4" wire:click="$emit('openModal', 'teacher.test-start-create-modal')">
+                            <x-icon.plus-2/>
+                            <span>{{ __('general.create test') }}</span>
+                        </x-button.cta>
+                    </div>
                 </div>
+                <x-grid class="my-4">
+                    @foreach(range(1, 6) as $value)
+                        <x-grid.loading-card
+                                :delay="$value"
+                                wire:loading.class.remove="hidden"
+                                wire:target="filters,clearFilters,$set"
+                        />
+                    @endforeach
+
+                    @foreach($results as $test)
+                        <x-grid.test-card :test="$test"/>
+                    @endforeach
+                </x-grid>
+                {{ $results->links('components.partials.tc-paginator') }}
+
+                <livewire:teacher.tests-overview-context-menu/>
             </div>
-            <x-grid class="my-4">
-                @foreach(range(1, 6) as $value)
-                    <x-grid.loading-card
-                            :delay="$value"
-                            wire:loading.class.remove="hidden"
-                            wire:target="filters,clearFilters,$set"
-                    />
-                @endforeach
-
-                @foreach($results as $test)
-                    <x-grid.test-card :test="$test"/>
-                @endforeach
-            </x-grid>
-            {{ $results->links('components.partials.tc-paginator') }}
-
-            <livewire:teacher.tests-overview-context-menu/>
         </div>
     </div>
-</div>
-<x-notification/>
-
+    <x-notification/>
 </div>
