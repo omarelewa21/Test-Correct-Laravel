8dea16620c7956cbb2c91ea482611abb669502d8
Erik | Sobit <Erik@sobit.nl>
2022-09-21 13:12:58 +0200

    moved publishTest from saving to saved

--- app/Test.php
+++ app/Test.php
@@ -83,10 +83,11 @@ class Test extends BaseModel
             if ((count($dirty) > 1 && array_key_exists('system_test_id', $dirty)) || (count($dirty) > 0 && !array_key_exists('system_test_id', $dirty)) && !$test->getAttribute('is_system_test')) {
                 $test->setAttribute('system_test_id', null);
             }
-            $test->handleTestPublishing();
+
         });
 
         static::saved(function (Test $test) {
+            $test->handleTestPublishing(); // moved from saving as there is no id yet, which is needed to populate the test_authors table
             $dirty = $test->getDirty();
             if ($test->isDirty(['subject_id', 'education_level_id', 'education_level_year'])) {
                 $testQuestions = $test->testQuestions;
