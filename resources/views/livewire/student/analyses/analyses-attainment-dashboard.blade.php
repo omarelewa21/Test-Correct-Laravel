a785cfc936510a73b3b8c6d4f0f11caf20dd550e
Martin Folkerts <martinfolkerts@iMac-van-Martin.local>
2022-10-07 10:20:51 +0200

    tcp-2282 fixed title for analyses attainment dashboard (no more json)

--- resources/views/livewire/student/analyses/analyses-attainment-dashboard.blade.php
+++ resources/views/livewire/student/analyses/analyses-attainment-dashboard.blade.php
@@ -13,7 +13,7 @@
 
 @section('analyses.page.title')
     <div class="flex pt-5 justify-between">
-        <h1 class="flex pt-5"> {{ $attainment }} </h1>
+        <h1 class="flex pt-5"> {{ $attainment->name }} </h1>
         <x-button.primary class="hidden bg-purple-900 flex">Exporteren</x-button.primary>
     </div>
 @endsection
