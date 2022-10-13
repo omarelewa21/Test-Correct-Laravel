3ec0db9287767cf8990d7ba72981df0c0dc23ff1
Erik | Sobit <Erik@sobit.nl>
2022-10-13 13:24:59 +0200

    make sure the cake environment gets the same result as the laravel environment for planning a test take

--- app/Http/Controllers/InvigilatorsController.php
+++ app/Http/Controllers/InvigilatorsController.php
@@ -7,6 +7,7 @@ use tcCore\Http\Requests;
 use tcCore\Http\Controllers\Controller;
 use tcCore\Role;
 use tcCore\SchoolLocation;
+use tcCore\Teacher;
 use tcCore\User;
 use tcCore\UserRole;
 
@@ -30,40 +31,42 @@ class InvigilatorsController extends Controller
 
     public static function getInvigilatorList()
     {
-        $fields = ['id', 'name_first', 'name_suffix', 'name'];
+        $fields = ['id', 'name_first', 'name_suffix', 'name','uuid'];
 
-        $query = User::withTrashed()->orderBy('name', 'asc')
-            ->join(with(new UserRole())->getTable(), 'user_roles.user_id', '=', 'users.id')
-            ->whereIn('user_roles.role_id', function ($query) {
-                $query->select('id')
-                    ->from(with(new Role())->getTable())
-                    ->whereIn('name', ['Teacher', 'Invigilator'])
-                    ->where('deleted_at', null);
-            })->where('users.deleted_at', null)
-            ->where('user_roles.deleted_at', null);
+        $query = Teacher::getTeacherUsersForSchoolLocationInCurrentYear(Auth::user()->schoolLocation);
 
-        $user = Auth::user()->getAttributes();
-
-        if (array_key_exists('school_id',
-                $user) && $user['school_id'] !== null && array_key_exists('school_location_id',
-                $user) && $user['school_location_id'] !== null) {
-            $query->where(function ($query) use ($user) {
-                $query->where('school_id', $user['school_id'])
-                    ->orWhere('school_location_id', $user['school_location_id']);
-            });
-        } elseif (array_key_exists('school_id', $user) && $user['school_id'] !== null) {
-            $query->where('school_id', $user['school_id']);
-        } elseif (array_key_exists('school_location_id', $user) && $user['school_location_id'] !== null) {
-            $query->where('school_location_id', $user['school_location_id']);
-        } else {
-            return false;
-        }
-
-        $query->union(User::select($fields)->join(with(new UserRole())->getTable(), 'user_roles.user_id', '=',
-            'users.id')
-            ->whereIn('users.id',
-                DB::table('school_location_user')->select('user_id')->where('school_location_id', $user['school_location_id']))
-        );
+//        $query = User::withTrashed()->orderBy('name', 'asc')
+//            ->join(with(new UserRole())->getTable(), 'user_roles.user_id', '=', 'users.id')
+//            ->whereIn('user_roles.role_id', function ($query) {
+//                $query->select('id')
+//                    ->from(with(new Role())->getTable())
+//                    ->whereIn('name', ['Teacher', 'Invigilator'])
+//                    ->where('deleted_at', null);
+//            })->where('users.deleted_at', null)
+//            ->where('user_roles.deleted_at', null);
+//
+//        $user = Auth::user()->getAttributes();
+//
+//        if (array_key_exists('school_id',
+//                $user) && $user['school_id'] !== null && array_key_exists('school_location_id',
+//                $user) && $user['school_location_id'] !== null) {
+//            $query->where(function ($query) use ($user) {
+//                $query->where('school_id', $user['school_id'])
+//                    ->orWhere('school_location_id', $user['school_location_id']);
+//            });
+//        } elseif (array_key_exists('school_id', $user) && $user['school_id'] !== null) {
+//            $query->where('school_id', $user['school_id']);
+//        } elseif (array_key_exists('school_location_id', $user) && $user['school_location_id'] !== null) {
+//            $query->where('school_location_id', $user['school_location_id']);
+//        } else {
+//            return false;
+//        }
+//
+//        $query->union(User::select($fields)->join(with(new UserRole())->getTable(), 'user_roles.user_id', '=',
+//            'users.id')
+//            ->whereIn('users.id',
+//                DB::table('school_location_user')->select('user_id')->where('school_location_id', $user['school_location_id']))
+//        );
 
         return $query->get($fields)->keyBy('id');
     }
