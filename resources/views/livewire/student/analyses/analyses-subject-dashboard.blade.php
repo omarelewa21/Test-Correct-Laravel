364bb70d7280800bdd2a6fd33c5b5040fcb50a64
roan <roan@sobit.nl>
2022-10-14 16:09:38 +0200

    stretched available data on card

--- resources/views/livewire/student/analyses/analyses-subject-dashboard.blade.php
+++ resources/views/livewire/student/analyses/analyses-subject-dashboard.blade.php
@@ -27,7 +27,7 @@
         <div class="flex flex-row">
 
             @foreach(['tests', 'assignments'] as $kind)
-                <div class="md:w-1/3 mr-5 gap-4 flex flex-col" x-data="{pValue: {{ number_format($generalStats[$kind.'_pvalue_average'], 2)  }} }">
+                <div class="flex-1 gap-4 flex flex-col" x-data="{pValue: {{ number_format($generalStats[$kind.'_pvalue_average'], 2)  }} }">
                     <div class="flex flex-col">
                         <span>{{ __('student.aantal '. $kind. ' gemaakt')}}</span>
                         <span class="bold">{{ $generalStats[$kind.'_taken'] }}</span>
