834dd5736f9ee5959118fb4322fb75a16af05761
roan <roan@sobit.nl>
2022-10-17 10:45:16 +0200

    Passing the is_assignment variable to the teacher planned test email template; Added conditional for teacher planned test email subject;

--- app/Jobs/SendTestPlannedMail.php
+++ app/Jobs/SendTestPlannedMail.php
@@ -7,7 +7,6 @@ use Illuminate\Mail\Mailer;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use tcCore\TestKind;
 use tcCore\TestTake;
 
 class SendTestPlannedMail extends Job implements ShouldQueue
@@ -37,10 +36,10 @@ class SendTestPlannedMail extends Job implements ShouldQueue
     public function handle(Mailer $mailer)
     {
         try {
-            $testTake   = TestTake::findOrFail($this->testTakeId);
+            $testTake = TestTake::findOrFail($this->testTakeId);
             $takeCode = $testTake->testTakeCode;
             $is_assessment = $testTake->isAssessmentType();
-            if($takeCode){
+            if ($takeCode) {
                 $takeCode = $takeCode->prefix . '  ' . implode(' ', str_split($takeCode->code));
             }
         } catch (ModelNotFoundException $e) {
@@ -48,8 +47,8 @@ class SendTestPlannedMail extends Job implements ShouldQueue
         }
 
         if ($testTake->testTakeStatus->name === 'Taking test' && $is_assessment) {
-            foreach($testTake->testParticipants as $testParticipant) {
-                if(null == $testParticipant->user || $testParticipant->user->shouldNotSendMail()) {
+            foreach ($testTake->testParticipants as $testParticipant) {
+                if (null == $testParticipant->user || $testParticipant->user->shouldNotSendMail()) {
                     continue;
                 }
                 $mailer->send('emails.assignment_planned', ['testParticipant' => $testParticipant, 'directlink' => $testTake->directLink, 'takeCode' => $takeCode], function ($mail) use ($testParticipant) {
@@ -60,32 +59,32 @@ class SendTestPlannedMail extends Job implements ShouldQueue
 
         if ($testTake->testTakeStatus->name === 'Planned') {
             // Send to students
-            foreach($testTake->testParticipants as $testParticipant) {
-                if(null == $testParticipant->user || $testParticipant->user->shouldNotSendMail()) {
+            foreach ($testTake->testParticipants as $testParticipant) {
+                if (null == $testParticipant->user || $testParticipant->user->shouldNotSendMail()) {
                     continue;
                 }
                 $mailer->send('emails.test_planned',
                     ['testParticipant' => $testParticipant, 'directlink' => $testTake->directLink, 'takeCode' => $takeCode, 'is_assessment' => $is_assessment],
                     function ($mail) use ($testParticipant, $is_assessment) {
-                        $mail->to($testParticipant->user->username, $testParticipant->user->getNameFullAttribute())->subject( $is_assessment ? __('test_planned.assignment_planned') : __('test_planned.Toetsafname ingepland.'));
-                });
+                        $mail->to($testParticipant->user->username, $testParticipant->user->getNameFullAttribute())->subject($is_assessment ? __('test_planned.assignment_planned') : __('test_planned.Toetsafname ingepland.'));
+                    });
             }
             // Send to Invigilators
-            foreach($testTake->invigilators as $invigilator){
-                if($invigilator->user->username !== $testTake->user->username){
+            foreach ($testTake->invigilators as $invigilator) {
+                if ($invigilator->user->username !== $testTake->user->username) {
                     $mailer->send('emails.teacher_test_planned',
                         ['user' => $invigilator->user, 'testTake' => $testTake, 'directlink' => $testTake->directLink, 'is_invigilator' => true, 'takeCode' => $takeCode, 'is_assessment' => $is_assessment],
                         function ($mail) use ($invigilator, $is_assessment) {
                             $mail->to($invigilator->user->username, $invigilator->user->getNameFullAttribute())->subject($is_assessment ? __('test_planned.assignment_planned') : __('test_planned.Toetsafname ingepland.'));
-                    });
+                        });
                 }
             }
             // Send to test owner
             $mailer->send('emails.teacher_test_planned',
-                ['user' => $testTake->user, 'testTake' => $testTake, 'directlink' => $testTake->directLink, 'is_invigilator' => false, 'takeCode' => $takeCode],
-                function ($mail) use ($testTake) {
-                    $mail->to($testTake->user->username, $testTake->user->getNameFullAttribute())->subject(__('test_planned.Toetsafname ingepland.'));
-            });
+                ['user' => $testTake->user, 'testTake' => $testTake, 'directlink' => $testTake->directLink, 'is_invigilator' => false, 'takeCode' => $takeCode, 'is_assessment' => $is_assessment],
+                function ($mail) use ($is_assessment, $testTake) {
+                    $mail->to($testTake->user->username, $testTake->user->getNameFullAttribute())->subject($is_assessment ? __('test_planned.assignment_planned') : __('test_planned.Toetsafname ingepland.'));
+                });
         }
     }
 }
