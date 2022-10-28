326e693012bf0458613e946e6f6ad02108d921a6
Martin Folkerts <martinfolkerts@iMac-van-Martin.local>
2022-10-28 16:15:22 +0200

    created x-empty-graph component

--- resources/views/livewire/student/analyses/analyses-attainment-dashboard.blade.php
+++ resources/views/livewire/student/analyses/analyses-attainment-dashboard.blade.php
@@ -33,7 +33,9 @@
             {{ __('student.p waarde subleerdoelen') }}
         </x-slot>
 
-        <div id="pValueChart" style="height: 400px;"></div>
+        <div id="pValueChart" style="height: 400px;" class="relative">
+            <x-empty-graph></x-empty-graph>
+        </div>
         <div x-data="analysesAttainmentsGraph( @entangle('dataValues') )"
              x-on:filters-updated.window="renderGraph"
         >
