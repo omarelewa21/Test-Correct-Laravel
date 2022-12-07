c2211de52ebab2aa5a99d0202888b5598d7f8917
Omar Elewa <omar.elewa@test-correct.nl>
2022-12-07 22:54:47 +0200

    Detect os and os version

--- app/Http/Helpers/AppVersionDetector.php
+++ app/Http/Helpers/AppVersionDetector.php
@@ -341,8 +341,8 @@ class AppVersionDetector
 
         session([
             'headers' => $headers,
-            'TLCVersion' => $version['app_version'],
-            'TLCOs' => $version['os'],
+            'TLCVersion' => self::getOSVersion(),
+            'TLCOs' => self::getOSPlatform(),
             'TLCIsIos12' => (Str::lower($version['os']) === 'ios') ? AppVersionDetector::isIos12($headers) : false,
         ]);
 
@@ -356,8 +356,68 @@ class AppVersionDetector
 //        $this->Session->write('TLCVersionCheckResult', $versionCheckResult);
     }
 
+    public static function getOSPlatform() 
+    { 
+        $headers = self::getAllHeaders();
+        $user_agent = $headers['user-agent'];
+        $os_platform  = "Unknown OS Platform"; 
+        $os_array     = array(
+                              '/windows nt 10/i'      =>  'Windows 10',
+                              '/windows nt 6.3/i'     =>  'Windows 8.1',
+                              '/windows nt 6.2/i'     =>  'Windows 8',
+                              '/windows nt 6.1/i'     =>  'Windows 7',
+                              '/windows nt 6.0/i'     =>  'Windows Vista',
+                              '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                              '/windows nt 5.1/i'     =>  'Windows XP',
+                              '/windows xp/i'         =>  'Windows XP',
+                              '/windows nt 5.0/i'     =>  'Windows 2000',
+                              '/windows me/i'         =>  'Windows ME',
+                              '/win98/i'              =>  'Windows 98',
+                              '/win95/i'              =>  'Windows 95',
+                              '/win16/i'              =>  'Windows 3.11',
+                              '/macintosh|mac os x/i' =>  'Mac OS X',
+                              '/mac_powerpc/i'        =>  'Mac OS 9',
+                              '/linux/i'              =>  'Linux',
+                              '/ubuntu/i'             =>  'Ubuntu',
+                              '/iphone/i'             =>  'iPhone',
+                              '/ipod/i'               =>  'iPod',
+                              '/ipad/i'               =>  'iPad',
+                              '/android/i'            =>  'Android',
+                              '/blackberry/i'         =>  'BlackBerry',
+                              '/webos/i'              =>  'Mobile'
+                        );
+    
+        foreach ($os_array as $regex => $value)
+            if (preg_match($regex, $user_agent))
+                $os_platform = $value;
+    
+        return $os_platform;
+    }
+
+    public static function getOSVersion()
+    {
+        $headers = self::getAllHeaders();
+        $version = AppVersionDetector::detect($headers);
+        $iosRegularExpression = '/ip(?:hone|[ao]d) os \K[\d_]+/i';
+        $androidRegularExpression = '/Android ((\d+|\.)+[^,;]+)/';
+        $user_agent = $headers['user-agent'];
+
+        if(preg_match($iosRegularExpression, $user_agent, $matches, PREG_OFFSET_CAPTURE, 0)) {
+            $version = $matches[0][0];
+        } elseif(preg_match($androidRegularExpression, $user_agent, $matches)) {
+            $version = $matches[1];
+        } else {
+            $version = $version['os'];
+        } 
+
+        return $version;
+    }
+
     public function getAppVersion(){
         AppVersionDetector::handleHeaderCheck();
+
+
+        return session()->all();
         return ['TLCVersion' => session('TLCVersion', null)];
     }
 }
