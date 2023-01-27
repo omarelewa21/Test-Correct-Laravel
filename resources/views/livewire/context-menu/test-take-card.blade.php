188b0fb97e162ebe5086403a6aa6a073eadd43bb
Omar Elewa <omar.elewa@test-correct.nl>
2023-01-27 16:33:03 +0200

    split with x-show

--- resources/views/livewire/context-menu/test-take-card.blade.php
+++ resources/views/livewire/context-menu/test-take-card.blade.php
@@ -1,41 +1,47 @@
 <x-menu.context-menu.base context="test-take-card" class="w-60">
 
     @if($this->hasArchiveOption())
-        <x-menu.context-menu.button wire:click="copyTestTakeLink">
-            <x-slot name="icon"><x-icon.copy/></x-slot>
-            <x-slot name="text">{{ __('test-take.copy-link') }}</x-slot>
-        </x-menu.context-menu.button>
-        <x-menu.context-menu.button wire:click="goToCoLearningPage">
-            <x-slot name="icon"><x-icon.filled-arrow/></x-slot>
-            <x-slot name="text">{{ __('test-take.CO-Learning') }}</x-slot>
-        </x-menu.context-menu.button>
-        <x-menu.context-menu.button wire:click="goToScheduleMakeUpPage">
-            <x-slot name="icon"><x-icon.schedule/></x-slot>
-            <x-slot name="text">{{ __('test-take.schedule-make-up-test') }}</x-slot>
-        </x-menu.context-menu.button>
-        @if($this->hasAnswerPdfOption())
-            <x-menu.context-menu.button wire:click="studentAnswersPdf">
-                <x-slot name="icon"><x-icon.pdf-file/></x-slot>
-                <x-slot name="text">{{ __('test-take.Antwoord PDF') }}</x-slot>
+        <div x-show="openTab == 'taken'">
+            <x-menu.context-menu.button wire:click="copyTestTakeLink">
+                <x-slot name="icon"><x-icon.copy/></x-slot>
+                <x-slot name="text">{{ __('test-take.copy-link') }}</x-slot>
             </x-menu.context-menu.button>
-            <x-menu.context-menu.button wire:click="updateStatusToTaken">
-                <x-slot name="icon"><x-icon.arrow/></x-slot>
-                <x-slot name="text">{{ __('test_take.update_to_taken') }}</x-slot>
+            <x-menu.context-menu.button wire:click="goToCoLearningPage">
+                <x-slot name="icon"><x-icon.filled-arrow/></x-slot>
+                <x-slot name="text">{{ __('test-take.CO-Learning') }}</x-slot>
             </x-menu.context-menu.button>
-        @endif
-        @if($this->hasSkipDiscussing())
-            <x-menu.context-menu.button wire:click="skipDiscussing">
-                <x-slot name="icon"><x-icon.grading/></x-slot>
-                <x-slot name="text">{{ __('test-take.Direct nakijken') }}</x-slot>
+            <x-menu.context-menu.button wire:click="goToScheduleMakeUpPage">
+                <x-slot name="icon"><x-icon.schedule/></x-slot>
+                <x-slot name="text">{{ __('test-take.schedule-make-up-test') }}</x-slot>
             </x-menu.context-menu.button>
-        @endif
+            @if($this->hasSkipDiscussing())
+                <x-menu.context-menu.button wire:click="skipDiscussing">
+                    <x-slot name="icon"><x-icon.grading/></x-slot>
+                    <x-slot name="text">{{ __('test-take.Direct nakijken') }}</x-slot>
+                </x-menu.context-menu.button>
+            @endif
+            <x-copy-to-clipboard/>
+        </div>
+
+        <div x-show="openTab == 'norm'">
+            @if($this->hasAnswerPdfOption())
+                <x-menu.context-menu.button wire:click="studentAnswersPdf">
+                    <x-slot name="icon"><x-icon.pdf-file/></x-slot>
+                    <x-slot name="text">{{ __('test-take.Antwoord PDF') }}</x-slot>
+                </x-menu.context-menu.button>
+                <x-menu.context-menu.button wire:click="updateStatusToTaken">
+                    <x-slot name="icon"><x-icon.arrow/></x-slot>
+                    <x-slot name="text">{{ __('test_take.update_to_taken') }}</x-slot>
+                </x-menu.context-menu.button>
+            @endif
+        </div>
+
         <x-menu.context-menu.button wire:click="archive">
             <x-slot name="icon"><x-icon.archive/></x-slot>
             <x-slot name="text">{{ __('test-take.Archiveren') }}</x-slot>
         </x-menu.context-menu.button>
-
-        <x-copy-to-clipboard/>  
     @endif
+
     @if($this->hasUnarchiveOption())
         <x-menu.context-menu.button wire:click="unarchive">
             <x-slot name="icon"><x-icon.archive/></x-slot>
