c6e12d783e41c6cf9e77f2e24a6ed1b6db12023e
Omar Elewa <omar.elewa@test-correct.nl>
2023-02-15 09:53:12 +0200

    Test case for number of participants

---
+++ tests/Unit/Http/Controllers/TestTakes/TestTakeGenerateParticipantsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers\TestTakes;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use tcCore\Factories\FactoryTestTake;
+use tcCore\FactoryScenarios\FactoryScenarioTestTakePlanned;
+use tests\TestCase;
+
+class TestTakeGenerateParticipantsTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected FactoryTestTake $testTakeFactory;
+    const STUDENTS_COUNT = 5;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->testTakeFactory = FactoryScenarioTestTakePlanned::create($this->getTeacherOne())->testTakeFactory;
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_factory_generateMany()
+    {
+        $testTake = $this->testTakeFactory->testTake;
+        $this->assertEquals(self::STUDENTS_COUNT, $testTake->testParticipants()->count());
+    }
+}
