386ac5a0275357f67d761fed7ea94fd43c5a6270
jonnevantleven <jonne@sobit.nl>
2023-03-09 13:53:41 +0100

    added logs:clear artisan command to clear the laravel.log file

---
+++ app/Console/Commands/ClearLaravelLog.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace tcCore\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class ClearLaravelLog extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'logs:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear the Laravel.log file';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if(Storage::disk('logs')->put('laravel.log', '')) {
+            $this->info('The laravel.log file has been cleared succesfully.');
+            return Command::SUCCESS;
+        }
+        $this->error('Something went wrong clearning the laravel.log file.');
+        return Command::FAILURE;
+    }
+}
