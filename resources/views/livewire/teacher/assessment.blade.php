009fe2638d8d6291e9dca8910a73c41c21aecf59
Roan <roan@sobit.nl>
2023-04-06 10:11:00 +0200

    Added student name to answer model when toggled on; Refactored tooltips into view and alpine components;

--- resources/views/livewire/teacher/assessment.blade.php
+++ resources/views/livewire/teacher/assessment.blade.php
@@ -141,16 +141,31 @@
                         >
                             <x-slot:title>
                                 <div class="question-indicator items-center flex gap-4">
-                                    <h4 class="flex items-center" selid="questiontitle">
+                                    <h4 class="flex items-center flex-wrap" selid="questiontitle">
                                         <span>@lang('co-learning.answer')</span>
+                                        @if($this->assessmentContext['showStudentNames'])
+                                            <span class="ml-2 truncate max-w-[170px]">{{ $this->currentAnswer->user->name_first }}</span>
+                                            <span x-bind:class="{'ml-2': $el.previousElementSibling.offsetWidth < 170}">{{ $this->currentAnswer->user->shortLastname }}</span>
+                                        @endif
                                         <span>:</span>
                                         <span class="ml-2">{{ $this->currentQuestion->type_name }}</span>
                                     </h4>
-                                    <h7 class="inline-block">{{ $this->currentQuestion->score }} pt</h7>
+                                    <h7 class="inline-block min-w-fit">{{ $this->currentQuestion->score }} pt</h7>
                                 </div>
                             </x-slot:title>
                             <x-slot:titleLeft>
-                                <div class="ml-auto mr-6 relative top-0.5">
+                                <div class="ml-auto mr-6 relative top-0.5 flex gap-2 items-center">
+                                    <span x-on:click.stop.prevent="">
+                                        <x-tooltip class="w-[40px] h-[30px]" :icon-height="true" :icon-width="true">
+                                            <x-slot:idleIcon>
+                                                <span class="flex items-center" x-show="!tooltip" x-cloak>
+                                                    <x-icon.profile />
+                                                    <x-icon.questionmark-small/>
+                                                </span>
+                                            </x-slot:idleIcon>
+                                            {{ $this->currentAnswer->user->nameFull }}
+                                        </x-tooltip>
+                                    </span>
                                     <x-dynamic-component :component="$this->currentAnswer->answeredStatus" />
                                 </div>
                             </x-slot:titleLeft>
@@ -250,6 +265,7 @@
                     <div id="slide-container"
                          class="slide-container | flex h-full max-w-[var(--sidebar-width)] overflow-x-hidden overflow-y-auto"
                          wire:ignore.self
+                         x-on:scroll="closeTooltips()"
                     >
                         <div class="slide-1 scoring | p-6 flex-[1_0_100%] h-fit min-h-full w-[var(--sidebar-width)] space-y-4 isolate">
                             <div class="question-indicator | items-center flex w-full">
@@ -357,7 +373,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="slide-3 co-learning | p-6 flex-[1_0_100%] h-fit min-h-full w-[var(--sidebar-width)] space-y-4 isolate">
+                        <div class="slide-3 co-learning | p-6 flex-[1_0_100%] h-fit min-h-full w-[var(--sidebar-width)] space-y-4">
                             <div class="flex flex-col w-full gap-2">
                                 <span class="flex ">@lang('assessment.CO-Learning scores')</span>
                                 @if(!$this->currentAnswerCoLearningRatingsHasNoDiscrepancy())
