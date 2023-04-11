5f7ba5702a891f83b0cf610f5e10c4e8c96c30db
Martin Folkerts <martinfolkerts@iMac-van-Martin.local>
2023-04-11 12:08:51 +0200

    added bugsnag Id to error message and extra layout

--- resources/lang/nl/app.php
+++ resources/lang/nl/app.php
@@ -1,6 +1,7 @@
 <?php
-	return [
-			'Inleveren' => 'Inleveren',
-			'Ga naar dashboard' => 'Ga naar dashboard',
+return [
+    'Inleveren'         => 'Inleveren',
+    'Ga naar dashboard' => 'Ga naar dashboard',
+    'oeps'              => 'Oeps, er is iets misgegaan. Geen zorgen. Het ligt aan ons, niet aan jou. Gebruik de knop om naar het dashboard terug te keren.',
+    'send to support'   => 'stuur deze code naar support',
 ];
-?>
--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,8 +1,8 @@
 <x-layouts.base>
     <div class="flex items-center justify-center h-screen">
         {{-- top content block height:120px --}}
-        <div class="bg-white rounded-t-[10px] px-10 pt-[31px] w-[480px] pb-0 shadow-lg flex flex-col relative ">
-            <div class="absolute top-[-100px] left-1/2 -translate-x-1/2 flex">
+        <div class="bg-white rounded-t-[10px] px-10 pt-[180px] w-[480px] pb-0 shadow-lg flex flex-col relative ">
+            <div class="absolute top-[-100px] left-[225px] -translate-x-1/2 flex">
                 <div class="w-[368px] w-[342px]">
                     <x-animations.error-pages/>
                 </div>
@@ -15,10 +15,14 @@
 
 
                 <div class="flex flex-col mt-auto pt-4 justify-center">
-                    <div >
-                        <h3 class="text-center text-[64px] leading-none">@yield('code')</h3>
+                    <div>
+                        <h3 class="text-center text-[64px] mb-2 leading-none">@yield('code')</h3>
 
-                        <h4 class="text-center text-[24px] leading-none">@yield('message')</h4>
+                        <h4 class="text-center text-[24px] mb-2 leading-none">@yield('message')</h4>
+
+                        <p class="text-center">
+                            {{ __('app.oeps') }}
+                        </p>
 
                     </div>
                     <div class="mt-4">
@@ -32,6 +36,9 @@
                             <x-icon.arrow-left></x-icon.arrow-left><span>{{ __('app.Ga naar dashboard') }}</span>
                         </x-button.cta>
                     </div>
+                    <p class="note text-center mt-4 ">{{ __('app.send to support') }}<BR>
+                        {{ \Request::get('error_id') }}
+                    </p>
                 </div>
 
             </div>
