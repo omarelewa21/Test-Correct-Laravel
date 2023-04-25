7e0ea176bfeae6a32807c3bfc0bf1f7c9a9b8645
jonnevantleven <jonne@sobit.nl>
2023-04-25 15:40:50 +0200

    changed pdf attachment viewer default scaling to 75%

--- public/pdf/viewer.js
+++ public/pdf/viewer.js
@@ -99,7 +99,7 @@ var _pdfjsLib = __webpack_require__(1);
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var CSS_UNITS = 96.0 / 72.0;
-var DEFAULT_SCALE_VALUE = 'auto';
+var DEFAULT_SCALE_VALUE = 0.75;
 var DEFAULT_SCALE = 1.0;
 var MIN_SCALE = 0.25;
 var MAX_SCALE = 10.0;
