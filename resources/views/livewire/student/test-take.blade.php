c837658f94f524162f2398aea2a0a59bdebc2d34
Omar Elewa <omar.elewa@test-correct.nl>
2023-05-02 06:23:34 +0200

    adjust the modal accroding to the design

--- resources/views/livewire/student/test-take.blade.php
+++ resources/views/livewire/student/test-take.blade.php
@@ -1,23 +1,33 @@
 <div x-data="{}" x-init="Core.init(); $wire.showAssignmentElements()" testtakemanager>
-    <x-modal maxWidth="lg" wire:model="showTurnInModal">
+    <x-modal maxWidth="2xl" :customFooter="true" wire:model="showTurnInModal">
         <x-slot name="title">{{ __("test-take.Toets inleveren") }}</x-slot>
         <x-slot name="body">
-            {{ __("test-take.Weet je zeker dat je de toets wilt inleveren?") }}
             @if (!$this->isAllQuestionsHaveAnswer())
-                <div class="flex w-full justify-center transition-all duration-300 mb-4 mt-5">
+                <div class="flex w-full justify-center transition-all duration-300">
                     <div class="notification warning stretched">
+                        <strong>  {{ __("test-take.attention-not-all-questions-answered") }} </strong> <br>
                         {{ __("test-take.not-all-questions-answered") }}
-                        <strong> {{ $questionsWithNoAnswer }} </strong>. <br>
+                        <strong>{{ $questionsWithNoAnswer }}</strong>.
                         {{ __("test-take.not-all-questions-answered-extension") }}
                     </div>
                 </div>
             @endif
         </x-slot>
-        <x-slot name="actionButton">
-            <x-button.cta size="md" onclick="endTest()">
-                <span>{{ __("test-take.Inleveren") }}</span>
-                <x-icon.arrow/>
-            </x-button.cta>
+         <x-slot name="customFooter">
+            <div class="flex justify-between items-center mt-6 w-full p-2">
+                <div>
+                    {{ __('test-take.Weet je zeker dat je de toets wilt inleveren?') }}
+                </div>
+                <div class="relative left-3">
+                    <x-button.text-button @click="show = false" class="rotate-svg-180 mr-2">
+                        <span>{{ __('modal.cancel') }}</span>
+                    </x-button.text-button>
+                    <x-button.cta size="md" onclick="endTest()">
+                        <span>{{ __('test-take.Inleveren') }}</span>
+                        <x-icon.checkmark-small />
+                    </x-button.cta>
+                </div>
+            <div>
         </x-slot>
     </x-modal>
 
