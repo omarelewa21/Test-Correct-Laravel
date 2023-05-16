df8a3ddaf385c2dbb2055e142f15b72754ed9466
Roan <roan@sobit.nl>
2023-05-16 14:14:44 +0200

    Fixed preview icon fill

--- resources/views/components/icon/preview.blade.php
+++ resources/views/components/icon/preview.blade.php
@@ -3,6 +3,6 @@
         <path stroke="currentColor" d="M10 13c3.314 0 6.314-2 9-6-2.686-4-5.686-6-9-6S3.686 3 1 7c2.686 4 5.686 6 9 6z"
               stroke-width="2" stroke-linejoin="round"/>
         <circle stroke="currentColor" cx="10" cy="7" r="5.5"/>
-        <path stroke="currentColor" d="M10 4c.1 0 .197.005.294.014a1.5 1.5 0 101.26.42A3 3 0 1110 4z"/>
+        <path fill="currentColor" d="M10 4c.1 0 .197.005.294.014a1.5 1.5 0 101.26.42A3 3 0 1110 4z"/>
     </g>
 </svg>
