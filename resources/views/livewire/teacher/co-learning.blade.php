6fa2709f5a812e13c5c7d9b66ad10437824e0d99
jonnevantleven <jonne@sobit.nl>
2023-05-23 16:19:25 +0200

    fixed hide on smartboard button

--- resources/views/livewire/teacher/co-learning.blade.php
+++ resources/views/livewire/teacher/co-learning.blade.php
@@ -145,16 +145,6 @@
                                             <x-icon.on-smartboard-hide/>
                                         </div>
                                     </div>
-                                    <div class="hide-on-smartboard group"
-                                         @click.stop="showStudentAnswer = false"
-                                         wire:click.stop.prevent="resetActiveAnswer()"
-                                    >
-                                        <div class="group-hover:bg-primary group-hover:opacity-[0.05]"></div>
-                                        <template x-if="true">
-                                            <x-icon.on-smartboard-hide
-                                            />
-                                        </template>
-                                    </div>
                                 </x-slot:titleLeft>
                                 <x-slot:body>
                                     <div class="student-answer | w-full"
