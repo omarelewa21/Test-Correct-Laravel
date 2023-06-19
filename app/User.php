4190063fa49412bbe5e3d155aa05683671bfc8a2
Erik | Sobit <Erik@sobit.nl>
2023-06-19 12:51:19 +0200

    also convert the name to mb

--- app/User.php
+++ app/User.php
@@ -2698,7 +2698,7 @@ class User extends BaseModel implements AuthenticatableContract, CanResetPasswor
 
     public function getFormalNameAttribute()
     {
-        return sprintf('%s.%s', mb_substr($this->name_first, 0, 1, 'utf-8'), $this->name);
+        return sprintf('%s.%s', mb_substr($this->name_first, 0, 1, 'utf-8'), mb_convert_encoding($this->name, 'utf-8'));
     }
 
     public function getFormalNameWithCurrentSchoolLocationShortAttribute()
