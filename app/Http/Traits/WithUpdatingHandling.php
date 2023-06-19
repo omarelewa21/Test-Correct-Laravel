273a890a334fc07a8fbb01aa3dd91e7827c2b172
Omar Elewa <omar.elewa@test-correct.nl>
2023-06-20 02:49:17 +0300

    Make the variable general in order to fit in other components

--- app/Http/Traits/WithUpdatingHandling.php
+++ app/Http/Traits/WithUpdatingHandling.php
@@ -11,14 +11,13 @@ trait WithUpdatingHandling
 {
     public function updatedWithUpdatingHandling(&$name, &$value)
     {
-        if(!isset($this->preventAnswerTransformation) || !$this->preventAnswerTransformation) {
-            $refValue = $value;
-            Request::filter($value);
-            if($refValue !== $value) {
-                $this->syncInput($name, $value);
-            }
-        }
+        if($this->skipFieldTransformation($name)) return;
 
+        $refValue = $value;
+        Request::filter($value);
+        if($refValue !== $value) {
+            $this->syncInput($name, $value);
+        }
     }
 
     public function updateAnswerIdForTestParticipant()
@@ -26,5 +25,22 @@ trait WithUpdatingHandling
         $answer = Answer::select('id','test_participant_id')->whereId($this->answers[$this->question->uuid]['id'])->first();
 
         TestParticipant::whereId($answer->test_participant_id)->update(['answer_id' => $answer->getKey()]);
+    }
+
+    /**
+     * Prevents the transformation of the given fields Or all fields if preventFieldTransformation property is true.
+     * 
+     * @param $name
+     */
+    private function skipFieldTransformation($name)
+    {
+        if(isset($this->preventFieldTransformation)){
+            if(is_array($this->preventFieldTransformation)){
+                return in_array($name, $this->preventFieldTransformation);
+            }
+            return is_bool($this->preventFieldTransformation) ? $this->preventFieldTransformation : false;
+        }
+
+        return false;
     }
 }
