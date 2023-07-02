6001b266cb56d7a98d35ea22d8aabd7cf53cdaa2
Omar Elewa <omar.elewa@test-correct.nl>
2023-07-02 13:58:36 +0300

    apply purifying to all fields except for those that should be ignored

--- app/Traits/ModelAttributePurifyTrait.php
+++ app/Traits/ModelAttributePurifyTrait.php
@@ -1,17 +1,20 @@
 <?php
 
 namespace tcCore\Traits;
+
+use Illuminate\Support\Arr;
 use tcCore\Http\Requests\Request;
 use tcCore\Casts\PurifyAttributeCast;
 
 trait ModelAttributePurifyTrait
 {
-    protected $purifyAttributes = [];
+    protected $purifyIgnoreFields = ['id', 'created_at', 'updated_at', 'deleted_at', 'uuid'];
+    protected $customPurifyIgnoreFields = [];
 
     public static function booted()
     {
         static::retrieved(function ($model) {
-            foreach ($model->purifyAttributes as $attribute) {
+            foreach($model->getAttributesToPurify() as $attribute) {
                 $model->mergeCasts([
                     $attribute => PurifyAttributeCast::class,
                 ]);
@@ -19,7 +22,7 @@ trait ModelAttributePurifyTrait
         });
 
         static::creating(function ($model) {
-            foreach ($model->purifyAttributes as $attribute) {
+            foreach ($model->getAttributesToPurify() as $attribute) {
                 $model->purifyAttributeIfNeeded($attribute);
             }
         });
@@ -32,10 +35,22 @@ trait ModelAttributePurifyTrait
      */
     protected function purifyAttributeIfNeeded($attribute)
     {
-        if(in_array($attribute, array_keys($this->casts))) return;  // already in cast
-
         $value = $this->$attribute;
         Request::filter($value);
         $this->setAttribute($attribute, $value);
+    }
+
+    /**
+     * get all model attributes except ignored fields and already casted fields
+     * 
+     * @return array
+     */
+    protected function getAttributesToPurify(): array
+    {
+        $ignoreFields = array_merge($this->purifyIgnoreFields, $this->customPurifyIgnoreFields, array_keys($this->casts));
+
+        return array_keys(
+            Arr::where($this->getAttributes(), fn($value, $key) => !in_array($key, $ignoreFields))
+        );
     }
 }
