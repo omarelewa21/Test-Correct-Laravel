39f8c30b8d7cc89ccaa9f6219bb70b023b7d71b2
Roan <roan@sobit.nl>
2023-07-21 14:02:02 +0200

    Fixed open question answer check with unanswerd items

--- app/Http/Livewire/Teacher/Assessment.php
+++ app/Http/Livewire/Teacher/Assessment.php
@@ -1143,7 +1143,7 @@ class Assessment extends EvaluationComponent implements CollapsableHeader
                 $this->answers->whereIn(
                     'question_id',
                     $this->questions->discussionTypeFiltered(true)->pluck('id')
-                )
+                )->reject(fn($answer) => !$answer->isAnswered)
             )->isEmpty();
     }
 
