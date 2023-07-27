86914eebb6207265e3b4822beea4aba4fd839145
Erik | Sobit <Erik@sobit.nl>
2023-07-27 14:43:17 +0200

    laravel is high enough to make the switch to yearlyOn

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -65,10 +65,11 @@ class Kernel extends ConsoleKernel
          *          ->yearlyOn(8, 1, '06:00'); // only available in laravel 8 and up
          */
         $schedule->command('school_locations:add_new_period')
-            ->monthlyOn(1,'06:00')
-            ->when(function(){
-               return ((int) date('m') === 8); // only on the first of august
-            });
+            ->yearlyOn(8, 1, '06:00'); // only available in laravel 8 and up
+//            ->monthlyOn(1,'06:00')
+//            ->when(function(){
+//               return ((int) date('m') === 8); // only on the first of august
+//            });
 
 //        $schedule->command('school_locations:add_new_period')
 //            ->yearlyOn(8, 1, '06:00'); // only available in laravel 8 and up
