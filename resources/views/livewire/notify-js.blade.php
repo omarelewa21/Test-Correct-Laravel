729ea7174d0d2d4532e9ffbd7670d27ba338c118
Omar Elewa <omar.elewa@test-correct.nl>
2023-08-11 11:55:21 +0300

    remove console logs

--- resources/views/livewire/notify-js.blade.php
+++ resources/views/livewire/notify-js.blade.php
@@ -1,10 +1,8 @@
 <div
     x-data="{
         notify: async (event) => {
-            console.log(event.detail);
             let message = await $wire.getLocalizedMessage(event.detail.message)
             let type = event.detail.type
-            console.log(message, type)
             Notify.notify(message, type)
         }
     }"
