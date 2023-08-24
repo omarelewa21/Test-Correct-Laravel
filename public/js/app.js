2e7ef00459d98d82fc70353dd0cecbe8e10cef99
Roan <roan@sobit.nl>
2023-08-24 11:15:56 +0200

    Added guard to disable new progressbar when its already running

--- public/js/app.js
+++ public/js/app.js
@@ -10266,9 +10266,9 @@ document.addEventListener("alpine:init", function () {
       },
       startTimeout: function startTimeout(eventData) {
         var _this83 = this;
+        if (this.progressBar) return;
         this.progressBar = true;
         this.startTime = eventData.timeout;
-        console.log(eventData);
         if (eventData.timeLeft) {
           this.progress = eventData.timeLeft;
         } else {
@@ -11784,7 +11784,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "662d128370816e2bbb66",
+  key: "fc18ed69b446aeb8c8a5",
   cluster: "eu",
   forceTLS: true
 });
