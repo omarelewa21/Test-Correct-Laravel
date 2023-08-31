58e7cb2443e3385f596d738039a57e4543334982
Roan <roan@sobit.nl>
2023-08-31 10:45:13 +0200

    Refactored true false toggle to use system rating when no valid teacher rating is given

--- app/View/Components/Answer/Student/MultipleChoiceQuestion.php
+++ app/View/Components/Answer/Student/MultipleChoiceQuestion.php
@@ -3,6 +3,7 @@
 namespace tcCore\View\Components\Answer\Student;
 
 use tcCore\Answer;
+use tcCore\AnswerRating;
 use tcCore\Question;
 
 class MultipleChoiceQuestion extends QuestionComponent
@@ -54,15 +55,12 @@ class MultipleChoiceQuestion extends QuestionComponent
      */
     private function getToggleStatus($link, $rating): bool
     {
-        if (!$this->question->isSubType('TrueFalse')) {
-            if (isset($rating->json[$link->order]) && is_bool($rating->json[$link->order])) {
-                return $rating->json[$link->order];
-            }
-            return $link->active && $link->score > 0;
+        if ($this->question->isSubType('TrueFalse')) {
+            return $this->setTrueFalseToggleStatus($rating);
         }
 
-        if ($this->ratingHasBoolValueForKey($rating, $this->question->id)) {
-            $this->trueFalseToggleActive = $rating->json[$this->question->id];
+        if (isset($rating->json[$link->order]) && is_bool($rating->json[$link->order])) {
+            return $rating->json[$link->order];
         }
 
         return $link->active && $link->score > 0;
@@ -77,5 +75,16 @@ class MultipleChoiceQuestion extends QuestionComponent
             return $rating->json[$this->question->id];
         }
         return $correctIds->diff($givenAnswerIds)->isEmpty();
+    }
+
+    private function setTrueFalseToggleStatus(?AnswerRating $teacherRating): bool
+    {
+        $rating = $teacherRating ?? $this->answer->answerRatings()->where('type', AnswerRating::TYPE_SYSTEM)->first();
+
+        $this->trueFalseToggleActive = $this->ratingHasBoolValueForKey($rating, $this->question->id)
+            ? $rating->json[$this->question->id]
+            : (float)$rating?->rating === (float)$this->question->score;
+
+        return $this->trueFalseToggleActive;
     }
 }
