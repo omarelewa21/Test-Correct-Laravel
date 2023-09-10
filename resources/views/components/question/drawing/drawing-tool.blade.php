328401ab325f0bc01ace25cda6ef8788493cf4d7
Omar Elewa <omar.elewa@test-correct.nl>
2023-09-10 18:31:56 +0300

    remove fill color because it now separated in a file

--- resources/views/components/question/drawing/drawing-tool.blade.php
+++ resources/views/components/question/drawing/drawing-tool.blade.php
@@ -126,14 +126,6 @@
                     </div>
                 </div>
 
-                <div class="property-group" id="fill">
-                    <x-input.color-picker  :name="'fill-color'" :id="'fill-color'" :title="__('drawing-modal.Opvulkleur')"/>
-                    <input type="number" name="fill-opacity" id="fill-opacity-number" min="0" max="100" value="25"
-                           step="1" autocomplete="off" title="{{ __('drawing-modal.Doorzichtigheid opvulkleur') }}">
-                    <input class="drawing-toolbar-slider" x-ref="slider" type="range" name="fill-opacity" id="fill-opacity-range" style="cursor: grab"
-                            min="0" max="100" value="25" step="1" autocomplete="off" title="{{ __('drawing-modal.Doorzichtigheid opvulkleur') }}">
-                </div>
-
                 <div class="property-group" id="endmarker-type" title="{{ __('drawing-modal.Type lijneinde') }}">
                     <div id="endmarker-type-wrapper">
                         <button class="endmarker-type active" id="no-endmarker" data-button-group="endmarker-type">
