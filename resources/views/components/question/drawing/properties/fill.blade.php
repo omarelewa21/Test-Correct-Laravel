26d00065af14779f827a879d807b06ef4e681a14
Omar Elewa <omar.elewa@test-correct.nl>
2023-09-10 18:32:29 +0300

    make separate files for properties

--- resources/views/components/question/drawing/properties/fill.blade.php
+++ resources/views/components/question/drawing/properties/fill.blade.php
@@ -2,8 +2,8 @@
 
 <div class="property-group" id="fill-{{$shape}}">
     <x-input.color-picker  :name="'fill-color-'.$shape" title="{{ __('drawing-modal.Opvulkleur') }}"/>
-    <input type="number" name="fill-opacity" id="fill-opacity-number" min="0" max="100" value="25"
+    <input type="number" name="fill-opacity-{{$shape}}" id="fill-opacity-number-{{$shape}}" min="0" max="100" value="25"
            step="1" autocomplete="off" title="{{ __('drawing-modal.Doorzichtigheid opvulkleur') }}">
-    <input class="drawing-toolbar-slider" x-ref="slider" type="range" name="fill-opacity" id="fill-opacity-range" style="cursor: grab"
+    <input class="drawing-toolbar-slider" x-ref="slider" type="range" name="fill-opacity-{{$shape}}" id="fill-opacity-range-{{$shape}}" style="cursor: grab"
             min="0" max="100" value="25" step="1" autocomplete="off" title="{{ __('drawing-modal.Doorzichtigheid opvulkleur') }}">
 </div>
--- resources/views/components/question/drawing/properties/stroke.blade.php
+++ resources/views/components/question/drawing/properties/stroke.blade.php
@@ -3,14 +3,14 @@
 <div class="property-group" id="stroke-{{$shape}}">
     <x-input.color-picker  :name="'stroke-color-'.$shape" title="{{ __('drawing-modal.Randkleur') }}"/>
     <div class="input-with-button-group">
-        <button id="decr-stroke-{{$shape}}" class="Secondary" title="{{ __('drawing-modal.Vergroot randdikte') }}">
+        <button id="decr-stroke-width-{{$shape}}" class="Secondary" title="{{ __('drawing-modal.Vergroot randdikte') }}">
             <div>
                 <x-icon.min/>
             </div>
         </button>
         <input type="number" name="stroke-width-{{$shape}}" id="stroke-width-{{$shape}}" min="0" max="100" value="1"
-               autocomplete="off" title="@lang('drawing-modal.Randdikte')">
-        <button id="incr-stroke-{{$shape}}" class="Secondary" title="{{ __('drawing-modal.Verklein randdikte') }}">
+               autocomplete="off" title="{{ __('drawing-modal.Randdikte') }}">
+        <button id="incr-stroke-width-{{$shape}}" class="Secondary" title="{{ __('drawing-modal.Verklein randdikte') }}">
             <div>
                 <x-icon.plus/>
             </div>
