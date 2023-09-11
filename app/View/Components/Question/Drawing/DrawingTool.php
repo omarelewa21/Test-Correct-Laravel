c00a8c7addafd0f6728636e2f835520c6a854e1a
Omar Elewa <omar.elewa@test-correct.nl>
2023-09-11 05:15:11 +0300

    adding text to shapes

--- app/View/Components/Question/Drawing/DrawingTool.php
+++ app/View/Components/Question/Drawing/DrawingTool.php
@@ -9,7 +9,7 @@ use Illuminate\View\Component;
 
 class DrawingTool extends Component
 {
-    const SHAPES = ['Rectangle', 'Circle', 'Line', 'Freehand'];
+    const SHAPES = ['Rectangle', 'Circle', 'Line', 'Freehand', 'Text'];
 
     public Collection $shapes;
 
