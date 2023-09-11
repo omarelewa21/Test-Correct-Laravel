a132593ed5704b2a5b4b5429521de8896fe0b8f0
Omar Elewa <omar.elewa@test-correct.nl>
2023-09-11 04:35:24 +0300

    transfer the end mark to the line file

--- resources/views/components/question/drawing/drawing-tool.blade.php
+++ resources/views/components/question/drawing/drawing-tool.blade.php
@@ -125,23 +125,6 @@
                         </button>
                     </div>
                 </div>
-
-                <div class="property-group" id="endmarker-type" title="{{ __('drawing-modal.Type lijneinde') }}">
-                    <div id="endmarker-type-wrapper">
-                        <button class="endmarker-type active" id="no-endmarker" data-button-group="endmarker-type">
-                            <x-icon.no-endmarker/>
-                        </button>
-                        <button class="endmarker-type" id="filled-arrow" data-button-group="endmarker-type">
-                            <x-icon.filled-arrow/>
-                        </button>
-                        <button class="endmarker-type" id="two-lines-arrow" data-button-group="endmarker-type">
-                            <x-icon.two-lines-arrow/>
-                        </button>
-                        <button class="endmarker-type" id="filled-dot" data-button-group="endmarker-type">
-                            <x-icon.filled-dot/>
-                        </button>
-                    </div>
-                </div>
             </div>
         </section>
         <button id="exit-btn" title="{{ __('drawing-modal.Sluiten') }}">
