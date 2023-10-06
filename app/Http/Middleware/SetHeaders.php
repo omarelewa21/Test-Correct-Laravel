14da8268a2244d78963983ba255a574c8730f427
Jonathan <jonathanjagt@teachandlearncompany.com>
2023-10-06 12:13:06 +0200

    Enable HSTS for Laravel
    This should automatically apply to all subdomains,
    so only implementing in Laravel is sufficient.

--- app/Http/Middleware/SetHeaders.php
+++ app/Http/Middleware/SetHeaders.php
@@ -19,6 +19,9 @@ class SetHeaders
         $handle = $next($request);
         if (method_exists($handle, 'withHeaders')) { // don't do this for instance for Binary File Responses
             $handle->headers->set('X-Frame-Options', 'SAMEORIGIN');
+            if (env('ENABLE_HSTS', true)) {
+                $handle->headers->set('Strict-Transport-Security', 'max-age=63072000; includeSubdomains');
+            }
         }
 
 
