8c876f14b5a94893998f01cc7a75d13165ad2966
Roan <roan@sobit.nl>
2023-10-23 12:59:18 +0200

    [3473] swapped download links to correct positions; Hide scoreslider thumb on firefox when necessary;

--- app/Http/Livewire/Teacher/PdfDownloadModal.php
+++ app/Http/Livewire/Teacher/PdfDownloadModal.php
@@ -91,7 +91,7 @@ class PdfDownloadModal extends TCModalComponent
     {
         $options = [
             'testopgavenpdf'  => [
-                'link'    => route('teacher.preview.test_attachments', ['test' => $this->test->uuid]),
+                'link'    => route('teacher.preview.test_pdf', ['test' => $this->test->uuid]),
                 'sticker' => 'test-export-questions',
                 'active'  => true,
                 'show'    => true,
@@ -115,7 +115,7 @@ class PdfDownloadModal extends TCModalComponent
                 'text'    => __('cms.toets_pdf_omschrijving'),
             ],
             'testattachments' => [
-                'link'    => route('teacher.preview.test_pdf', ['test' => $this->test->uuid]),
+                'link'    => route('teacher.preview.test_attachments', ['test' => $this->test->uuid]),
                 'sticker' => 'test-export-attachments',
                 'active'  => $this->test->attachments->isNotEmpty(),
                 'show'    => true,
