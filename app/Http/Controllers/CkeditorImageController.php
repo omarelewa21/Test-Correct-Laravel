2f962224aa5210f4bf3445e68ee209a15f69171c
jonnevantleven <jonne@sobit.nl>
2023-11-22 11:31:08 +0100

    [3572] added file MimeType validation for inline images

--- app/Http/Controllers/CkeditorImageController.php
+++ app/Http/Controllers/CkeditorImageController.php
@@ -3,13 +3,27 @@
 namespace tcCore\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class CkeditorImageController extends Controller
 {
     public function store(Request $request, $type)
     {
         if($request->hasFile('upload')) {
+
+            if(Validator::make($request->all(), [
+                'upload' => [
+                    'mimes:png,jpeg,jpg,gif,webp,bmp',
+                    'max:' . \tcCore\Http\Helpers\BaseHelper::getMaxFileUploadSize()
+                ],
+            ])->fails()) {
+                return [
+                    "uploaded" => 0,
+                    "error" => [
+                        "message" => __('cms.ckeditor_file_type_not_allowed')
+                    ]
+                ];
+            }
 
             $request->file('upload')->store('', 'inline_images');
 
