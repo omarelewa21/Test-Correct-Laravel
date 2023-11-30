3e90282c763a6cafbb3fc69c1a1cb952603c2312
Erik | Sobit <Erik@sobit.nl>
2023-11-30 16:35:22 +0100

    [3610] removed test helper function

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -8,15 +8,3 @@ if (!function_exists('settings')) {
         return new Settings();
     }
 }
-
-function haha(){
-
-
-    $controller = new \tcCore\Http\Controllers\PreviewTestTakeController();
-
-    $response = $controller->show(tcCore\TestTake::find(39809), new \Illuminate\Http\Request());
-
-    $prefix = \Illuminate\Support\Str::random(10);
-
-    Storage::put('pdf/preview.pdf', $response->getContent());
-}
