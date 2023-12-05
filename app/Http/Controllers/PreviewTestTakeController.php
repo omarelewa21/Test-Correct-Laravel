30f4fcdd1da7cd73c1abb393478709fa9cdb2111
Erik | Sobit <Erik@sobit.nl>
2023-12-05 09:51:26 +0100

    [3610] added try catch for out of memory

--- app/Http/Controllers/PreviewTestTakeController.php
+++ app/Http/Controllers/PreviewTestTakeController.php
@@ -2,6 +2,7 @@
 
 namespace tcCore\Http\Controllers;
 
+use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -27,57 +28,63 @@ class PreviewTestTakeController extends Controller
 
     public function show(TestTake $testTake, Request $request, $doDelete = true)
     {
-        $titleForPdfPage = $testTake->test->name.' '.Carbon::now()->format('d-m-Y H:i');
-        view()->share('titleForPdfPage',$titleForPdfPage);
-        view()->share('pdf_type','student_test_take');
-        $testParticipants = $testTake->testParticipants()->where('test_take_status_id','>',3)->get();
-        ini_set('max_execution_time', 90);
-        $html = view('test-take-overview-preview',compact(['testTake','testParticipants']))->render();
-
         $rand = Str::random(25);
-        $path = sprintf('pdf/%s.pdf',$rand);
-        $storagePath = storage_path($path);
-        $htmlPath = sprintf('pdf/%s.html',$rand);
-        $htmlStoragePath = storage_path($htmlPath);
 
-        $directoryPath = dirname($htmlStoragePath);
+        try {
+            ini_set('max_execution_time', 100);
 
-        if (!File::exists($directoryPath)) {
-            File::makeDirectory($directoryPath, 0775, true); // Maakt de directory met lees/schrijf/uitvoer rechten voor de eigenaar en alleen leesrechten voor anderen
-        }
+            $titleForPdfPage = $testTake->test->name . ' ' . Carbon::now()->format('d-m-Y H:i');
+            view()->share('titleForPdfPage', $titleForPdfPage);
+            view()->share('pdf_type', 'student_test_take');
+            $testParticipants = $testTake->testParticipants()->where('test_take_status_id', '>', 3)->get();
 
-        file_put_contents($htmlStoragePath,$html);
+            $path = sprintf('pdf/%s.pdf', $rand);
+            $storagePath = storage_path($path);
+            $htmlPath = sprintf('pdf/%s.html', $rand);
+            $htmlStoragePath = storage_path($htmlPath);
 
-        dispatch(new CreatePdfFromHtmlFileAndSaveJob($storagePath,$htmlStoragePath));
-        $runner = 0;
-        $doneFile = $storagePath.'.done';
-        while(!file_exists($doneFile) && $runner < 70){
-            sleep(1);
-            $runner++;
-        }
-        // SOLVED THROUGH LOCKFILE
-//        // the file exists the moment is starts to write, but that may not be the same time as it is closed, we've got to wait for that
-//        // as a fail safe we wait another 2 seconds;
-//        sleep(2);
+            $directoryPath = dirname($htmlStoragePath);
 
-        if(file_exists($storagePath) && $doDelete) {
+            if (!File::exists($directoryPath)) {
+                File::makeDirectory($directoryPath, 0775, true); // Maakt de directory met lees/schrijf/uitvoer rechten voor de eigenaar en alleen leesrechten voor anderen
+            }
 
-            AfterResponse::$performAction[] = function () use ($storagePath,$htmlStoragePath, $doneFile) {
-                if (file_exists($storagePath)) {
-                    unlink($storagePath);
-                }
-                if (file_exists($htmlStoragePath)) {
-                    unlink($htmlStoragePath);
-                }
-                if(file_exists($doneFile)){
-                    unlink($doneFile);
-                }
-            };
+            $html = view('test-take-overview-preview', compact(['testTake', 'testParticipants']))->render();
+            file_put_contents($htmlStoragePath, $html);
 
-            return response()->file($storagePath, [
-                'Content-Type'        => 'application/pdf',
-                'Content-Disposition' => 'inline; filename="toets.pdf"'
-            ]);
+            dispatch(new CreatePdfFromHtmlFileAndSaveJob($storagePath, $htmlStoragePath));
+            $runner = 0;
+            $doneFile = $storagePath . '.done';
+            while (!file_exists($doneFile) && $runner < 90) {
+                sleep(1);
+                $runner++;
+            }
+            // SOLVED THROUGH LOCKFILE
+            //        // the file exists the moment is starts to write, but that may not be the same time as it is closed, we've got to wait for that
+            //        // as a fail safe we wait another 2 seconds;
+            //        sleep(2);
+
+            if (file_exists($storagePath) && $doDelete) {
+
+                AfterResponse::$performAction[] = function () use ($storagePath, $htmlStoragePath, $doneFile) {
+                    if (file_exists($storagePath)) {
+                        unlink($storagePath);
+                    }
+                    if (file_exists($htmlStoragePath)) {
+                        unlink($htmlStoragePath);
+                    }
+                    if (file_exists($doneFile)) {
+                        unlink($doneFile);
+                    }
+                };
+
+                return response()->file($storagePath, [
+                    'Content-Type'        => 'application/pdf',
+                    'Content-Disposition' => 'inline; filename="toets.pdf"'
+                ]);
+            }
+        } catch (\Throwable $e){
+            bugsnag::notifyException($e);
         }
         $request->request->set("error_id", 'PDF-'.$rand.'-'.$testTake->getKey());
         throw new UserFriendlyException(__('test-pdf.Sorry, the download could not be generated, please get in contact in order for us to help you with that.'),500);
