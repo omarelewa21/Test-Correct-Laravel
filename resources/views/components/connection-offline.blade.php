c7629dbf8d2965fa5300357fc2d7efc5694f625f
Erik | Sobit <Erik@sobit.nl>
2024-01-08 15:46:49 +0100

    [3649] needs to show the correct tooltip

--- resources/views/components/connection-offline.blade.php
+++ resources/views/components/connection-offline.blade.php
@@ -1,10 +1,18 @@
 <div class="flex items-center space-x-3 ml-2"
-     x-data="{ showOffline: true }"
-     x-show.transition.duration.200ms="showOffline"
+     x-data="{  }"
+     x-show="Alpine.store('connection').offline"
+     x-transition:enter.duration.200ms
+     x-transition:leave.duration.3000ms
      x-cloak
-     wire:init="shouldDisplayFraudMessage"
-{{--     wire:poll.30000ms="isTestTakeEventConfirmed"--}}
 >
+
+
+        <x-tooltip class="mr-2"
+                :always-left="false"
+        >
+            <span class="text-base text-left">Heel veel uitleg met wat er te doen staat als je offline bent</span>
+        </x-tooltip>
+
     <div class="rounded-full bg-all-red text-white flex justify-center items-center connection-offline"
          style="width:40px;height:40px">
         <x-icon.wifi style="transform: scale(1.5)"/>
