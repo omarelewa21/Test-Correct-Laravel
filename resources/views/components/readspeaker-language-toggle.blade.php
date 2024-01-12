1de83e97c4da686cb27efda4ecbe14b8706d0535
martinfolkerts <m.folkerts@sobit.nl>
2024-01-12 15:48:01 +0100

    tcp-3635 added readspeaker multi language to attachment popup

--- resources/views/components/readspeaker-language-toggle.blade.php
+++ resources/views/components/readspeaker-language-toggle.blade.php
@@ -7,14 +7,14 @@
             x-show="isCurrent('{{ $language }}')"
             type="button"
             style="margin-top:-17px"
-            class="relative bg-white border-1 border-primary  px-2 py-1 rounded-full h-[40px] w-[40px] z-10"
+            class="rs_active_flag_button relative bg-white border-1 border-primary  px-2 py-1 rounded-full h-[40px] w-[40px] z-10"
             x-on:click="openPopover()"
             :class="{ 'bg-blue-50': isHovering }"
             title="{{__('test_take.language.'.$language)}}"
         >
             <div
 
-                class="w-8 h-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                class="rs_flag_absolute w-8 h-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
                 style="border-color: rgba(0, 77, 245, 0.5); border-width: 1px; border-style: solid; border-radius: 50%;"
 
             ></div>
@@ -26,7 +26,7 @@
         <div
             x-show="isOpen"
             x-on:click.away="closePopover()"
-            class="absolute z-[5] w-[40px] bg-white rounded-t-full rounded-b-full shadow-lg"
+            class="rs_language_menu absolute z-[5] w-[40px] bg-white rounded-t-full rounded-b-full shadow-lg"
             style="top: -170px; left: 0px; padding-bottom: 40px"
         >
 
@@ -39,13 +39,13 @@
                     type="button"
                     x-show="!isCurrent('{{ $language }}')"
                     x-on:click="selectLanguage('{{$language}}')"
-                    class="px-2 py-1 rounded-full h-[40px] w-[40px] bg-white relative"
+                    class="rs_flag px-2 py-1 rounded-full h-[40px] w-[40px] bg-white relative"
                     title="{{__('test_take.language.'.$language)}}"
 
                 >
                      <div
                          x-show="isHovering"
-                         class="w-8 h-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                         class="rs_flag_highlight w-8 h-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
                          style="border-color: rgba(0, 77, 245, 0.5); border-width: 1px; border-style: solid; border-radius: 50%; ;"
 
                      ></div>
